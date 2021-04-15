351e2cfeabd9c5fbb96e2ca8a0217a66b104c315
이백행 <dlqorgod@naver.com>
2021-04-16 06:56:55 +0900

    CmmUseDAOTest_selectOgrnztIdDetail 단위테스트
    org.mybatis.spring.MyBatisSystemException: nested exception is org.apache.ibatis.reflection.ReflectionException: There is no getter for property named 'detailConditionValue' in 'class egovframework.com.cmm.ComDefaultCodeVO'
    
    #{detailConditionValue} 를 #{detailCondition} 로 수정

---
+++ src/test/java/egovframework/com/cmm/service/impl/CmmUseDAOTest_selectOgrnztIdDetail.java
@@ -0,0 +1,48 @@
+package egovframework.com.cmm.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import egovframework.com.cmm.ComDefaultCodeVO;
+import egovframework.com.cmm.service.CmmnDetailCode;
+import egovframework.com.test.DAOTest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CmmUseDAOTest_selectOgrnztIdDetail extends DAOTest {
+
+	@Autowired
+	CmmUseDAO cmmUseDAO;
+
+	@Test
+	public void test() throws Exception {
+		log.debug("test");
+
+		// given
+		ComDefaultCodeVO vo = new ComDefaultCodeVO();
+		vo.setTableNm("COMTNORGNZTINFO");
+		vo.setHaveDetailCondition("Y");
+		vo.setDetailCondition("ORGNZT_0000000000000");
+
+		// when
+		List<CmmnDetailCode> results = cmmUseDAO.selectOgrnztIdDetail(vo);
+
+		// then
+		assertEquals(results.get(0).getCodeId(), vo.getTableNm());
+
+		log.debug("results={}", results);
+
+		results.forEach(result -> {
+			log.debug("result={}", result);
+			log.debug("getCodeId={}", result.getCodeId());
+			log.debug("getCode={}", result.getCode());
+			log.debug("getCodeNm={}", result.getCodeNm());
+			log.debug("getCodeDc={}", result.getCodeDc());
+		});
+	}
+
+}
