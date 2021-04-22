217ceef51a392d06fa30d69227079f93d1a60451
이백행 <dlqorgod@naver.com>
2021-04-23 06:58:38 +0900

    FileManageDAO '여러 개의 파일에 대한 정보(속성 및 상세)를 등록한다.' 단위 테스트

---
+++ src/test/java/egovframework/com/cmm/service/impl/FileManageDAOTest_insertFileInfs.java
@@ -0,0 +1,101 @@
+package egovframework.com.cmm.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import egovframework.com.cmm.service.FileVO;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { FileManageDAOTest_insertFileInfs.class })
+@ActiveProfiles({ "mysql", "dummy" })
+@Transactional
+
+@Configuration
+@ImportResource({ "classpath*:egovframework/spring/com/test-context-dao.xml",
+		"classpath*:egovframework/spring/com/idgn/context-idgn-File.xml" })
+@ComponentScan(useDefaultFilters = false, basePackages = { "egovframework.com.cmm.service.impl" }, includeFilters = {
+		@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { FileManageDAO.class }) })
+
+public class FileManageDAOTest_insertFileInfs {
+
+	@Autowired
+	private ApplicationContext context;
+
+	@Autowired
+	private FileManageDAO fileManageDAO;
+
+	@Resource(name = "egovFileIdGnrService")
+	private EgovIdGnrService egovFileIdGnrService;
+
+	@Before
+	public void setUp() throws Exception {
+		String[] beanDefinitionNames = context.getBeanDefinitionNames();
+
+		for (String beanDefinitionName : beanDefinitionNames) {
+			log.debug("beanDefinitionName={}", beanDefinitionName);
+		}
+	}
+
+	@Test
+	public void test() throws Exception {
+		log.debug("test");
+
+		// given
+		FileVO fvo = new FileVO();
+		fvo.setAtchFileId(egovFileIdGnrService.getNextStringId());
+		int maxFileSN = fileManageDAO.getMaxFileSN(fvo);
+		log.debug("maxFileSN={}", maxFileSN);
+
+		List<FileVO> fileList = new ArrayList<>();
+		FileVO file = new FileVO();
+		file.setAtchFileId(fvo.getAtchFileId());
+		file.setFileSn(String.valueOf(maxFileSN++));
+		file.setFileMg(file.getFileSn());
+		fileList.add(file);
+
+		file = new FileVO();
+		file.setAtchFileId(fvo.getAtchFileId());
+		file.setFileSn(String.valueOf(maxFileSN++));
+		file.setFileMg(file.getFileSn());
+		fileList.add(file);
+
+		for (int i = 0; i < 10; i++) {
+			file = new FileVO();
+			file.setAtchFileId(fvo.getAtchFileId());
+			file.setFileSn(String.valueOf(maxFileSN++));
+			file.setFileMg(file.getFileSn());
+			fileList.add(file);
+		}
+
+		// when
+		String result = fileManageDAO.insertFileInfs(fileList);
+
+		// then
+		assertEquals(result, fvo.getAtchFileId());
+
+		log.debug("result={}", result);
+	}
+
+}
---
+++ src/test/java/egovframework/com/test/TodoTest.java
@@ -0,0 +1,47 @@
+package egovframework.com.test;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.SystemUtils;
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TodoTest {
+
+	@Test
+	public void test() {
+		String pathname = "C:\\EGOVFRAME-3.10.0\\git\\egovframe-common-components\\src\\main\\java\\egovframework\\com";
+
+		final File directory = new File(pathname);
+		final String[] extensions = { "java" };
+		final boolean recursive = true;
+
+		Collection<File> listFiles = FileUtils.listFiles(directory, extensions, recursive);
+		int size = listFiles.size();
+
+		StringBuffer sb = new StringBuffer();
+		sb.append(SystemUtils.LINE_SEPARATOR);
+
+		int i = 1;
+
+		for (File listFile : listFiles) {
+			String name = listFile.getName();
+			if (name.endsWith("DAO.java")) {
+				sb.append(i);
+				sb.append(". ");
+				sb.append(name.replaceAll(".java", ""));
+				sb.append(SystemUtils.LINE_SEPARATOR);
+
+				i++;
+			}
+		}
+
+		log.debug(sb.toString());
+		log.debug("size={}", size);
+	}
+
+}
