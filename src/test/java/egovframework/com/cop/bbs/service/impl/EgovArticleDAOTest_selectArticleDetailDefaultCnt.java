9ae26fb51a80a6cc98ab24e711ba80c165c4c86a
이백행 <dlqorgod@naver.com>
2021-06-19 09:37:24 +0900

    EgovArticleDAO selectArticleDetailDefaultCnt 단위 테스트

---
+++ src/test/java/egovframework/com/cop/bbs/service/impl/EgovArticleDAOTest_selectArticleDetailDefaultCnt.java
@@ -0,0 +1,49 @@
+package egovframework.com.cop.bbs.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import egovframework.com.cop.bbs.service.BoardVO;
+import egovframework.com.test.EgovTestV1;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ContextConfiguration(classes = { EgovArticleDAOTest_Configuration.class })
+public class EgovArticleDAOTest_selectArticleDetailDefaultCnt extends EgovTestV1 {
+
+	@Resource(name = "egovBBSMstrIdGnrService")
+	private EgovIdGnrService egovBBSMstrIdGnrService;
+
+	@Resource(name = "egovBBSMstrIdGnrService")
+	private EgovIdGnrService egovNttIdGnrService;
+
+	@Resource(name = "egovBlogIdGnrService")
+	private EgovIdGnrService egovBlogIdGnrService;
+
+	@Autowired
+	private EgovArticleDAO egovArticleDAO;
+
+	@Test
+//	@Commit
+	public void test() throws Exception {
+		log.debug("test");
+
+		// given
+		BoardVO boardVO = new BoardVO();
+		boardVO.setBbsId(egovBBSMstrIdGnrService.getNextStringId());
+
+		// when
+		int rticleDetailDefaultCnt = egovArticleDAO.selectArticleDetailDefaultCnt(boardVO);
+		log.debug("rticleDetailDefaultCnt={}", rticleDetailDefaultCnt);
+
+		// then
+		assertEquals(rticleDetailDefaultCnt, 0);
+	}
+
+}
