9446528a66b5e0248cd3fba98dfbedcc9ec73311
이백행 <dlqorgod@naver.com>
2021-06-22 07:55:52 +0900

    EgovArticleDAO selectLoginUser 단위 테스트

---
+++ src/test/java/egovframework/com/cop/bbs/service/impl/EgovArticleDAOTest_selectLoginUser.java
@@ -0,0 +1,54 @@
+package egovframework.com.cop.bbs.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.com.cop.bbs.service.BoardVO;
+import egovframework.com.test.EgovTestV1;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ContextConfiguration(classes = { EgovArticleDAOTest_Configuration.class })
+public class EgovArticleDAOTest_selectLoginUser extends EgovTestV1 {
+
+	@Resource(name = "egovBBSMstrIdGnrService")
+	private EgovIdGnrService egovBBSMstrIdGnrService;
+
+	@Resource(name = "egovNttIdGnrService")
+	private EgovIdGnrService egovNttIdGnrService;
+
+	@Resource(name = "egovBlogIdGnrService")
+	private EgovIdGnrService egovBlogIdGnrService;
+
+	@Autowired
+	private EgovArticleDAO egovArticleDAO;
+
+	@Test
+//	@Commit
+	public void test() throws Exception {
+		log.debug("test");
+
+		// given
+		LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+
+		BoardVO boardVO = new BoardVO();
+		boardVO.setFrstRegisterId(loginVO.getUniqId());
+		boardVO.setBlogId(egovBBSMstrIdGnrService.getNextStringId());
+
+		// when
+		int loginUser = egovArticleDAO.selectLoginUser(boardVO);
+		log.debug("loginUser={}", loginUser);
+
+		// then
+		assertEquals(loginUser, 0);
+	}
+
+}
