f24cb7dcba66db40ba2d8767d1d500f40515f7fa
이백행 <dlqorgod@naver.com>
2021-07-02 06:52:50 +0900

    EgovArticleDAO testData selectArticleCnOne 단위 테스트

--- src/test/java/egovframework/com/cop/bbs/service/impl/EgovArticleDAOTest_selectArticleCnOne.java
+++ src/test/java/egovframework/com/cop/bbs/service/impl/EgovArticleDAOTest_selectArticleCnOne.java
@@ -2,21 +2,13 @@ package egovframework.com.cop.bbs.service.impl;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Date;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 
-import egovframework.com.cmm.LoginVO;
-import egovframework.com.cmm.util.EgovUserDetailsHelper;
 import egovframework.com.cop.bbs.service.Board;
-import egovframework.com.cop.bbs.service.BoardMaster;
 import egovframework.com.cop.bbs.service.BoardVO;
 import egovframework.com.test.EgovTestV1;
-import egovframework.rte.fdl.idgnr.EgovIdGnrService;
-import egovframework.rte.fdl.string.EgovDateUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -27,15 +19,7 @@ public class EgovArticleDAOTest_selectArticleCnOne extends EgovTestV1 {
 	private EgovArticleDAO egovArticleDAO;
 
 	@Autowired
-	private EgovBBSMasterDAO egovBBSMasterDAO;
-
-	@Autowired
-	@Qualifier("egovNttIdGnrService")
-	private EgovIdGnrService egovNttIdGnrService;
-
-	@Autowired
-	@Qualifier("egovBBSMstrIdGnrService")
-	private EgovIdGnrService egovBBSMstrIdGnrService;
+	private EgovArticleDAOTest_AaaTestData egovArticleDAOTest_AaaTestData;
 
 	@Test
 //	@Commit
@@ -43,39 +27,9 @@ public class EgovArticleDAOTest_selectArticleCnOne extends EgovTestV1 {
 		log.debug("test");
 
 		// given
-		LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
-
-		// insertBBSMasterInf
-		BoardMaster boardMaster = new BoardMaster();
-		boardMaster.setBbsId(egovBBSMstrIdGnrService.getNextStringId());
-		boardMaster.setFrstRegisterId(loginVO.getUniqId());
-
-		egovBBSMasterDAO.insertBBSMasterInf(boardMaster);
-
-		// insertArticle
-		Board board = new Board();
-
-		board.setNttId(egovNttIdGnrService.getNextIntegerId());
-		board.setBbsId(boardMaster.getBbsId());
-
-		String today = " " + EgovDateUtil.toString(new Date(), null, null);
-		board.setNttSj("test 게시물제목" + today);
-		board.setNttCn("test 게시물내용" + today);
-
-		board.setParnts("0");
-		board.setReplyLc("0");
-		board.setReplyAt("N");
-
-		board.setUseAt("Y");
-//		board.setNoticeAt("Y");
-
-		board.setFrstRegisterId(loginVO.getUniqId());
-
-		egovArticleDAO.insertArticle(board);
-
-		// selectArticleCnOne
+		Board board = egovArticleDAOTest_AaaTestData.insertArticle();
 		BoardVO boardVO = new BoardVO();
-		boardVO.setBbsId(boardMaster.getBbsId());
+		boardVO.setBbsId(board.getBbsId());
 
 		// when
 		BoardVO articleCnOne = egovArticleDAO.selectArticleCnOne(boardVO);
