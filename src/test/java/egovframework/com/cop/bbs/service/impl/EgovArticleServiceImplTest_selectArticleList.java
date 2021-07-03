fa7522454f3aa79c47456cc48cb8bec0eae08ce0
이백행 <dlqorgod@naver.com>
2021-07-03 17:32:30 +0900

    EgovArticleServiceImpl JavaParser 단위 테스트

---
+++ src/test/java/egovframework/com/cop/bbs/service/impl/EgovArticleServiceImplTest_selectArticleList.java
@@ -0,0 +1,14 @@
+package egovframework.com.cop.bbs.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EgovArticleServiceImplTest_selectArticleList {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
