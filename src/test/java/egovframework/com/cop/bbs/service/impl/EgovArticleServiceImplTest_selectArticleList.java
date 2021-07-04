a96087ac163549906404e3eed8ec33326573cc90
이백행 <dlqorgod@naver.com>
2021-07-05 08:09:20 +0900

    EgovArticleServiceImpl @Configuration 단위 테스트

--- src/test/java/egovframework/com/cop/bbs/service/impl/EgovArticleServiceImplTest_selectArticleList.java
+++ src/test/java/egovframework/com/cop/bbs/service/impl/EgovArticleServiceImplTest_selectArticleList.java
@@ -1,14 +1,40 @@
 package egovframework.com.cop.bbs.service.impl;
 
-import static org.junit.Assert.*;
+import java.util.Map;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 
-public class EgovArticleServiceImplTest_selectArticleList {
+import egovframework.com.cop.bbs.service.Board;
+import egovframework.com.cop.bbs.service.BoardVO;
+import egovframework.com.cop.bbs.service.EgovArticleService;
+import egovframework.com.test.EgovTestV1;
+import egovframework.rte.fdl.cmmn.exception.FdlException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ContextConfiguration(classes = { EgovArticleServiceImplTest_AAB_Configuration.class })
+public class EgovArticleServiceImplTest_selectArticleList extends EgovTestV1 {
+
+	@Autowired
+	private EgovArticleDAOTest_AaaTestData egovArticleDAOTest_AaaTestData;
+
+	@Autowired
+	private EgovArticleService egovArticleService;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws FdlException {
+		log.debug("test");
+
+		Board board = egovArticleDAOTest_AaaTestData.insertArticle();
+
+		BoardVO boardVO = new BoardVO();
+		boardVO.setBbsId(board.getBbsId());
+
+		Map<String, Object> result = egovArticleService.selectArticleList(boardVO);
+
+		log.debug("result={}", result);
 	}
 
 }
