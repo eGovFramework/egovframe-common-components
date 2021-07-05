6556592501f7e8bda8c73401d7f3d0a96b915c33
이백행 <dlqorgod@naver.com>
2021-07-06 07:07:49 +0900

    EgovArticleServiceImpl @Configuration 단위 테스트

--- src/test/java/egovframework/com/cop/bbs/service/impl/EgovArticleServiceImplTest_AAB_Configuration.java
+++ src/test/java/egovframework/com/cop/bbs/service/impl/EgovArticleServiceImplTest_AAB_Configuration.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ImportResource;
 
-import egovframework.com.cmm.service.EgovFileMngService;
+import egovframework.com.cmm.service.impl.EgovFileMngServiceImpl;
+import egovframework.com.cmm.service.impl.FileManageDAO;
 
 @Configuration
 
@@ -18,6 +19,7 @@ import egovframework.com.cmm.service.EgovFileMngService;
 		"classpath*:egovframework/spring/com/context-datasource.xml",
 		"classpath*:egovframework/spring/com/context-egovuserdetailshelper.xml",
 		"classpath*:egovframework/spring/com/context-mapper.xml",
+		"classpath*:egovframework/spring/com/context-properties.xml",
 		"classpath*:egovframework/spring/com/context-transaction.xml",
 
 		"classpath*:egovframework/spring/com/idgn/context-idgn-bbs.xml",
@@ -26,11 +28,11 @@ import egovframework.com.cmm.service.EgovFileMngService;
 
 })
 
-@ComponentScan(useDefaultFilters = false, basePackages = {
-		"egovframework.com.cop.bbs.service.impl", }, includeFilters = {
+@ComponentScan(useDefaultFilters = false, basePackages = { "egovframework.com.cop.bbs.service.impl",
+		"egovframework.com.cmm.service.impl" }, includeFilters = {
 				@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { EgovArticleDAO.class, EgovBBSMasterDAO.class,
 						EgovArticleDAOTest_AaaTestData.class, EgovArticleServiceImpl.class,
-						EgovFileMngService.class }) })
+						EgovFileMngServiceImpl.class, FileManageDAO.class }) })
 
 public class EgovArticleServiceImplTest_AAB_Configuration {
 
