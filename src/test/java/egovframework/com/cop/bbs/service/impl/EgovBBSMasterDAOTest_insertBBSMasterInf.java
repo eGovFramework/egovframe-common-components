03917915b11a532dbc86816b4bc372250863dbe3
이백행 <dlqorgod@naver.com>
2021-08-02 20:31:28 +0900

    EgovBBSMasterDAO insertBBSMasterInf 단위 테스트

--- src/test/java/egovframework/com/cop/bbs/service/impl/EgovBBSMasterDAOTest_insertBBSMasterInf.java
+++ src/test/java/egovframework/com/cop/bbs/service/impl/EgovBBSMasterDAOTest_insertBBSMasterInf.java
@@ -2,16 +2,21 @@ package egovframework.com.cop.bbs.service.impl;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
 import egovframework.com.cop.bbs.service.BoardMaster;
 import egovframework.com.test.EgovTestV1;
 import egovframework.rte.fdl.cmmn.exception.FdlException;
 import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import egovframework.rte.fdl.string.EgovDateUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -23,6 +28,12 @@ public class EgovBBSMasterDAOTest_insertBBSMasterInf extends EgovTestV1 {
 
 	@Resource(name = "egovBBSMstrIdGnrService")
 	private EgovIdGnrService egovBBSMstrIdGnrService;
+
+	@Resource(name = "egovNttIdGnrService")
+	private EgovIdGnrService egovNttIdGnrService;
+
+	@Resource(name = "egovBlogIdGnrService")
+	private EgovIdGnrService egovBlogIdGnrService;
 
 	@Test
 	public void test() {
@@ -36,18 +47,26 @@ public class EgovBBSMasterDAOTest_insertBBSMasterInf extends EgovTestV1 {
 			log.error(e.getMessage());
 		}
 
+		String today = " " + EgovDateUtil.toString(new Date(), null, null);
+
+		LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+
 		boardMaster.setBbsTyCode("BBST01"); // COM101 BBST01 통합게시판
-		boardMaster.setBbsNm("");
-		boardMaster.setBbsIntrcn("");
-		boardMaster.setReplyPosblAt("");
-		boardMaster.setFileAtchPosblAt("");
-		boardMaster.setAtchPosblFileNumber(0);
-		boardMaster.setTmplatId("");
-		boardMaster.setUseAt("");
-		boardMaster.setCmmntyId("");
-		boardMaster.setFrstRegisterId("");
-		boardMaster.setBlogId("");
-		boardMaster.setBlogAt("");
+		boardMaster.setBbsNm("test 게시판명" + today); // 게시판명
+		boardMaster.setBbsIntrcn("test 게시판소개" + today); // 게시판소개
+		boardMaster.setReplyPosblAt("Y"); // 답장가능여부
+		boardMaster.setFileAtchPosblAt("Y"); // 파일첨부가능여부
+		boardMaster.setAtchPosblFileNumber(3); // 첨부가능파일숫자
+		boardMaster.setTmplatId(""); // 템플릿ID
+		boardMaster.setUseAt("Y"); // 사용여부
+		boardMaster.setCmmntyId(""); // 커뮤니티ID
+		boardMaster.setFrstRegisterId(loginVO.getUniqId()); // 최초등록자ID
+		try {
+			boardMaster.setBlogId(egovBlogIdGnrService.getNextStringId());
+		} catch (FdlException e) {
+			log.error(e.getMessage());
+		} // 블로그 ID
+		boardMaster.setBlogAt("Y"); // 블로그 여부
 
 		// when
 		boolean result = false;
