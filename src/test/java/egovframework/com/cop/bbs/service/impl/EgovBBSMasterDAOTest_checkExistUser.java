8cf6f6c65478b52886e1ce41992cdb22ce5f690e
이백행 <dlqorgod@naver.com>
2021-09-06 07:27:48 +0900

    EgovBBSMasterDAOTest_checkExistUser 단위 테스트

---
+++ src/test/java/egovframework/com/cop/bbs/service/impl/EgovBBSMasterDAOTest_checkExistUser.java
@@ -0,0 +1,84 @@
+package egovframework.com.cop.bbs.service.impl;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.com.cop.bbs.service.BlogVO;
+import egovframework.com.test.EgovTestV1;
+import egovframework.rte.fdl.cmmn.exception.FdlException;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import egovframework.rte.fdl.string.EgovDateUtil;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ContextConfiguration(classes = { EgovBBSMasterDAOTest_AAA_Configuration.class })
+public class EgovBBSMasterDAOTest_checkExistUser extends EgovTestV1 {
+
+	@Autowired
+	private EgovBBSMasterDAO egovBBSMasterDAO;
+
+	@Resource(name = "egovBBSMstrIdGnrService")
+	private EgovIdGnrService egovBBSMstrIdGnrService;
+
+//	@Resource(name = "egovNttIdGnrService")
+//	private EgovIdGnrService egovNttIdGnrService;
+
+	@Resource(name = "egovBlogIdGnrService")
+	private EgovIdGnrService egovBlogIdGnrService;
+
+	@Test
+	public void test() {
+		log.debug("test");
+
+		// given
+		BlogVO blogVO = testData();
+
+		// when
+		int checkExistUser = egovBBSMasterDAO.checkExistUser(blogVO);
+
+		// then
+		assertTrue("checkExistUser", checkExistUser > 1);
+	}
+
+	BlogVO testData() {
+		String today = " " + EgovDateUtil.toString(new Date(), null, null);
+		LoginVO authenticatedUser = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+
+		BlogVO blogVO = new BlogVO();
+
+		try {
+			blogVO.setBlogId(egovBlogIdGnrService.getNextStringId());
+		} catch (FdlException e) {
+			log.error(e.getMessage());
+		}
+
+		blogVO.setBlogNm("test 블로그 명" + today);
+		blogVO.setBlogIntrcn("test 블로그 명" + today);
+		blogVO.setRegistSeCode("REGC02"); // 커뮤니티 등록
+//		blogVO.setTmplatId("");
+		blogVO.setUseAt("Y");
+		blogVO.setFrstRegisterId(authenticatedUser.getUniqId());
+
+		try {
+			blogVO.setBbsId(egovBBSMstrIdGnrService.getNextStringId());
+		} catch (FdlException e) {
+			log.error(e.getMessage());
+		}
+
+		blogVO.setBlogAt("Y");
+
+		egovBBSMasterDAO.insertBlogMaster(blogVO);
+
+		return blogVO;
+	}
+
+}
