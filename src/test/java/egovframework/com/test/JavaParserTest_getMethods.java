20d3ef3946d9d3c283de608d4c56728b04c1a8ef
이백행 <dlqorgod@naver.com>
2021-10-12 07:42:37 +0900

    JavaParserTest_getMethods EgovArticleCommentDAO

---
+++ src/test/java/egovframework/com/test/JavaParserTest_getMethods.java
@@ -0,0 +1,72 @@
+package egovframework.com.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.Test;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
+import egovframework.com.cop.cmt.service.impl.EgovArticleCommentDAO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JavaParserTest_getMethods {
+
+	@Test
+	public void test() {
+		try {
+			String first = getFirst(EgovArticleCommentDAO.class);
+
+			CompilationUnit cu = StaticJavaParser.parse(Paths.get(first));
+
+			StringBuffer sb = new StringBuffer();
+
+			cu.getPrimaryTypeName().ifPresent(primaryTypeName -> {
+
+				log.debug("primaryTypeName={}", primaryTypeName);
+
+				cu.getClassByName(primaryTypeName).ifPresent(coid -> {
+
+					coid.getMethods().forEach(method -> {
+
+						String methodName = method.getNameAsString();
+
+						log.debug("methodName={}", methodName);
+
+						sb.append(primaryTypeName);
+						sb.append("Test_");
+						sb.append(methodName);
+						sb.append("\n");
+
+					});
+
+				});
+
+				try {
+					FileUtils.writeStringToFile(
+							new File(SystemUtils.USER_HOME + "/Desktop/god.codegen/" + primaryTypeName + ".txt"),
+							sb.toString(), StandardCharsets.UTF_8);
+				} catch (IOException e) {
+					log.error(e.getMessage());
+				}
+
+			});
+
+			log.debug("sb={}", sb);
+
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private String getFirst(Class<?> clazz) {
+		return "src/main/java/" + clazz.getCanonicalName().replaceAll("\\.", "/") + ".java";
+	}
+
+}
