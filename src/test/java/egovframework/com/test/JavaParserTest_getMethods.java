6731f0106140eaaab71907f35552c0feda2d31e8
이백행 <dlqorgod@naver.com>
2021-10-22 08:22:51 +0900

    JavaParserTest_getMethods getClassByName getInterfaceByName

--- src/test/java/egovframework/com/test/JavaParserTest_getMethods.java
+++ src/test/java/egovframework/com/test/JavaParserTest_getMethods.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 
 import egovframework.com.cop.cmt.service.impl.EgovArticleCommentServiceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +23,7 @@ public class JavaParserTest_getMethods {
 	public void test() {
 		try {
 //			String first = getFirst(EgovArticleCommentDAO.class);
+//			String first = getFirst(EgovArticleCommentService.class);
 			String first = getFirst(EgovArticleCommentServiceImpl.class);
 
 			CompilationUnit cu = StaticJavaParser.parse(Paths.get(first));
@@ -33,20 +35,11 @@ public class JavaParserTest_getMethods {
 				log.debug("primaryTypeName={}", primaryTypeName);
 
 				cu.getClassByName(primaryTypeName).ifPresent(coid -> {
+					getMethods(coid, sb, primaryTypeName);
+				});
 
-					coid.getMethods().forEach(method -> {
-
-						String methodName = method.getNameAsString();
-
-						log.debug("methodName={}", methodName);
-
-						sb.append(primaryTypeName);
-						sb.append("Test_");
-						sb.append(methodName);
-						sb.append("\n");
-
-					});
-
+				cu.getInterfaceByName(primaryTypeName).ifPresent(coid -> {
+					getMethods(coid, sb, primaryTypeName);
 				});
 
 				try {
@@ -68,6 +61,21 @@ public class JavaParserTest_getMethods {
 
 	private String getFirst(Class<?> clazz) {
 		return "src/main/java/" + clazz.getCanonicalName().replaceAll("\\.", "/") + ".java";
+	}
+
+	private void getMethods(ClassOrInterfaceDeclaration coid, StringBuffer sb, String primaryTypeName) {
+		coid.getMethods().forEach(method -> {
+
+			String methodName = method.getNameAsString();
+
+			log.debug("methodName={}", methodName);
+
+			sb.append(primaryTypeName);
+			sb.append("Test_");
+			sb.append(methodName);
+			sb.append("\n");
+
+		});
 	}
 
 }
