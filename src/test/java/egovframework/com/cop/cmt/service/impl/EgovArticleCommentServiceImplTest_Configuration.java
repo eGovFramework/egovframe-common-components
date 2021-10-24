bffd4ba8bcb78cfa782567990cd96cf5eef91b65
이백행 <dlqorgod@naver.com>
2021-10-25 07:45:46 +0900

    EgovArticleCommentServiceImplTest_insertArticleComment 단위 테스트

---
+++ src/test/java/egovframework/com/cop/cmt/service/impl/EgovArticleCommentServiceImplTest_Configuration.java
@@ -0,0 +1,41 @@
+package egovframework.com.cop.cmt.service.impl;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ImportResource;
+
+import egovframework.com.cop.bbs.service.impl.BBSAddedOptionsDAO;
+import egovframework.com.cop.bbs.service.impl.EgovArticleDAO;
+import egovframework.com.cop.bbs.service.impl.EgovBBSMasterDAO;
+
+@Configuration
+
+@ImportResource({
+
+//	"classpath*:egovframework/spring/com/**/context-*.xml",
+
+		"classpath*:egovframework/spring/com/context-crypto.xml",
+		"classpath*:egovframework/spring/com/context-datasource.xml",
+		"classpath*:egovframework/spring/com/context-egovuserdetailshelper.xml",
+		"classpath*:egovframework/spring/com/context-mapper.xml",
+//		"classpath*:egovframework/spring/com/context-properties.xml",
+		"classpath*:egovframework/spring/com/context-transaction.xml",
+
+		"classpath*:egovframework/spring/com/idgn/context-idgn-bbs.xml",
+		"classpath*:egovframework/spring/com/idgn/context-idgn-AnswerNo.xml",
+
+		"classpath*:egovframework/spring/com/test-context-common.xml",
+
+})
+
+@ComponentScan(useDefaultFilters = false, basePackages = { "egovframework.com.cop.cmt.service.impl",
+		"egovframework.com.cop.bbs.service.impl" }, includeFilters = {
+				@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { EgovArticleCommentDAO.class,
+						EgovBBSMasterDAO.class, EgovArticleDAO.class, EgovArticleCommentServiceImpl.class,
+						BBSAddedOptionsDAO.class }) })
+
+public class EgovArticleCommentServiceImplTest_Configuration {
+
+}
---
+++ src/test/java/egovframework/com/cop/cmt/service/impl/EgovArticleCommentServiceImplTest_insertArticleComment.java
@@ -0,0 +1,132 @@
+package egovframework.com.cop.cmt.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.com.cop.bbs.service.Board;
+import egovframework.com.cop.bbs.service.BoardMaster;
+import egovframework.com.cop.bbs.service.impl.EgovArticleDAO;
+import egovframework.com.cop.bbs.service.impl.EgovBBSMasterDAO;
+import egovframework.com.cop.cmt.service.Comment;
+import egovframework.com.cop.cmt.service.EgovArticleCommentService;
+import egovframework.com.test.EgovTestV1;
+import egovframework.rte.fdl.cmmn.exception.FdlException;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import egovframework.rte.fdl.string.EgovDateUtil;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ContextConfiguration(classes = { EgovArticleCommentServiceImplTest_Configuration.class })
+public class EgovArticleCommentServiceImplTest_insertArticleComment extends EgovTestV1 {
+
+	@Resource(name = "egovBBSMstrIdGnrService")
+	private EgovIdGnrService egovBBSMstrIdGnrService;
+
+	@Resource(name = "egovNttIdGnrService")
+	private EgovIdGnrService egovNttIdGnrService;
+
+//	@Resource(name = "egovBlogIdGnrService")
+//	private EgovIdGnrService egovBlogIdGnrService;
+
+	@Resource(name = "egovAnswerNoGnrService")
+	private EgovIdGnrService egovAnswerNoGnrService;
+
+	@Resource
+	private EgovBBSMasterDAO egovBBSMasterDAO;
+
+	@Resource
+	private EgovArticleDAO egovArticleDAO;
+
+	@Resource(name = "EgovArticleCommentService")
+	private EgovArticleCommentService egovArticleCommentService;
+
+	// testData
+	String today;
+	LoginVO authenticatedUser;
+
+	BoardMaster boardMaster;
+
+	Board board;
+
+	// given
+	Comment comment;
+
+	// when
+	boolean result = false;
+
+	@Test
+//	@Commit
+	public void test() {
+		log.debug("test");
+		testData();
+		testData2();
+		testData3();
+		given();
+		when();
+		then();
+	}
+
+	void testData() {
+		today = " " + EgovDateUtil.toString(new Date(), null, null);
+		authenticatedUser = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+	}
+
+	void testData2() {
+		boardMaster = new BoardMaster();
+		try {
+			boardMaster.setBbsId(egovBBSMstrIdGnrService.getNextStringId());
+		} catch (FdlException e) {
+			log.error(e.getMessage());
+		}
+
+		egovBBSMasterDAO.insertBBSMasterInf(boardMaster);
+	}
+
+	void testData3() {
+		board = new Board();
+		try {
+			board.setNttId(egovNttIdGnrService.getNextLongId());
+		} catch (FdlException e) {
+			log.error(e.getMessage());
+		}
+
+		board.setBbsId(boardMaster.getBbsId());
+		egovArticleDAO.insertArticle(board);
+	}
+
+	void given() {
+		comment = new Comment();
+		// comment.setCommentNo("");
+		comment.setNttId(board.getNttId());
+		comment.setBbsId(boardMaster.getBbsId());
+		comment.setWrterId(authenticatedUser.getUniqId()); // 작성자ID
+		comment.setWrterNm(authenticatedUser.getName()); // 작성자명
+		comment.setCommentPassword("test 비밀번호" + today); // 비밀번호
+		comment.setCommentCn("test 댓글" + today); // 댓글
+		comment.setFrstRegisterId(authenticatedUser.getUniqId()); // 최초등록자ID
+	}
+
+	void when() {
+		try {
+			egovArticleCommentService.insertArticleComment(comment);
+			result = true;
+		} catch (FdlException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	void then() {
+		log.debug("result={}", result);
+
+		assertEquals(result, true);
+	}
+
+}
