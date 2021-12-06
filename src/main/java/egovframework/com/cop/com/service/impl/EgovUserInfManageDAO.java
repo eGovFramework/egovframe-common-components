3f1cc4ed98d4c62a098f65585410e8c3ae4e0929
이백행 <dlqorgod@naver.com>
2021-12-06 20:13:20 +0900

    클럽 운영자 조회(멀티건) 카운트 단위테스트

--- src/main/java/egovframework/com/cop/com/service/impl/EgovUserInfManageDAO.java
+++ src/main/java/egovframework/com/cop/com/service/impl/EgovUserInfManageDAO.java
@@ -136,12 +136,12 @@ public class EgovUserInfManageDAO extends EgovComAbstractDAO {
 	 * 동호회 운영자 목록에 대한 전체 건수를 조회한다.
 	 *
 	 * @param userVO
-	 * @return
+	 * @return int clubOprtrsCnt = egovUserInfManageDAO.selectClubOprtrListCnt(userVO);
 	 * @throws Exception
 	 */
 	@Deprecated
 	public int selectClubOprtrListCnt(UserInfVO userVO) throws Exception {
-		return (Integer) selectOne("EgovUserInfManageDAO.selectClubOprtrListCnt", userVO);
+		return selectOne("EgovUserInfManageDAO.selectClubOprtrListCnt", userVO);
 	}
 
 	/**
