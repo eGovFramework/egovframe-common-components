26261e9f1401065e85056659caf979a004570a45
Byeongheon <x1corea@gmail.com>
2022-05-16 18:18:30 +0900

    EgovPageLink.do 화이트리스트 처리방식 개선 - 기존 : context-whitelist.xml에 정의된 url과 완전일치되어야 함. - 변경 : url 패턴 매칭방식으로 변경 (사용법은 String.matches() 참고)   > whitelist 등록 예시   url에 include가 포함된 경우 : <value>.*include/.*</value>   url로 시작하는 경우 : <value>utl/sys/wsi/EgovWebStandardInspectionUri.*</value>   url값과 일치하는 경우 : <value>/egovframework/com/main_bottom</value>

--- src/main/java/egovframework/com/cmm/web/EgovComUtlController.java
+++ src/main/java/egovframework/com/cmm/web/EgovComUtlController.java
@@ -50,26 +50,36 @@ public class EgovComUtlController {
 	 */
 	@RequestMapping(value="/EgovPageLink.do")
 	public String moveToPage(@RequestParam("link") String linkPage){
-		String link = linkPage;
-		link = link.replace(";", "");
-		link = link.replace(".", "");
 		
 		// service 사용하여 리턴할 결과값 처리하는 부분은 생략하고 단순 페이지 링크만 처리함
-		if (linkPage==null || linkPage.equals("")){
-			link="egovframework/com/cmm/egovError";
+		if (linkPage == null || linkPage.equals(""))
+		{
+			return "egovframework/com/cmm/egovError";
+		}
+		else
+		{
+			// 화이트 리스트 처리
+			// whitelist목록에 있는 경우 결과가 true, 결과가 false인경우 FAIL처리
+			boolean isAuth = false;
+			
+			for (String urlPattern : egovWhitelist)
+			{
+				if (linkPage.matches(urlPattern))
+				{
+					isAuth = true;
+					break;
+				}
+			}
+			
+			if (!isAuth) {
+				LOGGER.debug("Page Link WhiteList Error! Please check whitelist!");
+				linkPage = "egovframework/com/cmm/egovError";
+			}
+			
+			// 안전한 경로 문자열로 조치
+			return EgovWebUtil.filePathBlackList(linkPage);
 		}
 		
-		// 화이트 리스트 처리
-		// whitelist목록에 있는 경우 결과가 true, 결과가 false인경우 FAIL처리
-		if (egovWhitelist.contains(linkPage) == false) {
-			LOGGER.debug("Page Link WhiteList Error! Please check whitelist!");
-			link="egovframework/com/cmm/egovError";
-		}
-		
-		// 안전한 경로 문자열로 조치
-		link = EgovWebUtil.filePathBlackList(link);
-		
-		return link;
 	}
 	
     /**
