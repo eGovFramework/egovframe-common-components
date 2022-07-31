d15ff7baba9e2a3087bb0645bd44c6ece0f8e850
DavidEugen <platonicojju@gmail.com>
2022-07-31 23:06:12 +0900

    #73 Fix: 표준프레임워크 컨텍스트 식별 값 변경
    spring에서 사용하는 시스템 프로퍼티와 (배포 환경: dev, test, prod) 같은 값을 사용하여 혼동의 우려가 있으므로 표준프레임워크의 의도했던 사용 목적을 확인 한 후 변경 필요해 보임.

--- src/main/java/egovframework/com/cmm/context/EgovWebServletContextListener.java
+++ src/main/java/egovframework/com/cmm/context/EgovWebServletContextListener.java
@@ -11,7 +11,7 @@ import egovframework.com.cmm.service.EgovProperties;
 /**
  * EgovWebServletContextListener 클래스
  * <Notice>
- * 	    데이터베이스 설정을 spring.profiles.active 방식으로 처리
+ * 	    데이터베이스 설정을 egov.profiles.active 방식으로 처리
  * 		(공통컴포넌트 특성상 데이터베이스별 분리/개발,검증,운영서버로 분리 가능)
  * <Disclaimer>
  *		N/A
@@ -40,23 +40,23 @@ public class EgovWebServletContextListener implements ServletContextListener {
 
     @Override
 	public void contextInitialized(ServletContextEvent event){
-    	if(System.getProperty("spring.profiles.active") == null){
+    	if(System.getProperty("egov.profiles.active") == null){
     		setEgovProfileSetting();
     	}
     }
 
     @Override
 	public void contextDestroyed(ServletContextEvent event) {
-    	if(System.getProperty("spring.profiles.active") != null){
-    		System.clearProperty("spring.profiles.active");
+    	if(System.getProperty("egov.profiles.active") != null){
+    		System.clearProperty("egov.profiles.active");
     	}
     }
 
     public void setEgovProfileSetting(){
         try {
             LOGGER.debug("===========================Start EgovServletContextLoad START ===========");
-            System.setProperty("spring.profiles.active", EgovProperties.getProperty("Globals.DbType")+","+EgovProperties.getProperty("Globals.Auth"));
-            LOGGER.debug("Setting spring.profiles.active>"+System.getProperty("spring.profiles.active"));
+            System.setProperty("egov.profiles.active", EgovProperties.getProperty("Globals.DbType")+","+EgovProperties.getProperty("Globals.Auth"));
+            LOGGER.debug("Setting egov.profiles.active>"+System.getProperty("egov.profiles.active"));
             LOGGER.debug("===========================END   EgovServletContextLoad END ===========");
         //2017.03.03 	조성원 	시큐어코딩(ES)-오류 메시지를 통한 정보노출[CWE-209]
         } catch(IllegalArgumentException e) {
