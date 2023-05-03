44ead38044d54a79c9da581aa46f9b9aeadfdf5b
이백행 <bhlee@gsitm.com>
2023-05-04 07:08:50 +0900

    TestBean.java 컨텍스트 닫음/LOGGER 사용/주석 제거

--- src/test/java/egovframework/com/bean/TestBean.java
+++ src/test/java/egovframework/com/bean/TestBean.java
@@ -7,43 +7,45 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * XML 기반 빈설정에서 SpEL 표현식 사용하여 메소드 호출
+ * 
  * @author 표준프레임워크 신용호
  * @since 2019.01.03
  * @version 4.0
  * @see
- * <pre>
+ * 
+ *      <pre>
  *
  *  수정일              수정자          수정내용
  *  ----------  --------  ---------------------------
  *  2022.08.29  신용호          최초 생성
  *
- * </pre>
+ *      </pre>
  */
 
 public class TestBean {
 
-    /** 로그설정 */
+	/** 로그설정 */
 	private static final Logger LOGGER = LoggerFactory.getLogger(TestBean.class);
 	private static MyBean myBean;
 	private static MyBean myBean1;
-	
+
 	public static void main(String[] args) {
-    	
-    	ApplicationContext context = new ClassPathXmlApplicationContext("classpath:egovframework/spring/com/test-context-datetime.xml");
-    	//EgovEnvCryptoService cryptoService = (EgovEnvCryptoService)factory.getBean("egovEnvCryptoService");
-    	myBean = context.getBean(MyBean.class);
-    	System.out.println(myBean.getCurrentTimeMills());
-    	
-    	try {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"classpath:egovframework/spring/com/test-context-datetime.xml");
+
+		myBean = context.getBean(MyBean.class);
+		LOGGER.debug("currentTimeMills={}", myBean.getCurrentTimeMills());
+
+		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOGGER.error("InterruptedException");
 		}
-    	
-    	myBean1 = context.getBean(MyBean.class);
-    	System.out.println(myBean1.getCurrentTimeMills());
 
+		myBean1 = context.getBean(MyBean.class);
+		LOGGER.debug("currentTimeMills={}", myBean1.getCurrentTimeMills());
+
+		((ClassPathXmlApplicationContext) context).close();
 	}
-	
+
 }
