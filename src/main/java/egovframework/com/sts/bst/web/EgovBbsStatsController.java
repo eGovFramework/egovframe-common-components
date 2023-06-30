cc4b45b98e17391e679a9f4c0bdd29c1042c615a
이백행 <bhlee@gsitm.com>
2023-07-01 07:50:13 +0900

    검색 후 기간 값설정 에러

--- src/main/java/egovframework/com/sts/bst/web/EgovBbsStatsController.java
+++ src/main/java/egovframework/com/sts/bst/web/EgovBbsStatsController.java
@@ -1,5 +1,7 @@
 package egovframework.com.sts.bst.web;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import egovframework.com.cmm.ComDefaultCodeVO;
@@ -11,6 +13,7 @@ import egovframework.com.sts.com.StatsVO;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.validator.GenericValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -183,6 +186,12 @@ public class EgovBbsStatsController {
 				model.addAttribute("bbsMaxNtcrList", bbsMaxNtcrList);
 				model.addAttribute("statsInfo", statsVO);
 			}
+            if (GenericValidator.isDate(statsVO.getFromDate(), "yyyyMMdd", true)) {
+                model.addAttribute("fDate", (LocalDate.parse(statsVO.getFromDate(), DateTimeFormatter.BASIC_ISO_DATE).format(DateTimeFormatter.ISO_LOCAL_DATE)));
+            }
+            if (GenericValidator.isDate(statsVO.getToDate(), "yyyyMMdd", true)) {
+                model.addAttribute("tDate", (LocalDate.parse(statsVO.getToDate(), DateTimeFormatter.BASIC_ISO_DATE).format(DateTimeFormatter.ISO_LOCAL_DATE)));
+            }
 		} else {
 			statsVO.setTabKind("tab1");
 			model.addAttribute("statsInfo", statsVO);
