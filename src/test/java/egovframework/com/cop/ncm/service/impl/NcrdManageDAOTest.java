09d3632345b418ce258c81d89707b709860ea649
Jurepi <dhan@repia.com>
2023-08-21 16:40:53 +0900

    명함 정보 등록 테스트 추가

---
+++ src/test/java/egovframework/com/cop/ncm/service/impl/NcrdManageDAOTest.java
@@ -0,0 +1,261 @@
+package egovframework.com.cop.ncm.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.egovframe.rte.fdl.idgnr.EgovIdGnrService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.context.ContextConfiguration;
+
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.com.cop.ncm.service.NameCard;
+import egovframework.com.cop.ncm.service.NameCardUser;
+import egovframework.com.test.EgovTestAbstractDAO;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 명함정보를 관리하기 위한 DAO 단위 테스트
+ * @author 주레피
+ *
+ */
+
+@ContextConfiguration(classes = { EgovTestAbstractDAO.class, NcrdManageDAOTest.class, })
+
+@Configuration
+
+@ImportResource({
+    "classpath*:egovframework/spring/com/idgn/context-idgn-Ncrd.xml",
+})
+
+@ComponentScan(
+
+        useDefaultFilters = false,
+
+        basePackages = {
+
+                "egovframework.com.cop.ncm.service.impl"
+
+        },
+
+        includeFilters = {
+
+                @Filter(
+
+                        type = FilterType.ASSIGNABLE_TYPE,
+
+                        classes = {
+
+                                NcrdManageDAO.class
+
+                        }
+
+                )
+
+        }
+
+)
+
+@NoArgsConstructor
+@Slf4j
+// @Commit
+public class NcrdManageDAOTest extends EgovTestAbstractDAO {
+    /**
+     * egovNcrdIdGnrService
+     */
+    @Autowired
+    @Qualifier("egovNcrdIdGnrService")
+    private EgovIdGnrService egovNcrdIdgenService;
+
+    /**
+     * NcrdManageDAO
+     */
+    @Autowired
+    private NcrdManageDAO ncrdManageDAO;
+
+    /**
+     * testNcrd
+     */
+    private NameCard testNameCard;
+
+    /**
+     * testNcrdUser;
+     */
+    private NameCardUser testNameCardUser;
+
+
+    /**
+     * 명함 정보 등록
+     *
+     */
+    private void testNcrdItemAdd(final NameCard nameCard, final LoginVO loginVO) {
+        /*
+         * 로그인 사용자 정보
+         * ESNTL_ID                USER_ID    USER_NM
+         * USRCNFRM_00000000000    TEST1      테스트1
+         */
+        if (loginVO != null) {
+            // 최초등록자ID 설정
+            nameCard.setFrstRegisterId(loginVO.getUniqId());
+            nameCard.setLastUpdusrId(loginVO.getUniqId());
+            nameCard.setNcrdTrgterId(loginVO.getUniqId()); // 명함대상자아이디
+        }
+
+        // 명함 정보 등록
+        ncrdManageDAO.insertNcrdItem(nameCard);
+    }
+
+    /**
+     * 명함사용자 등록
+     *
+     */
+    private void testNameCardUserAdd(final NameCard nameCard, final NameCardUser nameCardUser) {
+        nameCardUser.setNcrdId(nameCard.getNcrdId()); // 명함아이디
+        nameCardUser.setEmplyrId(nameCard.getFrstRegisterId()); // 사용자아이디
+        nameCardUser.setRegistSeCode("REGC04"); // 등록구분코드
+        nameCardUser.setUseAt("Y"); // 사용여부
+
+        ncrdManageDAO.insertNcrdUseInf(nameCardUser);
+    }
+
+    /**
+     * 테스트 데이터 생성
+     */
+    @Before
+    public void testData() {
+        /*
+         * 테스트 명함 정보 생성
+         */
+        testNameCard = new NameCard();
+        final LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+
+        testNameCard.setNationNo("82"); // 국가번호
+        testNameCard.setAreaNo("070"); // 지역번호
+        testNameCard.setMiddleTelNo("4448"); // 중간전화번호
+        testNameCard.setEndTelNo("2678"); // 끝전화번호
+        testNameCard.setTelNo(testNameCard.getNationNo() + testNameCard.getAreaNo() + testNameCard.getMiddleTelNo() + testNameCard.getEndTelNo());
+
+        testNameCard.setIdntfcNo("070"); // 식별번호
+        testNameCard.setMiddleMbtlNum("4448"); // 중간휴대폰번호
+        testNameCard.setEndMbtlNum("2678"); // 끝휴대폰번호
+        testNameCard.setMbtlNum(testNameCard.getIdntfcNo() + testNameCard.getMiddleMbtlNum() + testNameCard.getEndMbtlNum());
+
+        testNameCard.setNcrdId("TEST_NCRD_9999999000");
+        /**
+         * egovNcrdIdGnrService 사용
+         */
+        /*
+         * try {
+         *     nameCard.setNcrdId(egovNcrdIdgenService.getNextStringId());
+         * } catch (FdlException e) {
+         *     log.error("FdlException egovNcrdIdGnrService");
+         *     fail("FdlException egovNcrdIdGnrService");
+         * }
+         *
+         */
+
+        testNameCard.setExtrlUserAt("N"); // 외부사용자여부
+        testNameCard.setNcrdNm("테스터"); // 명함이름
+        testNameCard.setCmpnyNm("표준프레임워크센터"); // 회사명
+        testNameCard.setDeptNm("연구개발"); // 부서명
+        testNameCard.setOfcpsNm("대리"); // 직위명
+        testNameCard.setEmailAdres("egovframesupport@gmail.com"); // 이메일주소
+
+        testNcrdItemAdd(testNameCard, loginVO);
+
+        /*
+         * 테스트 명함사용자 생성
+         */
+        testNameCardUser = new NameCardUser();
+
+        testNameCardUserAdd(testNameCard, testNameCardUser);
+    }
+
+    /**
+     * 명함 정보 등록 테스트
+     */
+    @Test
+    public void testInsertNcrdItem() {
+        // given
+        final NameCard nameCard = new NameCard();
+        nameCard.setNcrdId("TEST_NCRD_9999999001");
+        nameCard.setNcrdNm("TEST2");
+        nameCard.setOthbcAt("Y"); // 공개여부
+
+        // when
+        final int result = ncrdManageDAO.insertNcrdItem(nameCard);
+
+        // then
+        assertEquals(egovMessageSource.getMessage("fail.common.insert"), 1, result);
+    }
+
+//    @Test
+//    public void testDeleteNcrdItemUser() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testDeleteNcrdItem() {
+//        fail("Not yet implemented");
+//    }
+//
+//
+//    @Test
+//    public void testInsertNcrdUseInf() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSelectNcrdItem() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSelectNcrdItemList() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSelectNcrdItemListCnt() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSelectNcrdUseInfs() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSelectNcrdUseInfsCnt() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testUpdateNcrdItem() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testUpdateNcrdUseInf() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSelectMyNcrdItemList() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSelectMyNcrdItemListCnt() {
+//        fail("Not yet implemented");
+//    }
+
+}
