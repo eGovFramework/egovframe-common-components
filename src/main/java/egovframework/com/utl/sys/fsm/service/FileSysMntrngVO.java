2de8b4d2acb7603cc45a9653daa9d6742117e718
Lee-KyuHwun <wxyz0904@naver.com>
2023-08-31 11:43:36 +0900

    FileSysMntrngVO 주석추가

--- src/main/java/egovframework/com/utl/sys/fsm/service/FileSysMntrngVO.java
+++ src/main/java/egovframework/com/utl/sys/fsm/service/FileSysMntrngVO.java
@@ -36,67 +36,114 @@ public class FileSysMntrngVO extends FileSysMntrng {
 
     /** 페이지당 레코드 개수 */
     private int recordCountPerPage = 10;
-
+	/**
+	 * 검색 조건을 가져옵니다.
+	 */
 	public String getSearchCnd() {
 		return searchCnd;
 	}
 
+	/**
+	 * 검색 조건을 설정합니다.
+	 */
 	public void setSearchCnd(String searchCnd) {
 		this.searchCnd = searchCnd;
 	}
 
+	/**
+	 * 검색어를 가져옵니다.
+	 */
 	public String getSearchWrd() {
 		return searchWrd;
 	}
 
+	/**
+	 * 검색어를 설정합니다.
+	 */
 	public void setSearchWrd(String searchWrd) {
 		this.searchWrd = searchWrd;
 	}
 
+	/**
+	 * 현재 페이지 인덱스를 가져옵니다.
+	 */
 	public int getPageIndex() {
 		return pageIndex;
 	}
 
+	/**
+	 * 현재 페이지 인덱스를 설정합니다.
+	 */
 	public void setPageIndex(int pageIndex) {
 		this.pageIndex = pageIndex;
 	}
 
+	/**
+	 * 페이지 단위를 가져옵니다.
+	 */
 	public int getPageUnit() {
 		return pageUnit;
 	}
 
+	/**
+	 * 페이지 단위를 설정합니다.
+	 */
 	public void setPageUnit(int pageUnit) {
 		this.pageUnit = pageUnit;
 	}
 
+	/**
+	 * 한 페이지의 최대 크기를 가져옵니다.
+	 */
 	public int getPageSize() {
 		return pageSize;
 	}
 
+	/**
+	 * 한 페이지의 최대 크기를 설정합니다.
+	 */
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
 	}
 
+	/**
+	 * 페이지 범위 내의 첫번째 인덱스를 가져옵니다.
+	 */
 	public int getFirstIndex() {
 		return firstIndex;
 	}
 
+	/**
+	 * 페이지 범위 내의 첫번째 인덱스를 설정합니다.
+	 */
 	public void setFirstIndex(int firstIndex) {
 		this.firstIndex = firstIndex;
 	}
 
+	/**
+	 * 페이지 범위 내의 마지막 인덱스를 가져옵니다.
+	 */
 	public int getLastIndex() {
 		return lastIndex;
 	}
 
+	/**
+	 * 페이지 범위 내의 마지막 인덱스를 설정합니다.
+	 */
 	public void setLastIndex(int lastIndex) {
 		this.lastIndex = lastIndex;
 	}
 
+	/**
+	 * 페이지당 기록(레코드) 수를 가져옵니다.
+	 */
 	public int getRecordCountPerPage() {
 		return recordCountPerPage;
 	}
 
+	/**
+	 * 페이지당 기록(레코드) 수를 설정합니다.
+	 */
 	public void setRecordCountPerPage(int recordCountPerPage) {
 		this.recordCountPerPage = recordCountPerPage;
 	}
