04011f55ec8e90d61208b1489458dec45b3a5f11
Lee-KyuHwun <wxyz0904@naver.com>
2023-08-31 15:28:07 +0900

    NtwrkSvcMntrngLog 주석 추가

--- src/main/java/egovframework/com/utl/sys/nsm/service/NtwrkSvcMntrngLog.java
+++ src/main/java/egovframework/com/utl/sys/nsm/service/NtwrkSvcMntrngLog.java
@@ -59,73 +59,158 @@ public class NtwrkSvcMntrngLog implements Serializable {
 	 * 최종수정시점
 	 */
 	private String lastUpdusrPnttm = "";
-	
+	/**
+	 * 현재 시스템 IP를 반환합니다.
+	 */
 	public String getSysIp() {
 		return sysIp;
 	}
+
+	/**
+	 * 시스템 IP를 설정합니다.
+	 */
 	public void setSysIp(String sysIp) {
 		this.sysIp = sysIp;
 	}
+
+	/**
+	 * 현재 시스템 포트를 반환합니다.
+	 */
 	public String getSysPort() {
 		return sysPort;
 	}
+
+	/**
+	 * 시스템 포트를 설정합니다.
+	 */
 	public void setSysPort(String sysPort) {
 		this.sysPort = sysPort;
 	}
+
+	/**
+	 * 로그 ID를 반환합니다.
+	 */
 	public String getLogId() {
 		return logId;
 	}
+
+	/**
+	 * 로그 ID를 설정합니다.
+	 */
 	public void setLogId(String logId) {
 		this.logId = logId;
 	}
+
+	/**
+	 * 시스템 이름을 반환합니다.
+	 */
 	public String getSysNm() {
 		return sysNm;
 	}
+
+	/**
+	 * 시스템 이름을 설정합니다.
+	 */
 	public void setSysNm(String sysNm) {
 		this.sysNm = sysNm;
 	}
+
+	/**
+	 * 모니터링 상태를 반환합니다.
+	 */
 	public String getMntrngSttus() {
 		return mntrngSttus;
 	}
+
+	/**
+	 * 모니터링 상태를 설정합니다.
+	 */
 	public void setMntrngSttus(String mntrngSttus) {
 		this.mntrngSttus = mntrngSttus;
 	}
+
+	/**
+	 * 로그 정보를 반환합니다.
+	 */
 	public String getLogInfo() {
 		return logInfo;
 	}
+
+	/**
+	 * 로그 정보를 설정합니다.
+	 */
 	public void setLogInfo(String logInfo) {
 		this.logInfo = logInfo;
 	}
+
+	/**
+	 * 생성 날짜를 반환합니다.
+	 */
 	public String getCreatDt() {
 		return creatDt;
 	}
+
+	/**
+	 * 생성 날짜를 설정합니다.
+	 */
 	public void setCreatDt(String creatDt) {
 		this.creatDt = creatDt;
 	}
+
+	/**
+	 * 최초 등록자 ID를 반환합니다.
+	 */
 	public String getFrstRegisterId() {
 		return frstRegisterId;
 	}
+
+	/**
+	 * 최초 등록자 ID를 설정합니다.
+	 */
 	public void setFrstRegisterId(String frstRegisterId) {
 		this.frstRegisterId = frstRegisterId;
 	}
+
+	/**
+	 * 최초 등록 시간을 반환합니다.
+	 */
 	public String getFrstRegisterPnttm() {
 		return frstRegisterPnttm;
 	}
+
+	/**
+	 * 최초 등록 시간을 설정합니다.
+	 */
 	public void setFrstRegisterPnttm(String frstRegisterPnttm) {
 		this.frstRegisterPnttm = frstRegisterPnttm;
 	}
+
+	/**
+	 * 마지막 수정자 ID를 반환합니다.
+	 */
 	public String getLastUpdusrId() {
 		return lastUpdusrId;
 	}
+
+	/**
+	 * 마지막 수정자 ID를 설정합니다.
+	 */
 	public void setLastUpdusrId(String lastUpdusrId) {
 		this.lastUpdusrId = lastUpdusrId;
 	}
+
+	/**
+	 * 마지막 수정 시간을 반환합니다.
+	 */
 	public String getLastUpdusrPnttm() {
 		return lastUpdusrPnttm;
 	}
+
+	/**
+	 * 마지막 수정 시간을 설정합니다.
+	 */
 	public void setLastUpdusrPnttm(String lastUpdusrPnttm) {
 		this.lastUpdusrPnttm = lastUpdusrPnttm;
 	}
-	
-	
+
 }
