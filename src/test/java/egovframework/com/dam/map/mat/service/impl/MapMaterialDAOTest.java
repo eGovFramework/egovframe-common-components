04055a7934d12e3839b9ddd96ecf248dabc26d55
이백행 <dlqorgod@naver.com>
2024-01-09 07:39:17 +0900

    등록된 지식맵(지식유형) 정보를 조회 한다. 테스트: resultMap id="MapMaterialList" 추가

--- src/test/java/egovframework/com/dam/map/mat/service/impl/MapMaterialDAOTest.java
+++ src/test/java/egovframework/com/dam/map/mat/service/impl/MapMaterialDAOTest.java
@@ -3,6 +3,7 @@ package egovframework.com.dam.map.mat.service.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -109,6 +110,7 @@ public class MapMaterialDAOTest extends EgovTestAbstractDAO {
 	private void testDataMapTeam(final MapTeam testDataMapTeam) {
 		// given
 		testDataMapTeam.setOrgnztId("TEST1_" + EgovDateUtil.toString(new Date(), "yyyyMMddHHmmss", null));
+		testDataMapTeam.setOrgnztNm("test 조직명 " + EgovDateUtil.toString(new Date(), "yyyyMMdd", null));
 
 		final LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 		if (loginVO != null) {
@@ -129,10 +131,15 @@ public class MapMaterialDAOTest extends EgovTestAbstractDAO {
 
 	private void testData(final MapMaterial testData, final MapTeam testDataMapTeam) {
 		// given
-		testData.setKnoTypeCd("000");
-//		LocalDateTime now = LocalDateTime.now();
+		testData.setKnoTypeCd("002");
+		final LocalDateTime now = LocalDateTime.now();
 		testData.setOrgnztId(testDataMapTeam.getOrgnztId());
 //		testData.setOrgnztId("test 이백행 조직ID " + now);
+		testData.setSpeId("test 이백행 전문가ID");
+		testData.setKnoTypeNm("test 이백행 지식유형명 " + now);
+//		testData.setClYmd(EgovDateUtil.toString(new Date(), "yyyyMMddHHmmss", null));
+		testData.setClYmd(EgovDateUtil.toString(new Date(), "yyyy-MM-dd", null));
+		testData.setKnoUrl("test 이백행 지식URL " + now);
 
 		final LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 		if (loginVO != null) {
@@ -178,13 +185,43 @@ public class MapMaterialDAOTest extends EgovTestAbstractDAO {
 		for (final MapMaterialVO result : resultList) {
 			if (log.isDebugEnabled()) {
 				log.debug("result={}", result);
-				log.debug("getOrgnztNm={}", result.getOrgnztNm());
-				log.debug("getKnoTypeCd={}", result.getKnoTypeCd());
+				log.debug("getOrgnztNm 조직명={}, {}", testDataMapTeam.getOrgnztNm(), result.getOrgnztNm());
+				log.debug("getKnoTypeCd 지식유형코드={}, {}", testData.getKnoTypeCd(), result.getKnoTypeCd());
+				log.debug("getSpeId 전문가ID={}, {}", testData.getSpeId(), result.getSpeId());
+				log.debug("getKnoTypeNm 지식유형명={}, {}", testData.getKnoTypeNm(), result.getKnoTypeNm());
+				log.debug("getClYmd 분류일={}, {}", testData.getClYmd(), result.getClYmd());
+				log.debug("getKnoUrl 지식URL={}, {}", testData.getKnoUrl(), result.getKnoUrl());
+				log.debug("getFrstRegisterId 최초등록자ID={}, {}", testData.getFrstRegisterId(), result.getFrstRegisterId());
+				log.debug("getFrstRegisterPnttm 최초등록시점={}, {}", testData.getFrstRegisterPnttm(),
+						result.getFrstRegisterPnttm());
+				log.debug("getLastUpdusrId 최종수정자ID={}, {}", testData.getLastUpdusrId(), result.getLastUpdusrId());
+				log.debug("getLastUpdusrPnttm 최종수정시점={}, {}", testData.getLastUpdusrPnttm(),
+						result.getLastUpdusrPnttm());
 			}
 		}
 
 		// then
 		assertTrue(egovMessageSource.getMessage(FAIL_COMMON_SELECT), resultList.size() > -1);
+		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 조직명", testDataMapTeam.getOrgnztNm(),
+				resultList.get(0).getOrgnztNm());
+//		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 지식유형코드", testData.getKnoTypeCd(),
+//				resultList.get(0).getKnoTypeCd());
+		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 전문가ID", testData.getSpeId(),
+				resultList.get(0).getSpeId());
+		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 지식유형명", testData.getKnoTypeNm(),
+				resultList.get(0).getKnoTypeNm());
+		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 분류일", testData.getClYmd(),
+				resultList.get(0).getClYmd());
+		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 지식URL", testData.getKnoUrl(),
+				resultList.get(0).getKnoUrl());
+		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 최초등록자ID", testData.getFrstRegisterId(),
+				resultList.get(0).getFrstRegisterId());
+//		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 최초등록시점", testData.getFrstRegisterPnttm(),
+//				resultList.get(0).getFrstRegisterPnttm());
+		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 최종수정자ID", testData.getLastUpdusrId(),
+				resultList.get(0).getLastUpdusrId());
+		assertEquals(egovMessageSource.getMessage(FAIL_COMMON_SELECT) + " 최종수정시점", testData.getLastUpdusrPnttm(),
+				resultList.get(0).getLastUpdusrPnttm());
 	}
 
 }
