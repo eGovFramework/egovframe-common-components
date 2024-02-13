176562ac1ef43d5c6347926f9aeafa8151adc84f
이백행 <dlqorgod@naver.com>
2024-02-14 07:45:51 +0900

    [버그수정][시큐어코딩][부적절한 예외 처리 (광범위한 예외객체 선언)]130. 사용자통계

--- src/main/java/egovframework/com/sts/ust/web/EgovUserStatsController.java
+++ src/main/java/egovframework/com/sts/ust/web/EgovUserStatsController.java
@@ -2,13 +2,6 @@ package egovframework.com.sts.ust.web;
 
 import java.util.List;
 
-import egovframework.com.cmm.ComDefaultCodeVO;
-import egovframework.com.cmm.annotation.IncludedInfo;
-import egovframework.com.cmm.service.CmmnDetailCode;
-import egovframework.com.cmm.service.EgovCmmUseService;
-import egovframework.com.sts.com.StatsVO;
-import egovframework.com.sts.ust.service.EgovUserStatsService;
-
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
@@ -18,14 +11,22 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import egovframework.com.cmm.ComDefaultCodeVO;
+import egovframework.com.cmm.annotation.IncludedInfo;
+import egovframework.com.cmm.service.CmmnDetailCode;
+import egovframework.com.cmm.service.EgovCmmUseService;
+import egovframework.com.sts.com.StatsVO;
+import egovframework.com.sts.ust.service.EgovUserStatsService;
+
 /**
  * 사용자 통계 검색 컨트롤러 클래스
+ * 
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.03.19
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일     수정자          수정내용
@@ -33,40 +34,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *  2009.03.19  박지욱          최초 생성
  *  2011.06.30  이기하          패키지 분리(sts -> sts.sst)
  *  2011.8.26	정진오			IncludedInfo annotation 추가
+ *  2024.02.14  이백행          보안약점 조치: 부적절한 예외 처리 (광범위한 예외객체 선언)
  *
- *  </pre>
+ *      </pre>
  */
-
 
 @Controller
 public class EgovUserStatsController {
 
 	/** EgovUserStatsService */
 	@Resource(name = "userStatsService")
-    private EgovUserStatsService userStatsService;
+	private EgovUserStatsService userStatsService;
 
 	/** EgovCmmUseService */
-	@Resource(name="EgovCmmUseService")
+	@Resource(name = "EgovCmmUseService")
 	private EgovCmmUseService cmmUseService;
 
-    /** log */
+	/** log */
 	private static final Logger LOGGER = LoggerFactory.getLogger(EgovUserStatsController.class);
 
-    /**
+	/**
 	 * 사용자 통계를 조회한다
+	 * 
 	 * @param statsVO StatsVO
 	 * @return String
 	 * @exception Exception
 	 */
-    @IncludedInfo(name="사용자통계", listUrl="/sts/ust/selectUserStats.do", order = 130 ,gid = 30)
-    @RequestMapping(value="/sts/ust/selectUserStats.do")
-	public String selectUserStats(@ModelAttribute("statsVO") StatsVO statsVO,
-			ModelMap model) throws Exception {
+	@IncludedInfo(name = "사용자통계", listUrl = "/sts/ust/selectUserStats.do", order = 130, gid = 30)
+	@RequestMapping(value = "/sts/ust/selectUserStats.do")
+	public String selectUserStats(@ModelAttribute("statsVO") StatsVO statsVO, ModelMap model) {
 
-    	// 세부통계구분 공통코드 목록 조회(회원유형,상태,성별에 대한 세부통계구분코드)
-    	ComDefaultCodeVO vo = new ComDefaultCodeVO();
+		// 세부통계구분 공통코드 목록 조회(회원유형,상태,성별에 대한 세부통계구분코드)
+		ComDefaultCodeVO vo = new ComDefaultCodeVO();
 
-    	vo.setCodeId("COM012");
+		vo.setCodeId("COM012");
 		List<CmmnDetailCode> code012 = cmmUseService.selectCmmCodeDetail(vo);
 		vo.setCodeId("COM013");
 		List<CmmnDetailCode> code013 = cmmUseService.selectCmmCodeDetail(vo);
@@ -84,7 +85,7 @@ public class EgovUserStatsController {
 			// 그래프에 표시될 이미지 길이를 결정한다.
 			float iMaxUnit = 50.0f;
 			for (int i = 0; i < userStats.size(); i++) {
-				StatsVO sVo = (StatsVO)userStats.get(i);
+				StatsVO sVo = userStats.get(i);
 				int iCnt = sVo.getStatsCo();
 				if (iCnt > 10 && iCnt <= 100) {
 					if (iMaxUnit > 5.0f) {
@@ -105,6 +106,6 @@ public class EgovUserStatsController {
 			model.addAttribute("userStats", userStats);
 			model.addAttribute("statsInfo", statsVO);
 		}
-        return "egovframework/com/sts/ust/EgovUserStats";
+		return "egovframework/com/sts/ust/EgovUserStats";
 	}
 }
