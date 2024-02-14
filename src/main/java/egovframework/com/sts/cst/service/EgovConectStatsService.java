dbaca72e7d180b235b11da791687f7c07dc31821
이백행 <dlqorgod@naver.com>
2024-02-15 07:27:37 +0900

    [버그수정][시큐어코딩][부적절한 예외 처리 (광범위한 예외객체 선언)]140. 접속통계

--- src/main/java/egovframework/com/sts/cst/service/EgovConectStatsService.java
+++ src/main/java/egovframework/com/sts/cst/service/EgovConectStatsService.java
@@ -6,28 +6,31 @@ import egovframework.com.sts.com.StatsVO;
 
 /**
  * 접속 통계 검색 비즈니스 인터페이스 클래스
+ * 
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.03.12
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자          수정내용
  *  -------    --------    ---------------------------
  *  2009.03.19  박지욱          최초 생성
  *  2011.06.30  이기하          패키지 분리(sts -> sts.cst)
+ *  2024.02.15  이백행          보안약점 조치: 부적절한 예외 처리 (광범위한 예외객체 선언)
  *
- *  </pre>
+ *      </pre>
  */
 public interface EgovConectStatsService {
 
 	/**
 	 * 접속 통계를 조회한다
+	 * 
 	 * @param vo StatsVO
 	 * @return List
 	 * @exception Exception
 	 */
-	List<StatsVO> selectConectStats(StatsVO vo) throws Exception;
+	List<StatsVO> selectConectStats(StatsVO vo);
 }
--- src/main/java/egovframework/com/sts/cst/web/EgovConectStatsController.java
+++ src/main/java/egovframework/com/sts/cst/web/EgovConectStatsController.java
@@ -4,10 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import egovframework.com.cmm.annotation.IncludedInfo;
-import egovframework.com.sts.com.StatsVO;
-import egovframework.com.sts.cst.service.EgovConectStatsService;
-
 import javax.annotation.Resource;
 
 import org.apache.commons.validator.GenericValidator;
@@ -16,14 +12,19 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import egovframework.com.cmm.annotation.IncludedInfo;
+import egovframework.com.sts.com.StatsVO;
+import egovframework.com.sts.cst.service.EgovConectStatsService;
+
 /**
  * 접속 통계 검색 컨트롤러 클래스
+ * 
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.03.19
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자          수정내용
@@ -31,32 +32,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *  2009.03.19  박지욱          최초 생성
  *  2011.06.30  이기하          패키지 분리(sts -> sts.cst)
  *  2011.8.26	정진오			IncludedInfo annotation 추가
+ *  2024.02.15  이백행          보안약점 조치: 부적절한 예외 처리 (광범위한 예외객체 선언)
  *
- *  </pre>
+ *      </pre>
  */
-
 
 @Controller
 public class EgovConectStatsController {
 
 	/** EgovConectStatsService */
 	@Resource(name = "conectStatsService")
-    private EgovConectStatsService conectStatsService;
+	private EgovConectStatsService conectStatsService;
 
 	/** EgovCmmUseService */
-/*	@Resource(name="EgovCmmUseService")
-	private EgovCmmUseService cmmUseService;*/
+	/*
+	 * @Resource(name="EgovCmmUseService") private EgovCmmUseService cmmUseService;
+	 */
 
-    /**
+	/**
 	 * 접속 통계를 조회한다
+	 * 
 	 * @param statsVO StatsVO
 	 * @return String
 	 * @exception Exception
 	 */
-    @IncludedInfo(name="접속통계", listUrl="/sts/cst/selectConectStats.do", order = 140 ,gid = 30)
-    @RequestMapping(value="/sts/cst/selectConectStats.do")
-	public String selectUserStats(@ModelAttribute("statsVO") StatsVO statsVO,
-			ModelMap model) throws Exception {
+	@IncludedInfo(name = "접속통계", listUrl = "/sts/cst/selectConectStats.do", order = 140, gid = 30)
+	@RequestMapping(value = "/sts/cst/selectConectStats.do")
+	public String selectUserStats(@ModelAttribute("statsVO") StatsVO statsVO, ModelMap model) {
 
 		if (statsVO.getFromDate() != null && !"".equals(statsVO.getFromDate())) {
 
@@ -66,12 +68,12 @@ public class EgovConectStatsController {
 			if ("SERVICE".equals(statsVO.getStatsKind())) {
 				model.addAttribute("conectStats", conectStats);
 				model.addAttribute("statsInfo", statsVO);
-			// 2. 개인별
+				// 2. 개인별
 			} else {
 				// 그래프에 표시될 이미지 길이를 결정한다.
 				float iMaxUnit = 50.0f;
 				for (int i = 0; i < conectStats.size(); i++) {
-					StatsVO vo = (StatsVO)conectStats.get(i);
+					StatsVO vo = conectStats.get(i);
 					int iCnt = vo.getStatsCo();
 					if (iCnt > 10 && iCnt <= 100) {
 						if (iMaxUnit > 5.0f) {
@@ -91,13 +93,15 @@ public class EgovConectStatsController {
 				model.addAttribute("conectStats", conectStats);
 				model.addAttribute("statsInfo", statsVO);
 			}
-            if (GenericValidator.isDate(statsVO.getFromDate(), "yyyyMMdd", true)) {
-                model.addAttribute("fDate", (LocalDate.parse(statsVO.getFromDate(), DateTimeFormatter.BASIC_ISO_DATE).format(DateTimeFormatter.ISO_LOCAL_DATE)));
-            }
-            if (GenericValidator.isDate(statsVO.getToDate(), "yyyyMMdd", true)) {
-                model.addAttribute("tDate", (LocalDate.parse(statsVO.getToDate(), DateTimeFormatter.BASIC_ISO_DATE).format(DateTimeFormatter.ISO_LOCAL_DATE)));
-            }
+			if (GenericValidator.isDate(statsVO.getFromDate(), "yyyyMMdd", true)) {
+				model.addAttribute("fDate", (LocalDate.parse(statsVO.getFromDate(), DateTimeFormatter.BASIC_ISO_DATE)
+						.format(DateTimeFormatter.ISO_LOCAL_DATE)));
+			}
+			if (GenericValidator.isDate(statsVO.getToDate(), "yyyyMMdd", true)) {
+				model.addAttribute("tDate", (LocalDate.parse(statsVO.getToDate(), DateTimeFormatter.BASIC_ISO_DATE)
+						.format(DateTimeFormatter.ISO_LOCAL_DATE)));
+			}
 		}
-        return "egovframework/com/sts/cst/EgovConectStats";
+		return "egovframework/com/sts/cst/EgovConectStats";
 	}
 }
