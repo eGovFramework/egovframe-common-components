20913ed483b1f71a32dc3ffe959cd356a93973d1
Jurepi <dhan@repia.com>
2024-02-20 14:14:21 +0900

    부적절한 예외처리 제거, 리턴 값 추가

--- src/main/java/egovframework/com/dam/mgm/service/impl/KnoManagementDAO.java
+++ src/main/java/egovframework/com/dam/mgm/service/impl/KnoManagementDAO.java
@@ -1,13 +1,11 @@
 package egovframework.com.dam.mgm.service.impl;
 
 import java.util.List;
-
+import org.egovframe.rte.psl.dataaccess.util.EgovMap;
+import org.springframework.stereotype.Repository;
 import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
 import egovframework.com.dam.mgm.service.KnoManagement;
 import egovframework.com.dam.mgm.service.KnoManagementVO;
-
-import org.egovframe.rte.psl.dataaccess.util.EgovMap;
-import org.springframework.stereotype.Repository;
 
 /**
  * 개요
@@ -24,67 +22,66 @@ import org.springframework.stereotype.Repository;
 @Repository("KnoManagementDAO")
 public class KnoManagementDAO extends EgovComAbstractDAO {
 
-	/**
-	 * 등록된 지식정보 정보를 조회 한다.
-	 * @param KnoManagementVO - 지식정보 VO
-	 * @return String - 지식정보 VO
-	 *
-	 * @param KnoManagementVO
-	 */
-	public List<EgovMap> selectKnoManagementList(KnoManagementVO searchVO) throws Exception {
-		return selectList("KnoManagementDAO.selectKnoManagementList", searchVO);
-	}
+    /**
+     * 등록된 지식정보 정보를 조회 한다.
+     * @param KnoManagementVO - 지식정보 VO
+     * @return String - 지식정보 VO
+     *
+     * @param KnoManagementVO
+     */
+    public List<EgovMap> selectKnoManagementList(KnoManagementVO searchVO) {
+        return selectList("KnoManagementDAO.selectKnoManagementList", searchVO);
+    }
 
-	/**
-	 * 지식정보 목록 총 개수를 조회한다.
-	 * @param KnoManagementVO - 지식정보 Vo
-	 * @return int - 지식정보 토탈 카운트 수
-	 *
-	 * @param KnoManagementVO
-	 */
-	public int selectKnoManagementTotCnt(KnoManagementVO searchVO) throws Exception {
-		return selectOne("KnoManagementDAO.selectKnoManagementTotCnt", searchVO);
-	}
+    /**
+     * 지식정보 목록 총 개수를 조회한다.
+     * @param KnoManagementVO - 지식정보 Vo
+     * @return int - 지식정보 토탈 카운트 수
+     *
+     * @param KnoManagementVO
+     */
+    public int selectKnoManagementTotCnt(KnoManagementVO searchVO) {
+        return selectOne("KnoManagementDAO.selectKnoManagementTotCnt", searchVO);
+    }
 
-	/**
-	 * 지식정보 상세 정보를 조회 한다.
-	 * @param KnoManagementVO - 지식정보 VO
-	 * @return String - 지식정보 VO
-	 *
-	 * @param KnoManagementVO
-	 */
-	public KnoManagement selectKnoManagement(KnoManagement knoManagement) throws Exception {
-		return selectOne("KnoManagementDAO.selectKnoManagement", knoManagement);
-	}
+    /**
+     * 지식정보 상세 정보를 조회 한다.
+     * @param KnoManagementVO - 지식정보 VO
+     * @return String - 지식정보 VO
+     *
+     * @param KnoManagementVO
+     */
+    public KnoManagement selectKnoManagement(KnoManagement knoManagement) {
+        return selectOne("KnoManagementDAO.selectKnoManagement", knoManagement);
+    }
 
-	/**
-	 * 지식정보 정보를 신규로 등록한다.
-	 * @param KnoNm - 지식정보 model
-	 *
-	 * @param knoNm
-	 */
-	public void insertKnoManagement(KnoManagement knoManagement) throws Exception {
-		insert("KnoManagementDAO.insertKnoManagement", knoManagement);
-	}
+    /**
+     * 지식정보 정보를 신규로 등록한다.
+     * @param KnoNm - 지식정보 model
+     *
+     * @param knoNm
+     */
+    public int insertKnoManagement(KnoManagement knoManagement) {
+        return insert("KnoManagementDAO.insertKnoManagement", knoManagement);
+    }
 
-	/**
-	 * 기 등록 된 지식정보 정보를 수정 한다.
-	 * @param ManagementKnoNm - 지식정보 model
-	 *
-	 * @param knoNm
-	 */
-	public void updateKnoManagement(KnoManagement knoManagement) throws Exception {
-		update("KnoManagementDAO.updateKnoManagement", knoManagement);
-	}
+    /**
+     * 기 등록 된 지식정보 정보를 수정 한다.
+     * @param ManagementKnoNm - 지식정보 model
+     *
+     * @param knoNm
+     */
+    public int updateKnoManagement(KnoManagement knoManagement) {
+        return update("KnoManagementDAO.updateKnoManagement", knoManagement);
+    }
 
-	/**
-	 * 기 등록된 지식정보 정보를 삭제한다.
-	 * @param ManagementKnoNm - 지식정보 model
-	 *
-	 * @param knoNm
-	 */
-	public void deleteKnoManagement(KnoManagement knoManagement) throws Exception {
-		delete("KnoManagementDAO.deleteKnoManagement", knoManagement);
-	}
-
+    /**
+     * 기 등록된 지식정보 정보를 삭제한다.
+     * @param ManagementKnoNm - 지식정보 model
+     *
+     * @param knoNm
+     */
+    public int deleteKnoManagement(KnoManagement knoManagement) {
+        return delete("KnoManagementDAO.deleteKnoManagement", knoManagement);
+    }
 }
