7b32d7409c96f75a442a30cdfb0cb2537fb41cf4
이백행 <dlqorgod@naver.com>
2024-07-29 07:54:15 +0900

    SndngMailRegistDAO Format source code/Organize imports/Additional actions

--- src/main/java/egovframework/com/cop/ems/service/impl/SndngMailRegistDAO.java
+++ src/main/java/egovframework/com/cop/ems/service/impl/SndngMailRegistDAO.java
@@ -2,44 +2,47 @@ package egovframework.com.cop.ems.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
 import egovframework.com.cop.ems.service.AtchmnFileVO;
 import egovframework.com.cop.ems.service.SndngMailVO;
 
-import org.springframework.stereotype.Repository;
-
 /**
  * 발송메일을 등록하는 DAO 클래스
+ * 
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.03.12
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자          수정내용
  *  -------    --------    ---------------------------
  *  2009.03.12  박지욱          최초 생성
  *
- *  </pre>
+ *      </pre>
  */
 @Repository("sndngMailRegistDAO")
 public class SndngMailRegistDAO extends EgovComAbstractDAO {
 
 	/**
 	 * 발송할 메일을 등록한다
+	 * 
 	 * @param vo SndngMailVO
 	 * @return SndngMailVO
 	 * @exception Exception
 	 */
 	public SndngMailVO insertSndngMail(SndngMailVO vo) throws Exception {
 		insert("sndngMailRegistDAO.insertSndngMail", vo);
-		return new SndngMailVO() ;
+		return new SndngMailVO();
 	}
 
 	/**
 	 * 발송할 메일에 있는 첨부파일 목록을 조회한다.
+	 * 
 	 * @param vo SndngMailVO
 	 * @return List
 	 * @exception Exception
@@ -50,6 +53,7 @@ public class SndngMailRegistDAO extends EgovComAbstractDAO {
 
 	/**
 	 * 발송결과를 수정한다.
+	 * 
 	 * @param vo SndngMailVO
 	 * @return SndngMailVO
 	 * @exception Exception
