6110ef80fffb6b6051fd38ed96456366b901c854
이백행 <dlqorgod@naver.com>
2024-07-29 07:50:52 +0900

    시큐어코딩 Exception 제거

--- src/main/java/egovframework/com/cmm/service/EgovFileMngService.java
+++ src/main/java/egovframework/com/cmm/service/EgovFileMngService.java
@@ -12,6 +12,7 @@ import java.util.Map;
  *    수정일       수정자         수정내용
  *    -------        -------     -------------------
  *    2009. 3. 25.     이삼섭    최초생성
+ *   2024.07.29  이백행          시큐어코딩 Exception 제거
  *               </pre>
  * 
  * @author 공통 서비스 개발팀 이삼섭
@@ -27,91 +28,80 @@ public interface EgovFileMngService {
 	 *
 	 * @param fvo
 	 * @return
-	 * @throws Exception
 	 */
-	public List<FileVO> selectFileInfs(FileVO fvo) throws Exception;
+	public List<FileVO> selectFileInfs(FileVO fvo);
 
 	/**
 	 * 하나의 파일에 대한 정보(속성 및 상세)를 등록한다.
 	 *
 	 * @param fvo
-	 * @throws Exception
 	 */
-	public String insertFileInf(FileVO fvo) throws Exception;
+	public String insertFileInf(FileVO fvo);
 
 	/**
 	 * 여러 개의 파일에 대한 정보(속성 및 상세)를 등록한다.
 	 *
 	 * @param fvoList
-	 * @throws Exception
 	 */
-	public String insertFileInfs(List<FileVO> fvoList) throws Exception;
+	public String insertFileInfs(List<FileVO> fvoList);
 
 	/**
 	 * 여러 개의 파일에 대한 정보(속성 및 상세)를 수정한다.
 	 *
 	 * @param fvoList
-	 * @throws Exception
 	 */
-	public void updateFileInfs(List<FileVO> fvoList) throws Exception;
+	public void updateFileInfs(List<FileVO> fvoList);
 
 	/**
 	 * 여러 개의 파일을 삭제한다.
 	 *
 	 * @param fvoList
-	 * @throws Exception
 	 */
-	public void deleteFileInfs(List<FileVO> fvoList) throws Exception;
+	public void deleteFileInfs(List<FileVO> fvoList);
 
 	/**
 	 * 하나의 파일을 삭제한다.
 	 *
 	 * @param fvo
-	 * @throws Exception
 	 */
-	public void deleteFileInf(FileVO fvo) throws Exception;
+	public void deleteFileInf(FileVO fvo);
 
 	/**
 	 * 파일에 대한 상세정보를 조회한다.
 	 *
 	 * @param fvo
 	 * @return
-	 * @throws Exception
 	 */
-	public FileVO selectFileInf(FileVO fvo) throws Exception;
+	public FileVO selectFileInf(FileVO fvo);
 
 	/**
 	 * 파일 구분자에 대한 최대값을 구한다.
 	 *
 	 * @param fvo
 	 * @return
-	 * @throws Exception
 	 */
-	public int getMaxFileSN(FileVO fvo) throws Exception;
+	public int getMaxFileSN(FileVO fvo);
 
 	/**
 	 * 전체 파일을 삭제한다.
 	 *
 	 * @param fvo
-	 * @throws Exception
 	 */
-	public void deleteAllFileInf(FileVO fvo) throws Exception;
+	public void deleteAllFileInf(FileVO fvo);
 
 	/**
 	 * 파일명 검색에 대한 목록을 조회한다.
 	 *
 	 * @param fvo
 	 * @return
-	 * @throws Exception
 	 */
-	public Map<String, Object> selectFileListByFileNm(FileVO fvo) throws Exception;
+	public Map<String, Object> selectFileListByFileNm(FileVO fvo);
 
 	/**
 	 * 이미지 파일에 대한 목록을 조회한다.
 	 *
 	 * @param vo
 	 * @return
-	 * @throws Exception
 	 */
-	public List<FileVO> selectImageFileList(FileVO vo) throws Exception;
+	public List<FileVO> selectImageFileList(FileVO vo);
 }
