99956cdcf95fd30fc0da8e0e1efe62b3a7f6fa8d
이백행 <dlqorgod@naver.com>
2024-07-29 07:56:25 +0900

    EgovSndngMailServiceImpl Format source code/Organize imports/Additional actions

--- src/main/java/egovframework/com/cop/ems/service/impl/EgovSndngMailServiceImpl.java
+++ src/main/java/egovframework/com/cop/ems/service/impl/EgovSndngMailServiceImpl.java
@@ -1,14 +1,9 @@
 package egovframework.com.cop.ems.service.impl;
 
-import egovframework.com.cop.ems.service.EgovMultiPartEmail;
-import egovframework.com.cop.ems.service.EgovSndngMailService;
-import egovframework.com.cop.ems.service.SndngMailVO;
-
-import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
-
 import javax.annotation.Resource;
 
 import org.apache.commons.mail.EmailAttachment;
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.MailAuthenticationException;
@@ -16,13 +11,18 @@ import org.springframework.mail.MailParseException;
 import org.springframework.mail.MailSendException;
 import org.springframework.stereotype.Service;
 
+import egovframework.com.cop.ems.service.EgovMultiPartEmail;
+import egovframework.com.cop.ems.service.EgovSndngMailService;
+import egovframework.com.cop.ems.service.SndngMailVO;
+
 /**
  * 메일 솔루션과 연동해서 이용해서 메일을 보내는 서비스 구현 클래스
+ * 
  * @since 2011.09.09
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자          수정내용
@@ -31,7 +31,7 @@ import org.springframework.stereotype.Service;
  *  2011.12.06  이기하       메일 첨부파일이 기능 추가
  *  2013.05.23  이기하       메일 첨부파일이 없을 때 로직 추가
  *
- *  </pre>
+ *      </pre>
  */
 @Service("egovSndngMailService")
 public class EgovSndngMailServiceImpl extends EgovAbstractServiceImpl implements EgovSndngMailService {
@@ -47,6 +47,7 @@ public class EgovSndngMailServiceImpl extends EgovAbstractServiceImpl implements
 
 	/**
 	 * 메일을 발송한다
+	 * 
 	 * @param vo SndngMailVO
 	 * @return boolean
 	 * @exception Exception
@@ -69,14 +70,13 @@ public class EgovSndngMailServiceImpl extends EgovAbstractServiceImpl implements
 				attachment.setPath(atchmnFilePath);
 				attachment.setDisposition(EmailAttachment.ATTACHMENT);
 				attachment.setDescription("첨부파일입니다");
-				//attachment.setName(new String(atchmnFileNm.getBytes("UTF-8"),"latin1")); // 구버전의 경우 필요
+				// attachment.setName(new String(atchmnFileNm.getBytes("UTF-8"),"latin1")); //
+				// 구버전의 경우 필요
 				attachment.setName(atchmnFileNm);
 
-				// 2015.05.08 주석수정 - 첨부파일 정보를 포함한 메일을 전송합니다 
+				// 2015.05.08 주석수정 - 첨부파일 정보를 포함한 메일을 전송합니다
 				egovMultiPartEmail.send(recptnPerson, subject, emailCn, attachment);
-			}
-			else
-			{
+			} else {
 				// 메일을 전송합니다
 				egovMultiPartEmail.send(recptnPerson, subject, emailCn);
 			}
