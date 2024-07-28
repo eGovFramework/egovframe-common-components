c11a8a898fee521db69a383b74c9b1c9ce03ef12
이백행 <dlqorgod@naver.com>
2024-07-29 07:46:26 +0900

    EgovSndngMailRegistService Format source code/Organize imports/Additional actions

--- src/main/java/egovframework/com/cop/ems/service/EgovSndngMailRegistService.java
+++ src/main/java/egovframework/com/cop/ems/service/EgovSndngMailRegistService.java
@@ -2,24 +2,26 @@ package egovframework.com.cop.ems.service;
 
 /**
  * 발송메일등록, 발송요청XML파일 생성하는 비즈니스 인터페이스 클래스
+ * 
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.03.12
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자          수정내용
  *  -------    --------    ---------------------------
  *  2009.03.12  박지욱          최초 생성
  *
- *  </pre>
+ *      </pre>
  */
 public interface EgovSndngMailRegistService {
 
 	/**
 	 * 발송할 메일을 등록한다
+	 * 
 	 * @param vo SndngMailVO
 	 * @return boolean
 	 * @exception Exception
@@ -28,6 +30,7 @@ public interface EgovSndngMailRegistService {
 
 	/**
 	 * 발송할 메일을 XML파일로 만들어 저장한다.
+	 * 
 	 * @param vo SndngMailVO
 	 * @return boolean
 	 * @exception Exception
@@ -36,6 +39,7 @@ public interface EgovSndngMailRegistService {
 
 	/**
 	 * 발송메일 발송결과 XML파일을 읽어 발송결과코드에 수정한다.
+	 * 
 	 * @param xml String
 	 * @return boolean
 	 * @exception Exception
