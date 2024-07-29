352b68da9433e792794ba0b81ac4432dae12aa33
이백행 <dlqorgod@naver.com>
2024-07-30 07:14:30 +0900

    AtLeastOneConstructor 적어도 하나의 생성자

--- src/test/java/egovframework/com/uat/uia/web/TestLoginUsrViewEgovLoginControllerSelenium.java
+++ src/test/java/egovframework/com/uat/uia/web/TestLoginUsrViewEgovLoginControllerSelenium.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class TestLoginUsrViewEgovLoginControllerSelenium {
 
 	WebDriver driver;
