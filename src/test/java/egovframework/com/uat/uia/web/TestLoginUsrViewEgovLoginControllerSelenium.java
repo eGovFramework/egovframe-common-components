b522a9aa7d3327e478d9360b37dd670d99a9e05f
이백행 <dlqorgod@naver.com>
2024-07-30 07:20:39 +0900

    DoNotUseThreads 스레드를 사용하지 마십시오

--- src/test/java/egovframework/com/uat/uia/web/TestLoginUsrViewEgovLoginControllerSelenium.java
+++ src/test/java/egovframework/com/uat/uia/web/TestLoginUsrViewEgovLoginControllerSelenium.java
@@ -2,6 +2,7 @@ package egovframework.com.uat.uia.web;
 
 import static org.junit.Assert.assertEquals;
 
+import org.egovframe.rte.fdl.cmmn.exception.BaseRuntimeException;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -11,6 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 10. 로그인 셀레늄 단위 테스트
@@ -20,6 +22,7 @@ import lombok.NoArgsConstructor;
  *
  */
 @NoArgsConstructor
+@Slf4j
 public class TestLoginUsrViewEgovLoginControllerSelenium {
 
 	/**
@@ -44,28 +47,28 @@ public class TestLoginUsrViewEgovLoginControllerSelenium {
 	public void test() throws InterruptedException {
 		// 로그인 화면 이동
 		driver.get("http://localhost:8080/egovframework-all-in-one/uat/uia/egovLoginUsr.do");
-		Thread.sleep(3000);
+		sleep();
 
 		JavascriptExecutor executor = (JavascriptExecutor) driver;
 
 		// 새로고침
 		executor.executeScript("location.reload();");
-		Thread.sleep(3000);
+		sleep();
 
 		// 업무 탭 클릭
 		WebElement typeUsr = driver.findElement(By.id("typeUsr"));
 		typeUsr.click();
-		Thread.sleep(3000);
+		sleep();
 
 		// 아이디 입력
 		WebElement id = driver.findElement(By.id("id"));
 		id.sendKeys("TEST1");
-		Thread.sleep(3000);
+		sleep();
 
 		// 비밀번호 입력
 		WebElement password = driver.findElement(By.id("password"));
 		password.sendKeys("rhdxhd12");
-		Thread.sleep(3000);
+		sleep();
 
 		// 로그인 버튼 클릭
 		executor.executeScript("actionLogin();");
@@ -74,6 +77,17 @@ public class TestLoginUsrViewEgovLoginControllerSelenium {
 		WebElement a = driver.findElement(By.tagName("a"));
 		String aString = a.getText();
 		assertEquals("", "로그아웃", aString);
+	}
+
+	private void sleep() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			if (log.isErrorEnabled()) {
+				log.error("InterruptedException sleep");
+			}
+			throw new BaseRuntimeException("InterruptedException sleep", e);
+		}
 	}
 
 }
