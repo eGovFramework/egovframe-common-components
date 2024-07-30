396b1c49c7525771de77d560ee7ada84aaa0ae77
이백행 <dlqorgod@naver.com>
2024-07-30 12:49:20 +0900

    Source > Format

--- src/main/java/egovframework/com/ext/oauth/service/OAuthLogin.java
+++ src/main/java/egovframework/com/ext/oauth/service/OAuthLogin.java
@@ -18,37 +18,37 @@ import com.github.scribejava.core.oauth.OAuth20Service;
 public class OAuthLogin {
 	private OAuth20Service oauthService;
 	private OAuthVO oauthVO;
-	
+
 	private static final ObjectMapper mapper = new ObjectMapper(); // 매 요청마다 객체를 생성하지 않기 위함
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(OAuthLogin.class);
-	
+
 	public OAuthLogin(OAuthVO oauthVO) {
-		this.oauthService = new ServiceBuilder(oauthVO.getClientId())
-				.apiSecret(oauthVO.getClientSecret())
-				.callback(oauthVO.getRedirectUrl())
-				.build(oauthVO.getApi20Instance());
-		
+		this.oauthService = new ServiceBuilder(oauthVO.getClientId()).apiSecret(oauthVO.getClientSecret())
+				.callback(oauthVO.getRedirectUrl()).build(oauthVO.getApi20Instance());
+
 		this.oauthVO = oauthVO;
 	}
-	
+
 	// 230825 카카오톡 scope 변경으로 인한 scope 처리 추가
 	public String getOAuthURL() {
-		if(oauthVO.getOrigin().equals("naver")) { // naver의 경우 state가 필수 조건에 포함
-			return this.oauthService.getAuthorizationUrl()+"&state=test&scope="+oauthVO.getScope();
-		}else { // 필수, 추가 동의 항목 포함
-			return this.oauthService.getAuthorizationUrl()+"&scope="+oauthVO.getScope();
+		if (oauthVO.getOrigin().equals("naver")) { // naver의 경우 state가 필수 조건에 포함
+			return this.oauthService.getAuthorizationUrl() + "&state=test&scope=" + oauthVO.getScope();
+		} else { // 필수, 추가 동의 항목 포함
+			return this.oauthService.getAuthorizationUrl() + "&scope=" + oauthVO.getScope();
 		}
 	}
 
 	public OAuthUniversalUser getUserProfile(String code) throws Exception {
-		//System.out.println("===>>> oauthService.getApiKey() = "+oauthService.getApiKey());
-		//System.out.println("===>>> oauthService.getApiSecret() = "+oauthService.getApiSecret());
+		// System.out.println("===>>> oauthService.getApiKey() =
+		// "+oauthService.getApiKey());
+		// System.out.println("===>>> oauthService.getApiSecret() =
+		// "+oauthService.getApiSecret());
 		OAuth2AccessToken accessToken = oauthService.getAccessToken(code);
-		
+
 		OAuthRequest request = new OAuthRequest(Verb.GET, this.oauthVO.getProfileUrl());
 		oauthService.signRequest(accessToken, request);
-		
+
 		Response response = oauthService.execute(request);
 		return parseJson(response.getBody());
 	}
@@ -56,9 +56,9 @@ public class OAuthLogin {
 	private OAuthUniversalUser parseJson(String body) throws Exception {
 		LOGGER.info("============================\n" + body + "\n==================");
 		OAuthUniversalUser user = new OAuthUniversalUser();
-		
+
 		JsonNode rootNode = mapper.readTree(body);
-		
+
 		if (oauthVO.getOrigin().equals("google")) {
 			user.setServiceName(OAuthConfig.GOOGLE_SERVICE_NAME);
 			user.setUserId(rootNode.get("sub").asText());
@@ -82,10 +82,10 @@ public class OAuthLogin {
 
 		return user;
 	}
-	
+
 	private void getEmails(OAuthUniversalUser user, JsonNode rootNode) {
 		Iterator<JsonNode> iterEmails = rootNode.path("emails").elements();
-		while(iterEmails.hasNext()) {
+		while (iterEmails.hasNext()) {
 			JsonNode emailNode = iterEmails.next();
 			String type = emailNode.get("type").asText();
 			if (StringUtils.equals(type, "account")) {
@@ -94,5 +94,5 @@ public class OAuthLogin {
 			}
 		}
 	}
-	
+
 }
