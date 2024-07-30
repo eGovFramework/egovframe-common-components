6f802434a2c52acd86238aaa074a60cfe35eaeb6
이백행 <dlqorgod@naver.com>
2024-07-31 06:48:44 +0900

    ShortVariable 짧은 변수

--- src/test/java/egovframework/com/uat/uia/service/impl/ActionLoginLoginDAOTest.java
+++ src/test/java/egovframework/com/uat/uia/service/impl/ActionLoginLoginDAOTest.java
@@ -46,26 +46,26 @@ public class ActionLoginLoginDAOTest extends EgovTestAbstractDAO {
 	@Test
 	public void selectDeptJob() throws Exception {
 		// given
-		final LoginVO vo = new LoginVO();
-		vo.setUserSe("USR");
-		vo.setId("TEST1");
-		vo.setPassword(EgovFileScrty.encryptPassword("rhdxhd12", vo.getId()));
+		final LoginVO loginVO = new LoginVO();
+		loginVO.setUserSe("USR");
+		loginVO.setId("TEST1");
+		loginVO.setPassword(EgovFileScrty.encryptPassword("rhdxhd12", loginVO.getId()));
 
 		// when
-		final LoginVO result = loginDAO.actionLogin(vo);
+		final LoginVO result = loginDAO.actionLogin(loginVO);
 
 		if (log.isDebugEnabled()) {
 			log.debug("result={}", result);
-			log.debug("getUserSe={}, {}", vo.getUserSe(), result.getUserSe());
-			log.debug("getId={}, {}", vo.getId(), result.getId());
-			log.debug("getPassword={}, {}", vo.getPassword(), result.getPassword());
+			log.debug("getUserSe={}, {}", loginVO.getUserSe(), result.getUserSe());
+			log.debug("getId={}, {}", loginVO.getId(), result.getId());
+			log.debug("getPassword={}, {}", loginVO.getPassword(), result.getPassword());
 		}
 
 		// then
 		final String message = egovMessageSource.getMessage(FAIL_COMMON_SELECT);
-		assertEquals(message, vo.getUserSe(), result.getUserSe());
-		assertEquals(message, vo.getId(), result.getId());
-		assertEquals(message, vo.getPassword(), result.getPassword());
+		assertEquals(message, loginVO.getUserSe(), result.getUserSe());
+		assertEquals(message, loginVO.getId(), result.getId());
+		assertEquals(message, loginVO.getPassword(), result.getPassword());
 	}
 
 }
