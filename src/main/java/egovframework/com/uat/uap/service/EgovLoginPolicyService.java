6d121822343de2fd242bdaf0fac34954266f53e5
이백행 <dlqorgod@naver.com>
2024-08-01 07:41:27 +0900

    EgovLoginPolicyService 시큐어코딩 Exception 제거

--- src/main/java/egovframework/com/uat/uap/service/EgovLoginPolicyService.java
+++ src/main/java/egovframework/com/uat/uap/service/EgovLoginPolicyService.java
@@ -1,27 +1,31 @@
+package egovframework.com.uat.uap.service;
+
+import java.util.List;
+
 /**
+ * <pre>
  * 개요
  * - 로그인정책에 대한 Service Interface를 정의한다.
  * 
  * 상세내용
  * - 로그인정책에 대한 등록, 수정, 삭제, 조회, 반영확인 기능을 제공한다.
  * - 로그인정책의 조회기능은 목록조회, 상세조회로 구분된다.
+ * </pre>
+ * 
  * @author lee.m.j
  * @version 1.0
  * @created 03-8-2009 오후 2:08:54
- *   <pre>
+ * 
+ *          <pre>
  * == 개정이력(Modification Information) ==
  * 
  *  수정일               수정자            수정내용
  *  ----------   --------   ---------------------------
  *  2009.08.03   이문준            최초 생성
  *  2021.02.18   신용호            selectLoginPolicyResult() 삭제
- * </pre>
+ *   2024.08.01  이백행          시큐어코딩 Exception 제거
+ *          </pre>
  */
-
-package egovframework.com.uat.uap.service;
-
-import java.util.List;
-
 public interface EgovLoginPolicyService {
 
 	/**
@@ -30,7 +34,7 @@ public interface EgovLoginPolicyService {
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return List - 로그인정책 목록
 	 */
-	public List<LoginPolicyVO> selectLoginPolicyList(LoginPolicyVO loginPolicyVO) throws Exception;
+	public List<LoginPolicyVO> selectLoginPolicyList(LoginPolicyVO loginPolicyVO);
 
 	/**
 	 * 로그인정책 목록 수를 조회한다.
@@ -38,7 +42,7 @@ public interface EgovLoginPolicyService {
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return int
 	 */
-	public int selectLoginPolicyListTotCnt(LoginPolicyVO loginPolicyVO) throws Exception;
+	public int selectLoginPolicyListTotCnt(LoginPolicyVO loginPolicyVO);
 
 	/**
 	 * 로그인정책 목록의 상세정보를 조회한다.
@@ -46,28 +50,28 @@ public interface EgovLoginPolicyService {
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return LoginPolicyVO - 로그인정책 VO
 	 */
-	public LoginPolicyVO selectLoginPolicy(LoginPolicyVO loginPolicyVO) throws Exception;
+	public LoginPolicyVO selectLoginPolicy(LoginPolicyVO loginPolicyVO);
 
 	/**
 	 * 로그인정책 정보를 신규로 등록한다.
 	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 */
-	public void insertLoginPolicy(LoginPolicy loginPolicy) throws Exception;
+	public void insertLoginPolicy(LoginPolicy loginPolicy);
 
 	/**
 	 * 기 등록된 로그인정책 정보를 수정한다.
 	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 */
-	public void updateLoginPolicy(LoginPolicy loginPolicy) throws Exception;
+	public void updateLoginPolicy(LoginPolicy loginPolicy);
 
 	/**
 	 * 기 등록된 로그인정책 정보를 삭제한다.
 	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 */
-	public void deleteLoginPolicy(LoginPolicy loginPolicy) throws Exception;
+	public void deleteLoginPolicy(LoginPolicy loginPolicy);
 
 	/**
 	 * 로그인정책에 대한 현재 반영되어 있는 결과를 조회한다.
@@ -76,7 +80,6 @@ public interface EgovLoginPolicyService {
 	 * @return LoginPolicyVO - 로그인정책 VO
 	 */
 	/*
-	 * public LoginPolicyVO selectLoginPolicyResult(LoginPolicyVO loginPolicyVO)
-	 * throws Exception;
+	 * public LoginPolicyVO selectLoginPolicyResult(LoginPolicyVO loginPolicyVO) ;
 	 */
 }
