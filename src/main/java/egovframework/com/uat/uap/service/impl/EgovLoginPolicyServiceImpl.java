a84f28607b9f5953e97383f4720cbdbf41ce94dd
이백행 <dlqorgod@naver.com>
2024-08-01 07:42:00 +0900

    EgovLoginPolicyServiceImpl Source > Format

--- src/main/java/egovframework/com/uat/uap/service/impl/EgovLoginPolicyServiceImpl.java
+++ src/main/java/egovframework/com/uat/uap/service/impl/EgovLoginPolicyServiceImpl.java
@@ -22,75 +22,87 @@ package egovframework.com.uat.uap.service.impl;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import org.springframework.stereotype.Service;
+
 import egovframework.com.uat.uap.service.EgovLoginPolicyService;
 import egovframework.com.uat.uap.service.LoginPolicy;
 import egovframework.com.uat.uap.service.LoginPolicyVO;
 
-import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
-
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Service;
-
 @Service("egovLoginPolicyService")
 public class EgovLoginPolicyServiceImpl extends EgovAbstractServiceImpl implements EgovLoginPolicyService {
-	
-	@Resource(name="loginPolicyDAO")
+
+	@Resource(name = "loginPolicyDAO")
 	LoginPolicyDAO loginPolicyDAO;
-	
+
 	/**
 	 * 로그인정책 목록을 조회한다.
+	 * 
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return List - 로그인정책 목록
 	 */
+	@Override
 	public List<LoginPolicyVO> selectLoginPolicyList(LoginPolicyVO loginPolicyVO) throws Exception {
 		return loginPolicyDAO.selectLoginPolicyList(loginPolicyVO);
 	}
 
 	/**
 	 * 로그인정책 목록 수를 조회한다.
+	 * 
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return int
 	 */
+	@Override
 	public int selectLoginPolicyListTotCnt(LoginPolicyVO loginPolicyVO) throws Exception {
 		return loginPolicyDAO.selectLoginPolicyListTotCnt(loginPolicyVO);
 	}
 
 	/**
 	 * 로그인정책 목록의 상세정보를 조회한다.
+	 * 
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return LoginPolicyVO - 로그인정책 VO
 	 */
+	@Override
 	public LoginPolicyVO selectLoginPolicy(LoginPolicyVO loginPolicyVO) throws Exception {
 		return loginPolicyDAO.selectLoginPolicy(loginPolicyVO);
 	}
 
 	/**
 	 * 로그인정책 정보를 신규로 등록한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 */
+	@Override
 	public void insertLoginPolicy(LoginPolicy loginPolicy) throws Exception {
 		loginPolicyDAO.insertLoginPolicy(loginPolicy);
 	}
 
 	/**
 	 * 기 등록된 로그인정책 정보를 수정한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 */
+	@Override
 	public void updateLoginPolicy(LoginPolicy loginPolicy) throws Exception {
 		loginPolicyDAO.updateLoginPolicy(loginPolicy);
 	}
 
 	/**
 	 * 기 등록된 로그인정책 정보를 삭제한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 */
+	@Override
 	public void deleteLoginPolicy(LoginPolicy loginPolicy) throws Exception {
 		loginPolicyDAO.deleteLoginPolicy(loginPolicy);
 	}
 
 	/**
 	 * 로그인정책에 대한 현재 반영되어 있는 결과를 조회한다.
+	 * 
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return LoginPolicyVO - 로그인정책 VO
 	 */
