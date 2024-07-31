351db4df3b03c2f14aac04d5f30094c64ffc7626
이백행 <dlqorgod@naver.com>
2024-08-01 07:43:31 +0900

    LoginPolicyDAO Source > Format

--- src/main/java/egovframework/com/uat/uap/service/impl/LoginPolicyDAO.java
+++ src/main/java/egovframework/com/uat/uap/service/impl/LoginPolicyDAO.java
@@ -22,52 +22,57 @@ package egovframework.com.uat.uap.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
 import egovframework.com.uat.uap.service.LoginPolicy;
 import egovframework.com.uat.uap.service.LoginPolicyVO;
 
-import org.springframework.stereotype.Repository;
-
 @Repository("loginPolicyDAO")
 public class LoginPolicyDAO extends EgovComAbstractDAO {
-	
+
 	/**
 	 * 로그인정책 목록을 조회한다.
+	 * 
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return List - 로그인정책 목록
-	 */	
+	 */
 	public List<LoginPolicyVO> selectLoginPolicyList(LoginPolicyVO loginPolicyVO) throws Exception {
 		return selectList("loginPolicyDAO.selectLoginPolicyList", loginPolicyVO);
 	}
 
 	/**
 	 * 로그인정책 목록 수를 조회한다.
+	 * 
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return int
 	 */
 	public int selectLoginPolicyListTotCnt(LoginPolicyVO loginPolicyVO) throws Exception {
-		return (Integer)selectOne("loginPolicyDAO.selectLoginPolicyListTotCnt", loginPolicyVO);
+		return (Integer) selectOne("loginPolicyDAO.selectLoginPolicyListTotCnt", loginPolicyVO);
 	}
 
 	/**
 	 * 로그인정책 목록의 상세정보를 조회한다.
+	 * 
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return LoginPolicyVO - 로그인정책 VO
 	 */
 	public LoginPolicyVO selectLoginPolicy(LoginPolicyVO loginPolicyVO) throws Exception {
-		return (LoginPolicyVO)selectOne("loginPolicyDAO.selectLoginPolicy", loginPolicyVO);
+		return (LoginPolicyVO) selectOne("loginPolicyDAO.selectLoginPolicy", loginPolicyVO);
 	}
 
 	/**
 	 * 로그인정책 정보를 신규로 등록한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 */
 	public void insertLoginPolicy(LoginPolicy loginPolicy) throws Exception {
-        insert("loginPolicyDAO.insertLoginPolicy", loginPolicy);
+		insert("loginPolicyDAO.insertLoginPolicy", loginPolicy);
 	}
 
 	/**
 	 * 기 등록된 로그인정책 정보를 수정한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 */
 	public void updateLoginPolicy(LoginPolicy loginPolicy) throws Exception {
@@ -76,6 +81,7 @@ public class LoginPolicyDAO extends EgovComAbstractDAO {
 
 	/**
 	 * 기 등록된 로그인정책 정보를 삭제한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 */
 	public void deleteLoginPolicy(LoginPolicy loginPolicy) throws Exception {
