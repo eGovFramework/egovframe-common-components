ca72d31fa509388bcdfb1aba418e911e3a98fe23
이백행 <dlqorgod@naver.com>
2024-08-03 12:47:28 +0900

    EgovAuthorManageService 시큐어코딩 Exception 제거

--- src/main/java/egovframework/com/sec/ram/service/EgovAuthorManageService.java
+++ src/main/java/egovframework/com/sec/ram/service/EgovAuthorManageService.java
@@ -16,6 +16,7 @@ import java.util.List;
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.03.20  이문준          최초 생성
+ *   2024.08.03  이백행          시큐어코딩 Exception 제거
  *
  *      </pre>
  */
@@ -26,58 +27,51 @@ public interface EgovAuthorManageService {
 	 * 
 	 * @param authorManageVO AuthorManageVO
 	 * @return List<AuthorManageVO>
-	 * @exception Exception
 	 */
-	public List<AuthorManageVO> selectAuthorAllList(AuthorManageVO authorManageVO) throws Exception;
+	public List<AuthorManageVO> selectAuthorAllList(AuthorManageVO authorManageVO);
 
 	/**
 	 * 시스템 사용자중 불필요한 시스템권한정보를 화면에 조회하여 데이터베이스에서 삭제
 	 * 
 	 * @param authorManage AuthorManage
-	 * @exception Exception
 	 */
-	public void deleteAuthor(AuthorManage authorManage) throws Exception;
+	public void deleteAuthor(AuthorManage authorManage);
 
 	/**
 	 * 사용자의 시스테접근권한를 화면에서 입력하여 입력항목의 정합성을 체크하고 데이터베이스에 저장
 	 * 
 	 * @param authorManage AuthorManage
-	 * @exception Exception
 	 */
-	public void insertAuthor(AuthorManage authorManage) throws Exception;
+	public void insertAuthor(AuthorManage authorManage);
 
 	/**
 	 * 개별사용자에게 할당된 권한 조회
 	 * 
 	 * @param authorManageVO AuthorManageVO
-	 * @exception Exception
 	 */
-	public AuthorManageVO selectAuthor(AuthorManageVO authorManageVO) throws Exception;
+	public AuthorManageVO selectAuthor(AuthorManageVO authorManageVO);
 
 	/**
 	 * 개별사용자에게 할당된 권한리스트 조회
 	 * 
 	 * @param authorManageVO AuthorManageVO
 	 * @return List<AuthorManageVO>
-	 * @exception Exception
 	 */
-	public List<AuthorManageVO> selectAuthorList(AuthorManageVO authorManageVO) throws Exception;
+	public List<AuthorManageVO> selectAuthorList(AuthorManageVO authorManageVO);
 
 	/**
 	 * 화면에 조회된 사용자권한정보를 수정하여 항목의 정합성을 체크하고 수정된 데이터를 데이터베이스에 반영
 	 * 
 	 * @param authorManage AuthorManage
-	 * @exception Exception
 	 */
-	public void updateAuthor(AuthorManage authorManage) throws Exception;
+	public void updateAuthor(AuthorManage authorManage);
 
 	/**
 	 * 목록조회 카운트를 반환한다
 	 * 
 	 * @param authorManageVO AuthorManageVO
 	 * @return int
-	 * @exception Exception
 	 */
-	public int selectAuthorListTotCnt(AuthorManageVO authorManageVO) throws Exception;
+	public int selectAuthorListTotCnt(AuthorManageVO authorManageVO);
 
 }
