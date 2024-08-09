c92d8f5f01bb5a76b3b460f779ab3f5dab036088
이백행 <dlqorgod@naver.com>
2024-08-10 06:32:12 +0900

    130. 사용자통계 시큐어코딩 Exception 제거

--- src/main/java/egovframework/com/sts/ust/service/EgovUserStatsService.java
+++ src/main/java/egovframework/com/sts/ust/service/EgovUserStatsService.java
@@ -6,34 +6,35 @@ import egovframework.com.sts.com.StatsVO;
 
 /**
  * 사용자 통계 검색 비즈니스 인터페이스 클래스
+ * 
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.03.12
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일     수정자          수정내용
  *  -------    --------    ---------------------------
  *  2009.03.19  박지욱          최초 생성
  *  2011.06.30  이기하          패키지 분리(sts -> sts.sst)
+ *   2024.08.10  이백행          시큐어코딩 Exception 제거
  *
- *  </pre>
+ *      </pre>
  */
 public interface EgovUserStatsService {
 
 	/**
 	 * 사용자 통계를 조회한다
+	 * 
 	 * @param vo StatsVO
 	 * @return List
-	 * @exception Exception
 	 */
-	List<StatsVO> selectUserStats(StatsVO vo) throws Exception;
+	List<StatsVO> selectUserStats(StatsVO vo);
 
 	/**
 	 * 사용자 통계를 위한 집계를 하루단위로 작업하는 배치 프로그램
-	 * @exception Exception
 	 */
-	public void summaryUserStats() throws Exception;
+	public void summaryUserStats();
 }
