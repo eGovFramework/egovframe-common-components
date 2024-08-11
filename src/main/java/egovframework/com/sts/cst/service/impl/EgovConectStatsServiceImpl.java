69412bb2b9f74ec62a6d21f540d5c751b75f2f6e
이백행 <dlqorgod@naver.com>
2024-08-12 06:23:55 +0900

    140. 접속통계 시큐어코딩 Exception 제거

--- src/main/java/egovframework/com/sts/cst/service/impl/EgovConectStatsServiceImpl.java
+++ src/main/java/egovframework/com/sts/cst/service/impl/EgovConectStatsServiceImpl.java
@@ -2,47 +2,47 @@ package egovframework.com.sts.cst.service.impl;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import org.springframework.stereotype.Service;
+
 import egovframework.com.sts.com.StatsVO;
 import egovframework.com.sts.cst.service.EgovConectStatsService;
 
-import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
-
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Service;
-
 /**
  * 접속 통계 검색 비즈니스 구현 클래스
+ * 
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.03.12
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자          수정내용
  *  -------    --------    ---------------------------
  *  2009.03.19  박지욱          최초 생성
  *  2011.06.30  이기하          패키지 분리(sts -> sts.cst)
+ *   2024.08.12  이백행          시큐어코딩 Exception 제거
  *
- *  </pre>
+ *      </pre>
  */
 @Service("conectStatsService")
-public class EgovConectStatsServiceImpl extends EgovAbstractServiceImpl implements
-	EgovConectStatsService {
+public class EgovConectStatsServiceImpl extends EgovAbstractServiceImpl implements EgovConectStatsService {
 
-    @Resource(name="conectStatsDAO")
-    private ConectStatsDAO conectStatsDAO;
+	@Resource(name = "conectStatsDAO")
+	private ConectStatsDAO conectStatsDAO;
 
-    /**
+	/**
 	 * 접속 통계를 조회한다
+	 * 
 	 * @param vo StatsVO
 	 * @return List
-	 * @exception Exception
 	 */
-    @Override
-	public List<StatsVO> selectConectStats(StatsVO vo) throws Exception {
-        return conectStatsDAO.selectConectStats(vo);
+	@Override
+	public List<StatsVO> selectConectStats(StatsVO vo) {
+		return conectStatsDAO.selectConectStats(vo);
 	}
 }
