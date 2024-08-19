5c5a401673e3d6afd51fcabfe3fdac5da97dd2af
이백행 <dlqorgod@naver.com>
2024-08-19 13:53:36 +0900

    161. 자료이용현황통계 시큐어코딩 Exception 제거

--- src/main/java/egovframework/com/sts/dst/service/impl/DtaUseStatsDAO.java
+++ src/main/java/egovframework/com/sts/dst/service/impl/DtaUseStatsDAO.java
@@ -1,98 +1,108 @@
+package egovframework.com.sts.dst.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
+import egovframework.com.sts.dst.service.DtaUseStats;
+import egovframework.com.sts.dst.service.DtaUseStatsVO;
+
 /**
+ * <pre>
  * 개요
  * - 자료이용현황 통계에 대한 DAO 클래스를 정의한다.
  * 
  * 상세내용
  * - 자료이용현황 통계에 대한 등록, 조회 기능을 제공한다.
  * - 자료이용현황 통계의 조회기능은 목록조회, 상세조회로 구분된다.
+ * </pre>
+ * 
  * @author lee.m.j
  * @version 1.0
  * @created 08-9-2009 오후 1:40:19
  */
-
-package egovframework.com.sts.dst.service.impl;
-
-import java.util.List;
-
-import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
-import egovframework.com.sts.dst.service.DtaUseStats;
-import egovframework.com.sts.dst.service.DtaUseStatsVO;
-
-import org.springframework.stereotype.Repository;
-
 @Repository("dtaUseStatsDAO")
 public class DtaUseStatsDAO extends EgovComAbstractDAO {
 
 	/**
 	 * 자료이용현황 통계정보의 대상목록을 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return List - 자료이용현황 목록
 	 */
-	public List<DtaUseStatsVO> selectDtaUseStatsList(DtaUseStatsVO dtaUseStatsVO) throws Exception {
+	public List<DtaUseStatsVO> selectDtaUseStatsList(DtaUseStatsVO dtaUseStatsVO) {
 		return selectList("dtaUseStatsDAO.selectDtaUseStatsList", dtaUseStatsVO);
 	}
 
 	/**
 	 * 자료이용현황 통계정보의 대상목록 카운트를 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return int
 	 */
-	public int selectDtaUseStatsListTotCnt(DtaUseStatsVO dtaUseStatsVO) throws Exception {
-		return (Integer)selectOne("dtaUseStatsDAO.selectDtaUseStatsListTotCnt", dtaUseStatsVO);
-	}	
-		
+	public int selectDtaUseStatsListTotCnt(DtaUseStatsVO dtaUseStatsVO) {
+		return (Integer) selectOne("dtaUseStatsDAO.selectDtaUseStatsListTotCnt", dtaUseStatsVO);
+	}
+
 	/**
 	 * 자료이용현황 통계정보의 전체 카운트를 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return int
 	 */
-	public int selectDtaUseStatsListBarTotCnt(DtaUseStatsVO dtaUseStatsVO) throws Exception {
-		return (Integer)selectOne("dtaUseStatsDAO.selectDtaUseStatsListBarTotCnt", dtaUseStatsVO);
-	}		
-	
+	public int selectDtaUseStatsListBarTotCnt(DtaUseStatsVO dtaUseStatsVO) {
+		return (Integer) selectOne("dtaUseStatsDAO.selectDtaUseStatsListBarTotCnt", dtaUseStatsVO);
+	}
+
 	/**
 	 * 자료이용현황 통계의 상세정보를 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return reprtStatsVO - 자료이용현황 VO
 	 */
-	public List<DtaUseStatsVO> selectDtaUseStats(DtaUseStatsVO dtaUseStatsVO) throws Exception {
+	public List<DtaUseStatsVO> selectDtaUseStats(DtaUseStatsVO dtaUseStatsVO) {
 		return selectList("dtaUseStatsDAO.selectDtaUseStats", dtaUseStatsVO);
 	}
 
 	/**
 	 * 자료이용현황 통계정보의 상세정보목록 카운트를 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return int
 	 */
-	public int selectDtaUseStatsTotCnt(DtaUseStatsVO dtaUseStatsVO) throws Exception {
-		return (Integer)selectOne("dtaUseStatsDAO.selectDtaUseStatsTotCnt", dtaUseStatsVO);
-	}	
-	
-    /**
+	public int selectDtaUseStatsTotCnt(DtaUseStatsVO dtaUseStatsVO) {
+		return (Integer) selectOne("dtaUseStatsDAO.selectDtaUseStatsTotCnt", dtaUseStatsVO);
+	}
+
+	/**
 	 * 자료이용현황 정보를 등록을 위한 다운로드 첨부화일 정보를 조회한다.
+	 * 
 	 * @param dtaUseStats DtaUseStats
 	 * @return DtaUseStats
 	 * @exception Exception
 	 */
-    public DtaUseStats selectInsertDtaUseStats(DtaUseStats dtaUseStats) throws Exception {
-        return (DtaUseStats) selectOne("dtaUseStatsDAO.selectInsertDtaUseStats", dtaUseStats);
-    }	
-	
+	public DtaUseStats selectInsertDtaUseStats(DtaUseStats dtaUseStats) {
+		return (DtaUseStats) selectOne("dtaUseStatsDAO.selectInsertDtaUseStats", dtaUseStats);
+	}
+
 	/**
 	 * 자료이용현황 정보를 등록한다.
+	 * 
 	 * @param dtaUseStats - 자료이용현황 model
 	 */
-	public void insertDtaUseStats(DtaUseStats dtaUseStats) throws Exception {
+	public void insertDtaUseStats(DtaUseStats dtaUseStats) {
 
 		insert("dtaUseStatsDAO.insertDtaUseStats", dtaUseStats);
 	}
 
 	/**
 	 * 등록일자별 통계정보를 그래프로 표현한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return List - 등록일자별 자료이용현황 목록
 	 */
-	public List<DtaUseStatsVO> selectDtaUseStatsBarList(DtaUseStatsVO dtaUseStatsVO) throws Exception {
+	public List<DtaUseStatsVO> selectDtaUseStatsBarList(DtaUseStatsVO dtaUseStatsVO) {
 		return selectList("dtaUseStatsDAO.selectDtaUseStatsBarList", dtaUseStatsVO);
 	}
 }
--- src/main/java/egovframework/com/sts/dst/service/impl/EgovDtaUseStatsServiceImpl.java
+++ src/main/java/egovframework/com/sts/dst/service/impl/EgovDtaUseStatsServiceImpl.java
@@ -1,27 +1,3 @@
-/**
- * 개요
- * - 자료이용현황 통계에 대한 ServiceImpl를 정의한다.
- *
- * 상세내용
- * - 자료이용현황 통계에 대한 등록, 조회 기능을 제공한다.
- * - 자료이용현황 통계의 조회기능은 목록조회, 상세조회로 구분된다.
- * - 게시판에서 다운로드한 통계만 적용된다.(게시판이 아닌경우는 통계에서 제외함)
- * @author lee.m.j
- * @version 1.0
- * @created 08-9-2009 오후 1:40:19
- *
- * <pre>
- * << 개정이력(Modification Information) >>
- *
- *   수정일      수정자           수정내용
- *  -------    --------    ---------------------------
- *   2011.8.23  정진오 		SQL Map에서 정의한 파라미터 클래스와 실제 전달하는 클래스가 달라서 발생하는 에러 수정
- *   						new DtaUseStats() -> new DtaUseStatsVO()
- *   2011.9.29	이기하		게시판외 다운로드시 에러발생(dtaUseStats 값이 null)을 방지
- *
- * </pre>
- */
-
 package egovframework.com.sts.dst.service.impl;
 
 import java.util.List;
@@ -30,6 +6,9 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.aspectj.lang.JoinPoint;
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import org.egovframe.rte.fdl.cmmn.exception.FdlException;
+import org.egovframe.rte.fdl.idgnr.EgovIdGnrService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,79 +18,113 @@ import egovframework.com.sts.dst.service.DtaUseStats;
 import egovframework.com.sts.dst.service.DtaUseStatsVO;
 import egovframework.com.sts.dst.service.EgovDtaUseStatsService;
 import egovframework.com.utl.fcc.service.EgovStringUtil;
-import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
-import org.egovframe.rte.fdl.idgnr.EgovIdGnrService;
 
+/**
+ * <pre>
+ * 개요
+ * - 자료이용현황 통계에 대한 ServiceImpl를 정의한다.
+ *
+ * 상세내용
+ * - 자료이용현황 통계에 대한 등록, 조회 기능을 제공한다.
+ * - 자료이용현황 통계의 조회기능은 목록조회, 상세조회로 구분된다.
+ * - 게시판에서 다운로드한 통계만 적용된다.(게시판이 아닌경우는 통계에서 제외함)
+ * </pre>
+ * 
+ * @author lee.m.j
+ * @version 1.0
+ * @created 08-9-2009 오후 1:40:19
+ *
+ *          <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2011.08.23  정진오          SQL Map에서 정의한 파라미터 클래스와 실제 전달하는 클래스가 달라서 발생하는 에러 수정
+ *                             new DtaUseStats() -> new DtaUseStatsVO()
+ *   2011.09.29  이기하          게시판외 다운로드시 에러발생(dtaUseStats 값이 null)을 방지
+ *   2024.08.19  이백행          시큐어코딩 Exception 제거
+ *
+ *          </pre>
+ */
 @Service("egovDtaUseStatsService")
 public class EgovDtaUseStatsServiceImpl extends EgovAbstractServiceImpl implements EgovDtaUseStatsService {
 
-
-
-
-	@Resource(name="dtaUseStatsDAO")
+	@Resource(name = "dtaUseStatsDAO")
 	DtaUseStatsDAO dtaUseStatsDAO;
 
-	@Resource(name="egovDtaUseStatsIdGnrService")
-    private EgovIdGnrService egovDtaUseStatsIdGnrService;
+	@Resource(name = "egovDtaUseStatsIdGnrService")
+	private EgovIdGnrService egovDtaUseStatsIdGnrService;
 
 	/**
 	 * 자료이용현황 통계정보의 대상목록을 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return List - 자료이용현황 목록
 	 */
-	public List<DtaUseStatsVO> selectDtaUseStatsList(DtaUseStatsVO dtaUseStatsVO) throws Exception {
+	@Override
+	public List<DtaUseStatsVO> selectDtaUseStatsList(DtaUseStatsVO dtaUseStatsVO) {
 		return dtaUseStatsDAO.selectDtaUseStatsList(dtaUseStatsVO);
 	}
 
 	/**
 	 * 자료이용현황 통계정보의 대상목록 카운트를 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return int
 	 */
-	public int selectDtaUseStatsListTotCnt(DtaUseStatsVO dtaUseStatsVO) throws Exception {
+	@Override
+	public int selectDtaUseStatsListTotCnt(DtaUseStatsVO dtaUseStatsVO) {
 		return dtaUseStatsDAO.selectDtaUseStatsListTotCnt(dtaUseStatsVO);
 	}
 
 	/**
 	 * 자료이용현황 통계정보의 전체 카운트를 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return int
 	 */
-	public int selectDtaUseStatsListBarTotCnt(DtaUseStatsVO dtaUseStatsVO) throws Exception {
+	@Override
+	public int selectDtaUseStatsListBarTotCnt(DtaUseStatsVO dtaUseStatsVO) {
 		return dtaUseStatsDAO.selectDtaUseStatsListBarTotCnt(dtaUseStatsVO);
 	}
 
 	/**
 	 * 자료이용현황 통계의 상세정보를 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return reprtStatsVO - 자료이용현황 VO
 	 */
-	public List<DtaUseStatsVO> selectDtaUseStats(DtaUseStatsVO dtaUseStatsVO) throws Exception {
+	@Override
+	public List<DtaUseStatsVO> selectDtaUseStats(DtaUseStatsVO dtaUseStatsVO) {
 		return dtaUseStatsDAO.selectDtaUseStats(dtaUseStatsVO);
 	}
 
 	/**
 	 * 자료이용현황 통계정보의 상세정보목록 카운트를 조회한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return int
 	 */
-	public int selectDtaUseStatsTotCnt(DtaUseStatsVO dtaUseStatsVO) throws Exception {
+	@Override
+	public int selectDtaUseStatsTotCnt(DtaUseStatsVO dtaUseStatsVO) {
 		return dtaUseStatsDAO.selectDtaUseStatsTotCnt(dtaUseStatsVO);
 	}
 
 	/**
 	 * 자료이용현황 정보를 생성한다.
-	 * @param jp - AOP의 pointcut을 위한 JoinPoint
+	 * 
+	 * @param jp          - AOP의 pointcut을 위한 JoinPoint
 	 * @param dtaUseStats - 자료이용현황 model
 	 */
-    public void insertDtaUseStats(JoinPoint jp, @RequestParam Map<String, Object> commandMap) throws Exception {
+	@Override
+	public void insertDtaUseStats(JoinPoint jp, @RequestParam Map<String, Object> commandMap) {
 
-    	String atchFileId = (String)commandMap.get("atchFileId");
-    	String fileSn = (String)commandMap.get("fileSn");
+		String atchFileId = (String) commandMap.get("atchFileId");
+		String fileSn = (String) commandMap.get("fileSn");
 
-		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 
-		DtaUseStats dtaUseStats = new DtaUseStatsVO();	//2011.08.23 수정 부분
+		DtaUseStats dtaUseStats = new DtaUseStatsVO(); // 2011.08.23 수정 부분
 		dtaUseStats.setAtchFileId(atchFileId);
 		dtaUseStats.setFileSn(fileSn);
 
@@ -119,13 +132,19 @@ public class EgovDtaUseStatsServiceImpl extends EgovAbstractServiceImpl implemen
 
 		// 2011.09.29 게시판외 다운로드시 에러발생(dtaUseStats 값이 null)을 방지
 		if (dtaUseStats != null) {
-			DtaUseStats vo = new DtaUseStatsVO();			//2011.08.23 수정 부분
-			String id = user == null ? "" : EgovStringUtil.isNullToString(user.getId()); // KISA 보안약점 조치 (2018-12-11, 신용호)
-			vo.setDtaUseStatsId(egovDtaUseStatsIdGnrService.getNextStringId());
+			DtaUseStats vo = new DtaUseStatsVO(); // 2011.08.23 수정 부분
+			String id = user == null ? "" : EgovStringUtil.isNullToString(user.getId()); // KISA 보안약점 조치 (2018-12-11,
+																							// 신용호)
+			try {
+				vo.setDtaUseStatsId(egovDtaUseStatsIdGnrService.getNextStringId());
+			} catch (FdlException e) {
+				processException("fail.common.msg");
+//				throw new BaseRuntimeException("FdlException: egovDtaUseStatsIdGnrService", e);
+			}
 			vo.setBbsId(dtaUseStats.getBbsId());
 			vo.setNttId(dtaUseStats.getNttId());
-	        vo.setAtchFileId(atchFileId);
-	        vo.setFileSn(fileSn);
+			vo.setAtchFileId(atchFileId);
+			vo.setFileSn(fileSn);
 			vo.setUserId(id);
 
 			dtaUseStatsDAO.insertDtaUseStats(vo);
@@ -134,10 +153,12 @@ public class EgovDtaUseStatsServiceImpl extends EgovAbstractServiceImpl implemen
 
 	/**
 	 * 등록일자별 통계정보를 그래프로 표현한다.
+	 * 
 	 * @param dtaUseStatsVO - 자료이용현황 VO
 	 * @return List - 등록일자별 자료이용현황 목록
 	 */
-	public List<DtaUseStatsVO> selectDtaUseStatsBarList(DtaUseStatsVO dtaUseStatsVO) throws Exception {
+	@Override
+	public List<DtaUseStatsVO> selectDtaUseStatsBarList(DtaUseStatsVO dtaUseStatsVO) {
 		return dtaUseStatsDAO.selectDtaUseStatsBarList(dtaUseStatsVO);
 	}
 
