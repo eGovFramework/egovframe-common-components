d66bcd98fc6619aeafcdfd5a25e5c60e1e097beb
이백행 <dlqorgod@naver.com>
2024-08-31 07:22:22 +0900

    [350. 전체일정관리] 시큐어코딩 Exception 제거

--- src/main/java/egovframework/com/cop/smt/sam/service/impl/AllSchdulManageDao.java
+++ src/main/java/egovframework/com/cop/smt/sam/service/impl/AllSchdulManageDao.java
@@ -2,49 +2,51 @@ package egovframework.com.cop.smt.sam.service.impl;
 
 import java.util.List;
 
+import org.egovframe.rte.psl.dataaccess.util.EgovMap;
+import org.springframework.stereotype.Repository;
+
 import egovframework.com.cmm.ComDefaultVO;
 import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
 
-import org.egovframe.rte.psl.dataaccess.util.EgovMap;
-import org.springframework.stereotype.Repository;
 /**
  * 전체일정을 처리하는 Dao Class 구현
+ * 
  * @author 공통서비스 장동한
  * @since 2009.04.10
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *   
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
- *   2009.04.10  장동한         최초 생성
- *   2016.08.01  장동한         표준프레임워크 v3.6 개선
+ *   2009.04.10  장동한          최초 생성
+ *   2016.08.01  장동한          표준프레임워크 v3.6 개선
+ *   2024.08.31  이백행          컨트리뷰션 시큐어코딩 Exception 제거
  *
- * </pre>
+ *      </pre>
  */
 @Repository("allSchdulManageDao")
 public class AllSchdulManageDao extends EgovComAbstractDAO {
-	
-    /**
-	 * 전체일정 목록을 조회한다. 
+
+	/**
+	 * 전체일정 목록을 조회한다.
+	 * 
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return List
-	 * @throws Exception
 	 */
-	public List<EgovMap> selectAllSchdulManageeList(ComDefaultVO searchVO) throws Exception{
+	public List<EgovMap> selectAllSchdulManageeList(ComDefaultVO searchVO) {
 		return selectList("AllSchdulManage.selectIndvdlSchdulManage", searchVO);
 	}
-	
 
-    /**
+	/**
 	 * 전체일정를(을) 목록 전체 건수를(을) 조회한다.
+	 * 
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return int
-	 * @throws Exception
 	 */
-	public int selectAllSchdulManageListCnt(ComDefaultVO searchVO) throws Exception{
-		return (Integer)selectOne("AllSchdulManage.selectIndvdlSchdulManageCnt", searchVO);
+	public int selectAllSchdulManageListCnt(ComDefaultVO searchVO) {
+		return (Integer) selectOne("AllSchdulManage.selectIndvdlSchdulManageCnt", searchVO);
 	}
 }
--- src/main/java/egovframework/com/cop/smt/sam/service/impl/EgovAllSchdulManageServiceImpl.java
+++ src/main/java/egovframework/com/cop/smt/sam/service/impl/EgovAllSchdulManageServiceImpl.java
@@ -2,57 +2,59 @@ package egovframework.com.cop.smt.sam.service.impl;
 
 import java.util.List;
 
-import egovframework.com.cmm.ComDefaultVO;
-import egovframework.com.cop.smt.sam.service.EgovAllSchdulManageService;
+import javax.annotation.Resource;
 
 import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
 import org.egovframe.rte.psl.dataaccess.util.EgovMap;
-
-import javax.annotation.Resource;
-
 import org.springframework.stereotype.Service;
+
+import egovframework.com.cmm.ComDefaultVO;
+import egovframework.com.cop.smt.sam.service.EgovAllSchdulManageService;
+
 /**
  * 전체일정을 처리하는 ServiceImpl Class 구현
+ * 
  * @author 공통서비스 장동한
  * @since 2009.04.10
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.04.10  장동한          최초 생성
+ *   2024.08.31  이백행          컨트리뷰션 시큐어코딩 Exception 제거
  *
- * </pre>
+ *      </pre>
  */
 @Service("egovAllSchdulManageService")
-public class EgovAllSchdulManageServiceImpl extends EgovAbstractServiceImpl implements EgovAllSchdulManageService{
+public class EgovAllSchdulManageServiceImpl extends EgovAbstractServiceImpl implements EgovAllSchdulManageService {
 
-	@Resource(name="allSchdulManageDao")
+	@Resource(name = "allSchdulManageDao")
 	private AllSchdulManageDao dao;
 
-    /**
+	/**
 	 * 전체일정 목록을 조회한다.
+	 * 
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return List
-	 * @throws Exception
 	 */
 	@Override
-	public List<EgovMap> selectAllSchdulManageeList(ComDefaultVO searchVO) throws Exception{
+	public List<EgovMap> selectAllSchdulManageeList(ComDefaultVO searchVO) {
 
 		return dao.selectAllSchdulManageeList(searchVO);
 	}
 
-    /**
+	/**
 	 * 전체일정를(을) 목록 전체 건수를(을) 조회한다.
+	 * 
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return int
-	 * @throws Exception
 	 */
 	@Override
-	public int selectAllSchdulManageListCnt(ComDefaultVO searchVO) throws Exception{
+	public int selectAllSchdulManageListCnt(ComDefaultVO searchVO) {
 
 		return dao.selectAllSchdulManageListCnt(searchVO);
 	}
