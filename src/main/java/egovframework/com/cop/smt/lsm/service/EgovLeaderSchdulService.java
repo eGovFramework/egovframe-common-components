277b1de78a02438d3215306f78ca3f8e8af9472d
이백행 <dlqorgod@naver.com>
2024-09-06 06:47:04 +0900

    Source > Format

--- src/main/java/egovframework/com/cop/smt/lsm/service/EgovLeaderSchdulService.java
+++ src/main/java/egovframework/com/cop/smt/lsm/service/EgovLeaderSchdulService.java
@@ -4,39 +4,45 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * <pre>
  * 개요
  * - 간부일정에 대한 Service Interface를 정의한다.
  * 
  * 상세내용
  * - 간부일정에 대한 등록, 수정, 삭제, 조회기능을 제공한다.
  * - 간부일정의 조회기능은 목록조회, 상세조회로 구분된다.
+ * </pre>
+ * 
  * @author 장철호
  * @version 1.0
  * @created 28-6-2010 오전 10:59:05
- * <pre>
+ * 
+ *          <pre>
  * << 개정이력(Modification Information) >>
  *   
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2010.6.28	장철호          최초 생성
  *
- * </pre>
+ *          </pre>
  */
 public interface EgovLeaderSchdulService {
-	
+
 	/**
 	 * 사용자 목록을 조회한다.
+	 * 
 	 * @param EmplyrVO
-	 * @return  Map<String, Object>
+	 * @return Map<String, Object>
 	 * 
 	 * @param emplyrVO
 	 */
 	public Map<String, Object> selectEmplyrList(EmplyrVO emplyrVO) throws Exception;
-	
+
 	/**
 	 * 월별 간부일정 목록을 조회한다.
+	 * 
 	 * @param LeaderSchdulVO
-	 * @return  List
+	 * @return List
 	 * 
 	 * @param leaderSchdulVo
 	 */
@@ -44,8 +50,9 @@ public interface EgovLeaderSchdulService {
 
 	/**
 	 * 간부일정 정보를 조회한다.
+	 * 
 	 * @param LeaderSchdulVO
-	 * @return  LeaderSchdulVO
+	 * @return LeaderSchdulVO
 	 * 
 	 * @param leaderSchdulVO
 	 */
@@ -53,6 +60,7 @@ public interface EgovLeaderSchdulService {
 
 	/**
 	 * 간부일정 정보를 수정한다.
+	 * 
 	 * @param LeaderSchdul
 	 * 
 	 * @param leaderSchdul
@@ -61,6 +69,7 @@ public interface EgovLeaderSchdulService {
 
 	/**
 	 * 간부일정 정보를 등록한다.
+	 * 
 	 * @param LeaderSchdul
 	 * 
 	 * @param leaderSchdul
@@ -69,25 +78,28 @@ public interface EgovLeaderSchdulService {
 
 	/**
 	 * 간부일정 정보를 삭제한다.
+	 * 
 	 * @param LeaderSchdul
 	 * 
 	 * @param leaderSchdul
 	 */
 	public void deleteLeaderSchdul(LeaderSchdul leaderSchdul) throws Exception;
-	
+
 	/**
 	 * 간부상태 목록을 조회한다.
+	 * 
 	 * @param LeaderSttusVO - 간부상태 VO
-	 * @return  Map<String, Object>
+	 * @return Map<String, Object>
 	 * 
 	 * @param leaderSttusVO
 	 */
 	public Map<String, Object> selectLeaderSttusList(LeaderSttusVO leaderSttusVO) throws Exception;
-	
+
 	/**
 	 * 간부상태 정보를 조회한다.
+	 * 
 	 * @param LeaderSttusVO - 간부상태 VO
-	 * @return  LeaderSttusVO
+	 * @return LeaderSttusVO
 	 * 
 	 * @param leaderSttusVO
 	 */
@@ -95,6 +107,7 @@ public interface EgovLeaderSchdulService {
 
 	/**
 	 * 간부상태 정보를 수정한다.
+	 * 
 	 * @param LeaderSttus - 간부상태 model
 	 * 
 	 * @param leaderSttus
@@ -103,23 +116,26 @@ public interface EgovLeaderSchdulService {
 
 	/**
 	 * 간부상태 정보를 등록한다.
+	 * 
 	 * @param LeaderSttus - 간부상태 model
 	 * 
 	 * @param leaderSttus
 	 */
 	public void insertLeaderSttus(LeaderSttus leaderSttus) throws Exception;
-	
+
 	/**
 	 * 간부상태를 등록하기 위한 중복 조회를 수행한다.
+	 * 
 	 * @param LeaderSttus - 간부상태 model
-	 * @return  int 
+	 * @return int
 	 * 
 	 * @param leaderSttus
 	 */
 	public int selectLeaderSttusCheck(LeaderSttus leaderSttus) throws Exception;
-	
+
 	/**
 	 * 간부상태 정보를 삭제한다.
+	 * 
 	 * @param LeaderSttus - 간부상태 model
 	 * 
 	 * @param leaderSttus
