ad93ef04030970db5571ae3c3248219c061cb65b
이백행 <dlqorgod@naver.com>
2024-09-19 19:10:50 +0900

    Source > Format

--- src/main/java/egovframework/com/ssi/syi/ist/service/impl/CntcSttusDAO.java
+++ src/main/java/egovframework/com/ssi/syi/ist/service/impl/CntcSttusDAO.java
@@ -2,22 +2,23 @@ package egovframework.com.ssi.syi.ist.service.impl;
 
 import java.util.List;
 
+import org.egovframe.rte.psl.dataaccess.util.EgovMap;
+import org.springframework.stereotype.Repository;
+
 import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
 import egovframework.com.ssi.syi.ist.service.CntcSttus;
 import egovframework.com.ssi.syi.ist.service.CntcSttusVO;
 
-import org.egovframe.rte.psl.dataaccess.util.EgovMap;
-import org.springframework.stereotype.Repository;
-
 /**
  *
  * 연계현황에 대한 데이터 접근 클래스를 정의한다
+ * 
  * @author 공통서비스 개발팀 이중호
  * @since 2009.04.01
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
@@ -25,14 +26,14 @@ import org.springframework.stereotype.Repository;
  *   2009.04.01  이중호          최초 생성
  *
  * Copyright (C) 2009 by MOPAS  All right reserved.
- * </pre>
+ *      </pre>
  */
 @Repository("CntcSttusDAO")
 public class CntcSttusDAO extends EgovComAbstractDAO {
 
-
 	/**
 	 * 연계현황 상세항목을 조회한다.
+	 * 
 	 * @param CntcSttus
 	 * @return CntcSttus(연계현황)
 	 */
@@ -40,24 +41,25 @@ public class CntcSttusDAO extends EgovComAbstractDAO {
 		return (CntcSttus) selectOne("CntcSttusDAO.selectCntcSttusDetail", CntcSttus);
 	}
 
-
-    /**
+	/**
 	 * 연계현황 목록을 조회한다.
-     * @param searchVO
-     * @return List(연계현황 목록)
-     * @throws Exception
-     */
-    public List<EgovMap> selectCntcSttusList(CntcSttusVO searchVO) throws Exception {
-        return selectList("CntcSttusDAO.selectCntcSttusList", searchVO);
-    }
+	 * 
+	 * @param searchVO
+	 * @return List(연계현황 목록)
+	 * @throws Exception
+	 */
+	public List<EgovMap> selectCntcSttusList(CntcSttusVO searchVO) throws Exception {
+		return selectList("CntcSttusDAO.selectCntcSttusList", searchVO);
+	}
 
-    /**
+	/**
 	 * 연계현황 총 개수를 조회한다.
-     * @param searchVO
-     * @return int(연계현황 총 개수)
-     */
-    public int selectCntcSttusListTotCnt(CntcSttusVO searchVO) throws Exception {
-        return (Integer)selectOne("CntcSttusDAO.selectCntcSttusListTotCnt", searchVO);
-    }
+	 * 
+	 * @param searchVO
+	 * @return int(연계현황 총 개수)
+	 */
+	public int selectCntcSttusListTotCnt(CntcSttusVO searchVO) throws Exception {
+		return (Integer) selectOne("CntcSttusDAO.selectCntcSttusListTotCnt", searchVO);
+	}
 
 }
--- src/main/java/egovframework/com/ssi/syi/ist/service/impl/EgovCntcSttusServiceImpl.java
+++ src/main/java/egovframework/com/ssi/syi/ist/service/impl/EgovCntcSttusServiceImpl.java
@@ -2,28 +2,26 @@ package egovframework.com.ssi.syi.ist.service.impl;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import org.egovframe.rte.psl.dataaccess.util.EgovMap;
+import org.springframework.stereotype.Service;
+
 import egovframework.com.ssi.syi.ist.service.CntcSttus;
 import egovframework.com.ssi.syi.ist.service.CntcSttusVO;
 import egovframework.com.ssi.syi.ist.service.EgovCntcSttusService;
 
-import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
-import org.egovframe.rte.psl.dataaccess.util.EgovMap;
-
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Service;
-
-
-
 /**
  *
  * 연계현황에 대한 서비스 구현클래스를 정의한다.
+ * 
  * @author 공통서비스 개발팀 이중호
  * @since 2009.04.01
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
@@ -31,21 +29,21 @@ import org.springframework.stereotype.Service;
  *   2009.04.01  이중호          최초 생성
  *
  * Copyright (C) 2009 by MOPAS  All right reserved.
- * </pre>
+ *      </pre>
  */
 @Service("CntcSttusService")
-public class EgovCntcSttusServiceImpl extends EgovAbstractServiceImpl implements  EgovCntcSttusService {
+public class EgovCntcSttusServiceImpl extends EgovAbstractServiceImpl implements EgovCntcSttusService {
 
-    @Resource(name="CntcSttusDAO")
-    private CntcSttusDAO cntcSttusDAO;
+	@Resource(name = "CntcSttusDAO")
+	private CntcSttusDAO cntcSttusDAO;
 
 	/**
 	 * 연계현황 상세항목을 조회한다.
 	 */
 	@Override
 	public CntcSttus selectCntcSttusDetail(CntcSttus CntcSttus) throws Exception {
-    	CntcSttus ret = cntcSttusDAO.selectCntcSttusDetail(CntcSttus);
-    	return ret;
+		CntcSttus ret = cntcSttusDAO.selectCntcSttusDetail(CntcSttus);
+		return ret;
 	}
 
 	/**
@@ -53,7 +51,7 @@ public class EgovCntcSttusServiceImpl extends EgovAbstractServiceImpl implements
 	 */
 	@Override
 	public List<EgovMap> selectCntcSttusList(CntcSttusVO searchVO) throws Exception {
-        return cntcSttusDAO.selectCntcSttusList(searchVO);
+		return cntcSttusDAO.selectCntcSttusList(searchVO);
 	}
 
 	/**
@@ -61,7 +59,7 @@ public class EgovCntcSttusServiceImpl extends EgovAbstractServiceImpl implements
 	 */
 	@Override
 	public int selectCntcSttusListTotCnt(CntcSttusVO searchVO) throws Exception {
-        return cntcSttusDAO.selectCntcSttusListTotCnt(searchVO);
+		return cntcSttusDAO.selectCntcSttusListTotCnt(searchVO);
 	}
 
 }
