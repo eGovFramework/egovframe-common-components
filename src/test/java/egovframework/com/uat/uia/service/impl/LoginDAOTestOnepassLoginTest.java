588e4df4604df407ac211373712d7196945deb54
이백행 <dlqorgod@naver.com>
2024-09-23 07:02:54 +0900

    [10. 로그인][LoginDAO.selectPassedDayChangePWD] DAO 단위 테스트

---
+++ src/test/java/egovframework/com/uat/uia/service/impl/LoginDAOTestOnepassLoginTest.java
@@ -0,0 +1,65 @@
+package egovframework.com.uat.uia.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ContextConfiguration;
+
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.test.EgovTestAbstractDAO;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * [10. 로그인][LoginDAO.selectPassedDayChangePWD] DAO 단위 테스트
+ * 
+ * @author 이백행
+ * @since 2024-09-23
+ *
+ */
+@ContextConfiguration(classes = { LoginDAOTestOnepassLoginTest.class, EgovTestAbstractDAO.class })
+@Configuration
+@ComponentScan(useDefaultFilters = false, basePackages = {
+		"egovframework.com.uat.uia.service.impl" }, includeFilters = {
+				@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { LoginDAO.class }) })
+@NoArgsConstructor
+@Slf4j
+public class LoginDAOTestOnepassLoginTest extends EgovTestAbstractDAO {
+
+	/**
+	 * 일반 로그인, 인증서 로그인을 처리하는 DAO 클래스
+	 */
+	@Autowired
+	private LoginDAO loginDAO;
+
+	/**
+	 * 테스트
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void test() throws Exception {
+		// given
+		final String id = "USER";
+
+		// when
+		final LoginVO resultLoginVO = loginDAO.onepassLogin(id);
+
+		// then
+		if (log.isDebugEnabled()) {
+			log.debug("id={}", id);
+
+			log.debug("resultLoginVO={}", resultLoginVO);
+			log.debug("getId={}", resultLoginVO.getId());
+
+		}
+
+		assertEquals("디지털원패스 인증 회원 조회한다.", id, resultLoginVO.getId());
+	}
+
+}
