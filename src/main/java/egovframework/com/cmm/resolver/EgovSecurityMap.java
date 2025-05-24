41b4923fb3ca8a5a53753f24db16b5d54f9baa8a
이백행 <dlqorgod@naver.com>
2025-05-24 11:53:22 +0900

    개정이력 수정

--- src/main/java/egovframework/com/cmm/resolver/EgovSecurityMap.java
+++ src/main/java/egovframework/com/cmm/resolver/EgovSecurityMap.java
@@ -13,16 +13,26 @@ import egovframework.com.cmm.web.EgovComUtlController;
  * 
  * @author 표준프레임워크팀 신용호
  * @since 2024.07.09
- * @version 4.3
+ * @version 4.3.0
  * @see
+ * 
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
  *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2024.07.09  신용호          Map 타입에서 noteId 복호화 적용을 위한 EgovSecurityMap 추가
+ *   2025.05.24  이백행          PMD로 소프트웨어 보안약점 진단하고 제거하기-SwitchStmtsShouldHaveDefault(스위치 명령문에는 기본값이 있어야 합니다.), AvoidReassigningParameters(매개변수 재할당 방지)
+ *
+ *      </pre>
+ * 
  *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일          수정자        수정내용
  *  ----------     --------    ---------------------------
- *  2024.07.09     신용호        Map 타입에서 noteId 복호화 적용을 위한 EgovSecurityMap 추가
  *
+ * 
  *      </pre>
  */
 public class EgovSecurityMap {
