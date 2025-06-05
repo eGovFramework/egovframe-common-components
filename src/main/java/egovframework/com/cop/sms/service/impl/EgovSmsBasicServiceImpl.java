dd2ec619553bd0e0185efa1e96a4979d5da40488
이백행 <dlqorgod@naver.com>
2025-06-05 15:48:16 +0900

    개정이력 수정

--- src/main/java/egovframework/com/cop/sms/service/impl/EgovSmsBasicServiceImpl.java
+++ src/main/java/egovframework/com/cop/sms/service/impl/EgovSmsBasicServiceImpl.java
@@ -21,19 +21,19 @@ import egovframework.com.cop.sms.service.SmsVO;
  * 문자메시지를 위한 서비스 구현 클래스 (프레임워크 비종속 버전)
  *
  * @author 공통컴포넌트개발팀 한성곤
+ * @since 2009.11.24
  * @version 1.0
  * @see
  * 
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.11.24  한성곤          최초 생성
+ *   2025.06.05  이백행          PMD로 소프트웨어 보안약점 진단하고 제거하기-ImmutableField(불변필드), LocalVariableNamingConventions(지역 변수 명명 규칙)
  *
  *      </pre>
- * 
- * @since 2009.11.24
  */
 public class EgovSmsBasicServiceImpl implements EgovSmsInfoService {
 	private final SmsBasicDAO smsDao = new SmsBasicDAO();
