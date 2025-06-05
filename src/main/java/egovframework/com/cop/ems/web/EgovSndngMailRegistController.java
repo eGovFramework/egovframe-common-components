0e276fbd8ce25a599a63ab4d908d7eb66bc45765
이백행 <dlqorgod@naver.com>
2025-06-05 15:26:52 +0900

    개정이력 수정

--- src/main/java/egovframework/com/cop/ems/web/EgovSndngMailRegistController.java
+++ src/main/java/egovframework/com/cop/ems/web/EgovSndngMailRegistController.java
@@ -32,15 +32,16 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  * @since 2009.03.12
  * @version 1.0
  * @see
- *
+ * 
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
- *   수정일      	수정자          수정내용
- *  ----------     --------    ---------------------------
- *  2009.03.12  	박지욱          최초 생성
- *  2011.12.06  	이기하          메일 첨부파일이 기능 추가
- *  2015.05.08  	조정국          오류페이지 표시 경로 수정 - insertSndngMail()
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.03.12  박지욱          최초 생성
+ *   2011.12.06  이기하          메일 첨부파일이 기능 추가
+ *   2015.05.08  조정국          오류페이지 표시 경로 수정 - insertSndngMail()
+ *   2025.06.05  이백행          PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(지역 변수 명명 규칙)
  *
  *      </pre>
  */
