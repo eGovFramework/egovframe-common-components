7f63f9b0f404d1992170b00751903f355854adf4
이백행 <dlqorgod@naver.com>
2025-06-05 15:24:17 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/cop/ems/web/EgovSndngMailRegistController.java
+++ src/main/java/egovframework/com/cop/ems/web/EgovSndngMailRegistController.java
@@ -27,12 +27,13 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
 
 /**
  * 발송메일등록, 발송요청XML파일 생성하는 컨트롤러 클래스
+ * 
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.03.12
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      	수정자          수정내용
@@ -41,7 +42,7 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  *  2011.12.06  	이기하          메일 첨부파일이 기능 추가
  *  2015.05.08  	조정국          오류페이지 표시 경로 수정 - insertSndngMail()
  *
- *  </pre>
+ *      </pre>
  */
 @Controller
 public class EgovSndngMailRegistController {
@@ -63,13 +64,15 @@ public class EgovSndngMailRegistController {
 
 	/**
 	 * 발송메일 등록화면으로 들어간다
+	 * 
 	 * @param sndngMailVO SndngMailVO
 	 * @return String
 	 * @exception Exception
 	 */
 	@IncludedInfo(name = "메일발송", order = 360, gid = 40)
 	@RequestMapping(value = "/cop/ems/insertSndngMailView.do")
-	public String insertSndngMailView(@ModelAttribute("sndngMailVO") SndngMailVO sndngMailVO, ModelMap model) throws Exception {
+	public String insertSndngMailView(@ModelAttribute("sndngMailVO") SndngMailVO sndngMailVO, ModelMap model)
+			throws Exception {
 
 		model.addAttribute("resultInfo", sndngMailVO);
 		return "egovframework/com/cop/ems/EgovMailRegist";
@@ -77,13 +80,15 @@ public class EgovSndngMailRegistController {
 
 	/**
 	 * 발송메일을 등록한다
+	 * 
 	 * @param multiRequest MultipartHttpServletRequest
-	 * @param sndngMailVO SndngMailVO
+	 * @param sndngMailVO  SndngMailVO
 	 * @return String
 	 * @exception Exception
 	 */
 	@RequestMapping(value = "/cop/ems/insertSndngMail.do")
-	public String insertSndngMail(final MultipartHttpServletRequest multiRequest, @ModelAttribute("sndngMailVO") SndngMailVO sndngMailVO, ModelMap model, HttpServletRequest request)
+	public String insertSndngMail(final MultipartHttpServletRequest multiRequest,
+			@ModelAttribute("sndngMailVO") SndngMailVO sndngMailVO, ModelMap model, HttpServletRequest request)
 			throws Exception {
 
 		String link = "N";
@@ -92,19 +97,19 @@ public class EgovSndngMailRegistController {
 		}
 
 		LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
-   	 	// KISA 보안취약점 조치 (2018-12-10, 신용호)
-        Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		// KISA 보안취약점 조치 (2018-12-10, 신용호)
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
 
-        if(!isAuthenticated) {
-            return "redirect:/uat/uia/egovLoginUsr.do";
-        }
+		if (!isAuthenticated) {
+			return "redirect:/uat/uia/egovLoginUsr.do";
+		}
 
 		List<FileVO> _result = new ArrayList<FileVO>();
 		String _atchFileId = "";
 		final Map<String, MultipartFile> files = multiRequest.getFileMap();
 		if (!files.isEmpty()) {
 			_result = fileUtil.parseFileInf(files, "MSG_", 0, "", "");
-			_atchFileId = fileMngService.insertFileInfs(_result); //파일이 생성되고나면 생성된 첨부파일 ID를 리턴한다.
+			_atchFileId = fileMngService.insertFileInfs(_result); // 파일이 생성되고나면 생성된 첨부파일 ID를 리턴한다.
 
 		}
 
@@ -137,12 +142,14 @@ public class EgovSndngMailRegistController {
 
 	/**
 	 * 발송메일 내용조회로 돌아간다.
+	 * 
 	 * @param sndngMailVO SndngMailVO
 	 * @return String
 	 * @exception Exception
 	 */
 	@RequestMapping(value = "/cop/ems/backSndngMailRegist.do")
-	public String backSndngMailRegist(@ModelAttribute("sndngMailVO") SndngMailVO sndngMailVO, ModelMap model) throws Exception {
+	public String backSndngMailRegist(@ModelAttribute("sndngMailVO") SndngMailVO sndngMailVO, ModelMap model)
+			throws Exception {
 
 		return "redirect:/cop/ems/selectSndngMailList.do";
 	}
