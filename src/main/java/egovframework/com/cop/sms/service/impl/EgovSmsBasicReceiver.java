56d32e2f9117cd53822d1e956eeae8cad4b12699
이백행 <dlqorgod@naver.com>
2025-06-05 15:33:22 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/cop/sms/service/impl/EgovSmsBasicReceiver.java
+++ src/main/java/egovframework/com/cop/sms/service/impl/EgovSmsBasicReceiver.java
@@ -20,12 +20,13 @@ import x3.client.smeapi.impl.SMELogger;
 
 /**
  * 문자메시지 연동 결과 수신 처리를 위한 클래스 (프레임워크 비종속 버전)
+ * 
  * @author 공통컴포넌트개발팀 한성곤
  * @since 2009.11.24
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
@@ -34,7 +35,7 @@ import x3.client.smeapi.impl.SMELogger;
  *   2011.10.10	 이기하			 보안점검 후속초치(디버거코드 주석처리)
  *   2017.03.07    조성원 	시큐어코딩(ES)-부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
  *
- * </pre>
+ *      </pre>
  */
 public class EgovSmsBasicReceiver implements SMEListener {
 	private SmsBasicDAO smsDao = new SmsBasicDAO();
@@ -146,109 +147,112 @@ public class EgovSmsBasicReceiver implements SMEListener {
 			if (msg.isConnected()) {
 				SMEReport rpt = msg;
 				String msgId = rpt.getMessageId();
-				int nRes = rpt.getResult(); 						// 결과코드
-				String doneTime = rpt.getDeliverTime();	// 이동통신사 결과처리시간-단말기에 전달된 시간(이동통신사 생성)
-				String netCode = rpt.getDestination();		// 이동통신사 정보
+				int nRes = rpt.getResult(); // 결과코드
+				String doneTime = rpt.getDeliverTime(); // 이동통신사 결과처리시간-단말기에 전달된 시간(이동통신사 생성)
+				String netCode = rpt.getDestination(); // 이동통신사 정보
 
-				//System.out.println("Receiver Number is :" + ((SMEReportImpl)rpt).receiver.activeCount()); // 주석처리
+				// System.out.println("Receiver Number is :" +
+				// ((SMEReportImpl)rpt).receiver.activeCount()); // 주석처리
 
 				String resultMsg = "";
 
 				switch (nRes) {
-					case 0:
-						resultMsg = "";
-						break;
-					case 4001:
-						resultMsg = "잘못된 전화번호; 착신 이통사를 결정할 수 없음";
-						break;
-					case 4002:
-						resultMsg = "MessageID 중복";
-						break;
-					case 4005:
-						resultMsg = "스팸 메시지로 처리 거부됨";
-						break;
-					case 4006:
-						resultMsg = "스팸 콜백번호로 처리 거부됨";
-						break;
-					case 5000:
-						resultMsg = "SMG Server 내부 에러 (인증실패,연결실패)";
-						break;
-					case 5050:
-						resultMsg = "착신 이통사 연동 실패";
-						break;
-					case 6000:
-						resultMsg = "이통사 시스템 장애";
-						break;
-					case 6001:
-						resultMsg = "이통사 메시지 형식 오류";
-						break;
-					case 6002:
-						resultMsg = "이통사 착신번호 인증 에러";
-						break;
-					case 6003:
-						resultMsg = "이통사 스팸 메시지로 처리 거부됨";
-						break;
-					case 6004:
-						resultMsg = "이통사 순간 전송량 제한 초과";
-						break;
-					case 6005:
-						resultMsg = "이통사 월 전송량 제한 초과";
-						break;
-					case 6006:
-						resultMsg = "이통사 Resource 제한에 의한 전송 제어";
-						break;
-					case 6007:
-						resultMsg = "이통사 Resource full";
-						break;
-					case 6008:
-						resultMsg = "이통사 번호이동 시스템 장애";
-						break;
-					case 6009:
-						resultMsg = "이통사 메시지 타입 오류";
-						break;
-					case 6010:
-						resultMsg = "이통사 전송 실패";
-						break;
-					case 6011:
-						resultMsg = "이통사 메시지 전송불가(단말기에서 착신 거부)";
-						break;
-					case 6012:
-						resultMsg = "이통사 전송 실패(무선망단)";
-						break;
-					case 6013:
-						resultMsg = "이통사 전송 실패(무선망 -> 단말기단)";
-						break;
-					case 6014:
-						resultMsg = "이통사 수신 단말기 형식 오류";
-						break;
-					case 6015:
-						resultMsg = "이통사 Unknown Error";
-						break;
-					case 7000:
-						resultMsg = "수신 단말기 전원꺼짐";
-						break;
-					case 7001:
-						resultMsg = "수신 단말기 메시지 버퍼 풀";
-						break;
-					case 7002:
-						resultMsg = "수신 단말기 음영지역";
-						break;
-					case 7003:
-						resultMsg = "수신 단말기 메시지 삭제됨";
-						break;
-					default:
-						resultMsg = "알 수 없는 오류 발생";
+				case 0:
+					resultMsg = "";
+					break;
+				case 4001:
+					resultMsg = "잘못된 전화번호; 착신 이통사를 결정할 수 없음";
+					break;
+				case 4002:
+					resultMsg = "MessageID 중복";
+					break;
+				case 4005:
+					resultMsg = "스팸 메시지로 처리 거부됨";
+					break;
+				case 4006:
+					resultMsg = "스팸 콜백번호로 처리 거부됨";
+					break;
+				case 5000:
+					resultMsg = "SMG Server 내부 에러 (인증실패,연결실패)";
+					break;
+				case 5050:
+					resultMsg = "착신 이통사 연동 실패";
+					break;
+				case 6000:
+					resultMsg = "이통사 시스템 장애";
+					break;
+				case 6001:
+					resultMsg = "이통사 메시지 형식 오류";
+					break;
+				case 6002:
+					resultMsg = "이통사 착신번호 인증 에러";
+					break;
+				case 6003:
+					resultMsg = "이통사 스팸 메시지로 처리 거부됨";
+					break;
+				case 6004:
+					resultMsg = "이통사 순간 전송량 제한 초과";
+					break;
+				case 6005:
+					resultMsg = "이통사 월 전송량 제한 초과";
+					break;
+				case 6006:
+					resultMsg = "이통사 Resource 제한에 의한 전송 제어";
+					break;
+				case 6007:
+					resultMsg = "이통사 Resource full";
+					break;
+				case 6008:
+					resultMsg = "이통사 번호이동 시스템 장애";
+					break;
+				case 6009:
+					resultMsg = "이통사 메시지 타입 오류";
+					break;
+				case 6010:
+					resultMsg = "이통사 전송 실패";
+					break;
+				case 6011:
+					resultMsg = "이통사 메시지 전송불가(단말기에서 착신 거부)";
+					break;
+				case 6012:
+					resultMsg = "이통사 전송 실패(무선망단)";
+					break;
+				case 6013:
+					resultMsg = "이통사 전송 실패(무선망 -> 단말기단)";
+					break;
+				case 6014:
+					resultMsg = "이통사 수신 단말기 형식 오류";
+					break;
+				case 6015:
+					resultMsg = "이통사 Unknown Error";
+					break;
+				case 7000:
+					resultMsg = "수신 단말기 전원꺼짐";
+					break;
+				case 7001:
+					resultMsg = "수신 단말기 메시지 버퍼 풀";
+					break;
+				case 7002:
+					resultMsg = "수신 단말기 음영지역";
+					break;
+				case 7003:
+					resultMsg = "수신 단말기 메시지 삭제됨";
+					break;
+				default:
+					resultMsg = "알 수 없는 오류 발생";
 				}
 
 				if (nRes != SMEMessage.RESULT_SUCCESS) {
-					// System.out.println("SMSMessage (msgId = " + msgId + ") report = " + rpt.getResult());
+					// System.out.println("SMSMessage (msgId = " + msgId + ") report = " +
+					// rpt.getResult());
 					LOGGER.info("MessageId   : {}", msgId);
 					LOGGER.info("Result      : {}", nRes);
 					LOGGER.info("Result Msg. : {}", resultMsg);
 					LOGGER.info("Done Time   : {}", doneTime);
 					LOGGER.info("Net Code    : {}", netCode);
 				} else {
-					// System.out.println("SMEMessage (msgId = " + msgId + ") report = " + rpt.getResult());
+					// System.out.println("SMEMessage (msgId = " + msgId + ") report = " +
+					// rpt.getResult());
 					LOGGER.info("MessageId   : {}", msgId);
 					LOGGER.info("Result      : {}", nRes);
 					LOGGER.info("Result Msg. : {}", resultMsg);
@@ -260,21 +264,21 @@ public class EgovSmsBasicReceiver implements SMEListener {
 				if (smeConfigPath != null) {
 					SmsRecptn recptn = new SmsRecptn();
 
-					recptn.setSmsId(msgId.substring(0, 20)); 			// SMS_ID + "-" + 수신전화번호
-					recptn.setRecptnTelno(msgId.substring(21)); 	// "-" 제외
+					recptn.setSmsId(msgId.substring(0, 20)); // SMS_ID + "-" + 수신전화번호
+					recptn.setRecptnTelno(msgId.substring(21)); // "-" 제외
 
 					recptn.setResultCode(Integer.toString(nRes));
 					recptn.setResultMssage(resultMsg);
 
 					try {
 						smsDao.updateSmsRecptnInf(recptn);
-					//2017.02.08 	이정은 	시큐어코딩(ES)-부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
+						// 2017.02.08 이정은 시큐어코딩(ES)-부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
 					} catch (IOException ex) {
 //						LOGGER.error("Exception: {}", ex.getClass().getName());
 //						LOGGER.error("Exception  Message: {}", ex.getMessage());
 						LOGGER.error("[IOException] : Connection Close");
 					} catch (Exception ex) {
-						LOGGER.error("["+ ex.getClass() +"] Connection Close : ", ex.getMessage());
+						LOGGER.error("[" + ex.getClass() + "] Connection Close : ", ex.getMessage());
 					}
 				}
 			} else {
@@ -290,58 +294,37 @@ public class EgovSmsBasicReceiver implements SMEListener {
 	 * @param args
 	 */
 	// 2022.01. Exit methods should not be called 처리 - 예제이므로 주석 처리
-	/*	public static void mainExample(String[] args) {
-			if (args.length < 1) {
-				LOGGER.error("SMEConfig.conf file full path needed.");
-				LOGGER.error("ex) java [JVM Options] [className] /home/egovframe/conf/SMEConfig.conf");
-				System.exit(-1);
-			}
-
-			EgovSmsBasicReceiver receiver = new EgovSmsBasicReceiver();
-
-			try {
-				try {
-					SMEConfig.configSet(args[0]);
-					receiver.readPropertyFile();
-
-				} catch (Exception ex) {
-	//				LOGGER.error("DEBUG: {}", ex.getMessage());
-					//2017.03.07 	조성원 	시큐어코딩(ES)-부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
-					LOGGER.error("["+ ex.getClass() +"] : Connection Close ", ex.getMessage());
-					return;
-				}
-
-				// 결과 수신을 위해서 리포트 세션을 접속한다.
-				// 프로그램 시작시 최초 한번만 해준다.
-				receiver.open();
-
-				long startTimestamp = System.currentTimeMillis();
-				long nowTimestamp = 0;
-				long limitTimeInterval = (60)*60*1000; //60분간으로 제한
-
-				// 데몬이 종료안되도록 10초씩 쉬면서 루프를 돌렸습니다.
-				// 실제 사용 목적에 맞게끔 고쳐주시면 됩니다.
-				while (true) {
-					// 연결을 유지해야하는데 서버측에서 세션을 끊어버리거나
-					// 네트워크 간섭 또는 장애 상황으로 연결이 끊겼을 경우 재접속할 수 있도록 처리
-					//if (receiver.isConnected == false) {
-					if (!receiver.isConnected) { // recommended by PMD
-						receiver.close();
-						Thread.sleep(10000);
-						receiver.open();
-					}
-
-					Thread.sleep(10000);
-					nowTimestamp = System.currentTimeMillis();
-					if ( (nowTimestamp - startTimestamp) > limitTimeInterval) break;
-				}
-
-			} catch (SMEException ex) {
-				LOGGER.error("DEBUG: {}", ex.getMessage());
-			} catch (InterruptedException ie) {
-				EgovBasicLogger.ignore("InterruptedException", ie);
-			} finally {
-				receiver.close();
-			}
-		}*/
+	/*
+	 * public static void mainExample(String[] args) { if (args.length < 1) {
+	 * LOGGER.error("SMEConfig.conf file full path needed."); LOGGER.
+	 * error("ex) java [JVM Options] [className] /home/egovframe/conf/SMEConfig.conf"
+	 * ); System.exit(-1); }
+	 * 
+	 * EgovSmsBasicReceiver receiver = new EgovSmsBasicReceiver();
+	 * 
+	 * try { try { SMEConfig.configSet(args[0]); receiver.readPropertyFile();
+	 * 
+	 * } catch (Exception ex) { // LOGGER.error("DEBUG: {}", ex.getMessage());
+	 * //2017.03.07 조성원 시큐어코딩(ES)-부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
+	 * LOGGER.error("["+ ex.getClass() +"] : Connection Close ", ex.getMessage());
+	 * return; }
+	 * 
+	 * // 결과 수신을 위해서 리포트 세션을 접속한다. // 프로그램 시작시 최초 한번만 해준다. receiver.open();
+	 * 
+	 * long startTimestamp = System.currentTimeMillis(); long nowTimestamp = 0; long
+	 * limitTimeInterval = (60)*60*1000; //60분간으로 제한
+	 * 
+	 * // 데몬이 종료안되도록 10초씩 쉬면서 루프를 돌렸습니다. // 실제 사용 목적에 맞게끔 고쳐주시면 됩니다. while (true) {
+	 * // 연결을 유지해야하는데 서버측에서 세션을 끊어버리거나 // 네트워크 간섭 또는 장애 상황으로 연결이 끊겼을 경우 재접속할 수 있도록
+	 * 처리 //if (receiver.isConnected == false) { if (!receiver.isConnected) { //
+	 * recommended by PMD receiver.close(); Thread.sleep(10000); receiver.open(); }
+	 * 
+	 * Thread.sleep(10000); nowTimestamp = System.currentTimeMillis(); if (
+	 * (nowTimestamp - startTimestamp) > limitTimeInterval) break; }
+	 * 
+	 * } catch (SMEException ex) { LOGGER.error("DEBUG: {}", ex.getMessage()); }
+	 * catch (InterruptedException ie) {
+	 * EgovBasicLogger.ignore("InterruptedException", ie); } finally {
+	 * receiver.close(); } }
+	 */
 }
