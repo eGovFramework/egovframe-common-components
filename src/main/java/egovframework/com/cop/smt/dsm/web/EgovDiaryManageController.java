78e3446342516749ac27e70450bed5ee92576a56
이백행 <dlqorgod@naver.com>
2025-06-10 19:58:25 +0900

    개정이력 수정

--- src/main/java/egovframework/com/cop/smt/dsm/web/EgovDiaryManageController.java
+++ src/main/java/egovframework/com/cop/smt/dsm/web/EgovDiaryManageController.java
@@ -41,16 +41,18 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  * @since 2009.04.10
  * @version 1.0
  * @see
- *
+ * 
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
- *  수정일               수정자            수정내용
- *  ----------   --------   ---------------------------
- *  2009.04.10   장동한            최초 생성
- *  2011.08.26   정진오            IncludedInfo annotation 추가
- *  2019.12.09   신용호            KISA 보안약점 조치 (위험한 형식 파일 업로드)
- *  2020.10.28   신용호            파일 업로드 수정 (multiRequest.getFiles)
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.03.20  홍길동          최초 생성
+ *   2009.04.10  장동한          최초 생성
+ *   2011.08.26  정진오          IncludedInfo annotation 추가
+ *   2019.12.09  신용호          KISA 보안약점 조치 (위험한 형식 파일 업로드)
+ *   2020.10.28  신용호          파일 업로드 수정 (multiRequest.getFiles)
+ *   2025.06.10  이백행          PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(지역 변수 명명 규칙)
  *
  *      </pre>
  */
