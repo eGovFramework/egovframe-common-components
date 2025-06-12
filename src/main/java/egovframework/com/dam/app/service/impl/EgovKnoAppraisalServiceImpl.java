4c7ec46ed3cc4653df29e80c2822c8defdae635f
이백행 <dlqorgod@naver.com>
2025-06-13 07:01:02 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/dam/app/service/impl/EgovKnoAppraisalServiceImpl.java
+++ src/main/java/egovframework/com/dam/app/service/impl/EgovKnoAppraisalServiceImpl.java
@@ -13,37 +13,41 @@ import egovframework.com.dam.app.service.KnoAppraisal;
 import egovframework.com.dam.app.service.KnoAppraisalVO;
 
 /**
+ * <pre>
  * 개요
  * - 지식정보평가에 대한 ServiceImpl 클래스를 정의한다.
  *
  * 상세내용
  * - 지식정보평가에 대한 등록, 수정, 삭제, 조회 기능을 제공한다.
  * - 지식정보평가의 조회기능은 목록조회, 상세조회로 구분된다.
+ * </pre>
+ * 
  * @author 박종선
  * @version 1.0
  * @created 12-8-2010 오후 3:44:38
  */
-
 @Service("KnoAppraisalService")
 public class EgovKnoAppraisalServiceImpl extends EgovAbstractServiceImpl implements EgovKnoAppraisalService {
 
-	@Resource(name="KnoAppraisalDAO")
+	@Resource(name = "KnoAppraisalDAO")
 	private KnoAppraisalDAO KnoAppraisalDAO;
 
 	/**
 	 * 등록된 지식정보평가 정보를 조회 한다.
+	 * 
 	 * @param KnoAppraisalVO - 지식정보평가 VO
 	 * @return String - 지식정보평가 VO
 	 *
 	 * @param KnoAppraisalVO
 	 */
 	@Override
-	public List<EgovMap> selectKnoAppraisalList(KnoAppraisalVO  searchVO) throws Exception {
+	public List<EgovMap> selectKnoAppraisalList(KnoAppraisalVO searchVO) throws Exception {
 		return KnoAppraisalDAO.selectKnoAppraisalList(searchVO);
 	}
 
 	/**
 	 * 지식정보평가 목록 총 개수를 조회한다.
+	 * 
 	 * @param MapTeamVO - 지식정보평가 Vo
 	 * @return int - 지식정보평가 토탈 카운트 수
 	 *
@@ -56,6 +60,7 @@ public class EgovKnoAppraisalServiceImpl extends EgovAbstractServiceImpl impleme
 
 	/**
 	 * 지식정보평가 상세 정보를 조회 한다.
+	 * 
 	 * @param KnoAppraisalVO - 지식정보평가 VO
 	 * @return String - 지식정보평가 VO
 	 *
@@ -69,6 +74,7 @@ public class EgovKnoAppraisalServiceImpl extends EgovAbstractServiceImpl impleme
 
 	/**
 	 * 지식정보평가 정보를 신규로 등록한다.
+	 * 
 	 * @param knoAps - 지식정보평가 model
 	 *
 	 * @param knoAps
@@ -80,6 +86,7 @@ public class EgovKnoAppraisalServiceImpl extends EgovAbstractServiceImpl impleme
 
 	/**
 	 * 기 등록 된 지식정보평가 정보를 수정 한다.
+	 * 
 	 * @param AppraisalknoAps - 지식정보평가 model
 	 *
 	 * @param knoAps
@@ -91,6 +98,7 @@ public class EgovKnoAppraisalServiceImpl extends EgovAbstractServiceImpl impleme
 
 	/**
 	 * 기 등록된 지식정보평가 정보를 삭제한다.
+	 * 
 	 * @param AppraisalknoAps - 지식정보평가 model
 	 *
 	 * @param knoAps
