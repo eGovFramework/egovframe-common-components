6c847237a344f03603e0feaff47d2a713a551453
이백행 <dlqorgod@naver.com>
2025-06-14 07:56:50 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/dam/map/mat/service/impl/EgovMapMaterialServiceImpl.java
+++ src/main/java/egovframework/com/dam/map/mat/service/impl/EgovMapMaterialServiceImpl.java
@@ -12,12 +12,15 @@ import egovframework.com.dam.map.mat.service.MapMaterial;
 import egovframework.com.dam.map.mat.service.MapMaterialVO;
 
 /**
+ * <pre>
  * 개요
  * - 지식맵(지식유형)에 대한 ServiceImpl 클래스를 정의한다.
  *
  * 상세내용
  * - 지식맵(지식유형)에 대한 등록, 수정, 삭제, 조회 기능을 제공한다.
  * - 지식맵(지식유형)의 조회기능은 목록조회, 상세조회로 구분된다.
+ * </pre>
+ * 
  * @author 박종선
  * @version 1.0
  * @created 12-8-2010 오후 3:44:45
@@ -26,11 +29,12 @@ import egovframework.com.dam.map.mat.service.MapMaterialVO;
 @Service("MapMaterialService")
 public class EgovMapMaterialServiceImpl extends EgovAbstractServiceImpl implements EgovMapMaterialService {
 
-	@Resource(name="MapMaterialDAO")
+	@Resource(name = "MapMaterialDAO")
 	private MapMaterialDAO MapMaterialDAO;
 
 	/**
 	 * 등록된 지식맵(지식유형) 정보를 조회 한다.
+	 * 
 	 * @param mapMaterialVO- 지식맵(지식유형) VO
 	 * @return String - 지식맵(지식유형)목록
 	 *
@@ -43,6 +47,7 @@ public class EgovMapMaterialServiceImpl extends EgovAbstractServiceImpl implemen
 
 	/**
 	 * 지식맵(지식유형) 목록 총 개수를 조회한다.
+	 * 
 	 * @param MapMaterialVO - 지식맵(지식유형) Vo
 	 * @return int - 지식맵(지식유형) 토탈 카운트 수
 	 *
@@ -55,6 +60,7 @@ public class EgovMapMaterialServiceImpl extends EgovAbstractServiceImpl implemen
 
 	/**
 	 * 지식맵(지식유형)상세 정보를 조회 한다.
+	 * 
 	 * @param MapMaterialVO - 지식맵(지식유형) VO
 	 * @return String - 지식맵(지식유형)VO
 	 *
@@ -68,7 +74,8 @@ public class EgovMapMaterialServiceImpl extends EgovAbstractServiceImpl implemen
 
 	/**
 	 * 지식맵(지식유형) 정보를 신규로 등록한다.
-	 * @param konTypeNm - 지식맵(지식유형) model
+	 * 
+	 * @param konTypeNm     - 지식맵(지식유형) model
 	 *
 	 * @param MapMaterialVO
 	 */
@@ -79,7 +86,8 @@ public class EgovMapMaterialServiceImpl extends EgovAbstractServiceImpl implemen
 
 	/**
 	 * 기 등록 된 지식맵(지식유형)링 정보를 수정 한다.
-	 * @param konTypeNm - 지식맵(지식유형) model
+	 * 
+	 * @param konTypeNm     - 지식맵(지식유형) model
 	 *
 	 * @param MapMaterialVO
 	 */
@@ -90,7 +98,8 @@ public class EgovMapMaterialServiceImpl extends EgovAbstractServiceImpl implemen
 
 	/**
 	 * 기 등록된 지식맵(지식유형) 정보를 삭제한다.
-	 * @param konTypeNm - 지식맵(지식유형) model
+	 * 
+	 * @param konTypeNm     - 지식맵(지식유형) model
 	 *
 	 * @param MapMaterialVO
 	 */
@@ -101,6 +110,7 @@ public class EgovMapMaterialServiceImpl extends EgovAbstractServiceImpl implemen
 
 	/**
 	 * 지식유형코드 중복 여부 체크(위치 : 1260.지식맵관리(유형) > 등록)
+	 * 
 	 * @param knoTypeCd
 	 * @return 중복여부
 	 * @throws Exception
