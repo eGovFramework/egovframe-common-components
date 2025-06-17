38fb28dd0b69b0bf3516e870ec23c2de8714e448
이백행 <dlqorgod@naver.com>
2025-06-17 20:00:58 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/dam/mgm/service/impl/EgovKnoManagementServiceImpl.java
+++ src/main/java/egovframework/com/dam/mgm/service/impl/EgovKnoManagementServiceImpl.java
@@ -13,12 +13,15 @@ import egovframework.com.dam.mgm.service.KnoManagement;
 import egovframework.com.dam.mgm.service.KnoManagementVO;
 
 /**
+ * <pre>
  * 개요
  * - 지식정보에 대한 ServiceImpl 클래스를 정의한다.
  *
  * 상세내용
  * - 지식정보에 대한 등록, 수정, 삭제, 조회 기능을 제공한다.
  * - 지식정보의 조회기능은 목록조회, 상세조회로 구분된다.
+ * </pre>
+ * 
  * @author 박종선
  * @version 1.0
  * @created 12-8-2010 오후 3:44:39
@@ -27,11 +30,12 @@ import egovframework.com.dam.mgm.service.KnoManagementVO;
 @Service("KnoManagementService")
 public class EgovKnoManagementServiceImpl extends EgovAbstractServiceImpl implements EgovKnoManagementService {
 
-	@Resource(name="KnoManagementDAO")
+	@Resource(name = "KnoManagementDAO")
 	private KnoManagementDAO KnoManagementDAO;
 
 	/**
 	 * 등록된 지식정보 정보를 조회 한다.
+	 * 
 	 * @param KnoManagementVO - 지식정보 VO
 	 * @return String - 지식정보 VO
 	 *
@@ -44,6 +48,7 @@ public class EgovKnoManagementServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 지식정보 목록 총 개수를 조회한다.
+	 * 
 	 * @param KnoManagementVO - 지식정보 Vo
 	 * @return int - 지식정보 토탈 카운트 수
 	 *
@@ -56,6 +61,7 @@ public class EgovKnoManagementServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 지식정보 상세 정보를 조회 한다.
+	 * 
 	 * @param KnoManagementVO - 지식정보 VO
 	 * @return String - 지식정보 VO
 	 *
@@ -69,6 +75,7 @@ public class EgovKnoManagementServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 지식정보 정보를 신규로 등록한다.
+	 * 
 	 * @param KnoNm - 지식정보 model
 	 *
 	 * @param knoNm
@@ -80,6 +87,7 @@ public class EgovKnoManagementServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 기 등록 된 지식정보 정보를 수정 한다.
+	 * 
 	 * @param ManagementKnoNm - 지식정보 model
 	 *
 	 * @param knoNm
@@ -91,6 +99,7 @@ public class EgovKnoManagementServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 기 등록된 지식정보 정보를 삭제한다.
+	 * 
 	 * @param ManagementKnoNm - 지식정보 model
 	 *
 	 * @param knoNm
