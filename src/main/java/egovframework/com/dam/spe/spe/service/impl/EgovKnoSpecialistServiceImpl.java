07001d334dd31cfa32fe89d5805970f103f09c25
이백행 <dlqorgod@naver.com>
2025-06-18 19:40:09 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/dam/spe/spe/service/impl/EgovKnoSpecialistServiceImpl.java
+++ src/main/java/egovframework/com/dam/spe/spe/service/impl/EgovKnoSpecialistServiceImpl.java
@@ -12,12 +12,15 @@ import egovframework.com.dam.spe.spe.service.KnoSpecialist;
 import egovframework.com.dam.spe.spe.service.KnoSpecialistVO;
 
 /**
+ * <pre>
  * 개요
  * - 지식전문가에 대한 ServiceImpl 클래스를 정의한다.
  *
  * 상세내용
  * - 지식전문가에 대한 등록, 수정, 삭제, 조회 기능을 제공한다.
  * - 지식전문가의 조회기능은 목록조회, 상세조회로 구분된다.
+ * </pre>
+ * 
  * @author 박종선
  * @version 1.0
  * @created 12-8-2010 오후 3:44:43
@@ -26,11 +29,12 @@ import egovframework.com.dam.spe.spe.service.KnoSpecialistVO;
 @Service("KnoSpecialistService")
 public class EgovKnoSpecialistServiceImpl extends EgovAbstractServiceImpl implements EgovKnoSpecialistService {
 
-	@Resource(name="KnoSpecialistDAO")
+	@Resource(name = "KnoSpecialistDAO")
 	private KnoSpecialistDAO KnoSpecialistDAO;
 
 	/**
 	 * 등록된 지식전문가 정보를 조회 한다.
+	 * 
 	 * @param KnoSpecialistVO- 지식전문가 VO
 	 * @return String - 지식전문가 목록
 	 *
@@ -43,6 +47,7 @@ public class EgovKnoSpecialistServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 지식전문가 목록 총 개수를 조회한다.
+	 * 
 	 * @param KnoSpecialistVO - 지식전문가 Vo
 	 * @return int - 지식전문가 토탈 카운트 수
 	 *
@@ -55,6 +60,7 @@ public class EgovKnoSpecialistServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 지식전문가 상세 정보를 조회 한다.
+	 * 
 	 * @param KonSpecialistVO - 지식전문가 VO
 	 * @return String - 지식전문가 VO
 	 *
@@ -68,6 +74,7 @@ public class EgovKnoSpecialistServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 지식전문가 정보를 신규로 등록한다.
+	 * 
 	 * @param speNm - 지식전문가 model
 	 *
 	 * @param speNm
@@ -79,6 +86,7 @@ public class EgovKnoSpecialistServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 기 등록 된 지식전문가 정보를 수정 한다.
+	 * 
 	 * @param speNm - 지식전문가 model
 	 *
 	 * @param speNm
@@ -90,6 +98,7 @@ public class EgovKnoSpecialistServiceImpl extends EgovAbstractServiceImpl implem
 
 	/**
 	 * 기 등록된 지식전문가 정보를 삭제한다.
+	 * 
 	 * @param siteUrl - 지식전문가 model
 	 *
 	 * @param speNm
