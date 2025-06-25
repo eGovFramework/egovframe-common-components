8861796b4cebaacaf089922e5073a252e6f258f5
rukegithub <rukegg@gmail.com>
2025-06-25 15:14:13 +0900

    PR #539 fork repo 충돌 해소 권한 부족으로 자체 반영 처리

--- src/main/java/egovframework/com/cmm/util/EgovUrlRewriteFilter.java
+++ src/main/java/egovframework/com/cmm/util/EgovUrlRewriteFilter.java
@@ -16,21 +16,23 @@ import org.springframework.util.AntPathMatcher;
 import egovframework.com.cmm.EgovWebUtil;
 
 /**
- * @Class Name : UrlRewriteFilter.java
- * @Description : UrlRewriteFilter Class
- * @Modification Information
- * @
- * @ 수정일               수정자              수정내용
- * @ ----------   ---------   -------------------------------
- * @ 2014.09.30               최초생성
- * @ 2020.11.02   신용호              KISA 보안약점 조치 (CRLF 제거 조치)
- *
+ * URL 재작성 필터
+ * 
  * @author 전자정부 표준프레임워크 유지보수
  * @since 2014. 09.30
  * @version 1.0
  * @see
  *
- *  Copyright (C) by MOPAS All rights reserved.
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2014.09.30  표프센          최초생성
+ *   2020.11.02  신용호          KISA 보안약점 조치 (CRLF 제거 조치)
+ *   2025.05.28  이백행          PMD로 소프트웨어 보안약점 진단하고 제거하기-StringInstantiation(문자열 인스턴스화)
+ *
+ *      </pre>
  */
 public class EgovUrlRewriteFilter implements Filter {
 
@@ -43,10 +45,10 @@ public class EgovUrlRewriteFilter implements Filter {
 
 	private String[] uriPatterns;
 
-    @Override
-    public void init(FilterConfig config) throws ServletException {
+	@Override
+	public void init(FilterConfig config) throws ServletException {
 
-    	String delimiter = ",";
+		String delimiter = ",";
 		this.config = config;
 
 		this.targetURI = config.getInitParameter("targetURI");
@@ -55,10 +57,11 @@ public class EgovUrlRewriteFilter implements Filter {
 
 		this.uriPatterns = targetURI.split(delimiter);
 
-    }
+	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws ServletException, IOException {
 
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
@@ -73,26 +76,24 @@ public class EgovUrlRewriteFilter implements Filter {
 
 			if (pm.match(uriPattern.trim(), uri)) {
 
-				 if (getProtocol.toLowerCase().equals("http")) {
+				if (getProtocol.toLowerCase().equals("http")) {
 
-					 response.setContentType("text/html");
+					response.setContentType("text/html");
 
-					 String httpsPath = "https" + "://" + getDomain + ":" + httpsPort + uri;
-					 String site = new String(httpsPath);
-					 res.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-					 res.setHeader("Location", EgovWebUtil.removeCRLF(site));
+					String httpsPath = "https" + "://" + getDomain + ":" + httpsPort + uri;
+					res.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+					res.setHeader("Location", EgovWebUtil.removeCRLF(httpsPath));
 
-				 }
+				}
 
-			}else if(getProtocol.toLowerCase().equals("https")){
+			} else if (getProtocol.toLowerCase().equals("https")) {
 
 				response.setContentType("text/html");
 
 				String httpPath = "http" + "://" + getDomain + ":" + httpPort + uri;
 
-				String site = new String(httpPath);
 				res.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-				res.setHeader("Location", EgovWebUtil.removeCRLF(site));
+				res.setHeader("Location", EgovWebUtil.removeCRLF(httpPath));
 
 			}
 		}
@@ -101,12 +102,12 @@ public class EgovUrlRewriteFilter implements Filter {
 
 	}
 
-    @Override
-    public void destroy() {
-    	this.targetURI = null;
-    	this.httpsPort = null;
+	@Override
+	public void destroy() {
+		this.targetURI = null;
+		this.httpsPort = null;
 		this.httpPort = null;
 		this.uriPatterns = null;
-    }
+	}
 
 }
