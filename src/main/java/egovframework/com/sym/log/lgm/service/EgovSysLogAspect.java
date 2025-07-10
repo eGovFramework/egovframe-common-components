3eda17bb3ec413f20aecef966b00f65440e37a77
이백행 <dlqorgod@naver.com>
2025-07-11 06:33:23 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/sym/log/lgm/service/EgovSysLogAspect.java
+++ src/main/java/egovframework/com/sym/log/lgm/service/EgovSysLogAspect.java
@@ -12,12 +12,14 @@ import egovframework.com.cmm.util.EgovUserDetailsHelper;
  * @Class Name : EgovSysLogAspect.java
  * @Description : 시스템 로그 생성을 위한 ASPECT 클래스
  * @Modification Information
- *
+ * 
+ *               <pre>
  *    수정일         수정자         수정내용
  *    -------        -------     -------------------
  *    2009. 3. 11.   이삼섭         최초생성
  *    2011. 7. 01.   이기하         패키지 분리(sym.log -> sym.log.lgm)
- *
+ *               </pre>
+ * 
  * @author 공통 서비스 개발팀 이삼섭
  * @since 2009. 3. 11.
  * @version
@@ -26,12 +28,11 @@ import egovframework.com.cmm.util.EgovUserDetailsHelper;
  */
 public class EgovSysLogAspect {
 
-	@Resource(name="EgovSysLogService")
+	@Resource(name = "EgovSysLogService")
 	private EgovSysLogService sysLogService;
 
 	/**
-	 * 시스템 로그정보를 생성한다.
-	 * sevice Class의 insert로 시작되는 Method
+	 * 시스템 로그정보를 생성한다. sevice Class의 insert로 시작되는 Method
 	 *
 	 * @param ProceedingJoinPoint
 	 * @return Object
@@ -59,13 +60,13 @@ public class EgovSysLogAspect {
 			String uniqId = "";
 			String ip = "";
 
-	    	/* Authenticated  */
-	        Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
-	    	if(isAuthenticated.booleanValue()) {
-				LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+			/* Authenticated */
+			Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+			if (isAuthenticated.booleanValue()) {
+				LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 				uniqId = (user == null || user.getUniqId() == null) ? "" : user.getUniqId();
-			    ip = (user == null || user.getIp() == null) ? "" : user.getIp();
-	    	}
+				ip = (user == null || user.getIp() == null) ? "" : user.getIp();
+			}
 
 			sysLog.setSrvcNm(className);
 			sysLog.setMethodNm(methodName);
@@ -81,8 +82,7 @@ public class EgovSysLogAspect {
 	}
 
 	/**
-	 * 시스템 로그정보를 생성한다.
-	 * sevice Class의 update로 시작되는 Method
+	 * 시스템 로그정보를 생성한다. sevice Class의 update로 시작되는 Method
 	 *
 	 * @param ProceedingJoinPoint
 	 * @return Object
@@ -110,13 +110,13 @@ public class EgovSysLogAspect {
 			String uniqId = "";
 			String ip = "";
 
-	    	/* Authenticated  */
-	        Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
-	    	if(isAuthenticated.booleanValue()) {
-				LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+			/* Authenticated */
+			Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+			if (isAuthenticated.booleanValue()) {
+				LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 				uniqId = (user == null || user.getUniqId() == null) ? "" : user.getUniqId();
-			    ip = (user == null || user.getIp() == null) ? "" : user.getIp();
-	    	}
+				ip = (user == null || user.getIp() == null) ? "" : user.getIp();
+			}
 
 			sysLog.setSrvcNm(className);
 			sysLog.setMethodNm(methodName);
@@ -132,8 +132,7 @@ public class EgovSysLogAspect {
 	}
 
 	/**
-	 * 시스템 로그정보를 생성한다.
-	 * sevice Class의 delete로 시작되는 Method
+	 * 시스템 로그정보를 생성한다. sevice Class의 delete로 시작되는 Method
 	 *
 	 * @param ProceedingJoinPoint
 	 * @return Object
@@ -161,13 +160,13 @@ public class EgovSysLogAspect {
 			String uniqId = "";
 			String ip = "";
 
-	    	/* Authenticated  */
-	        Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
-	    	if(isAuthenticated.booleanValue()) {
-				LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+			/* Authenticated */
+			Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+			if (isAuthenticated.booleanValue()) {
+				LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 				uniqId = (user == null || user.getUniqId() == null) ? "" : user.getUniqId();
-			    ip = (user == null || user.getIp() == null) ? "" : user.getIp();
-	    	}
+				ip = (user == null || user.getIp() == null) ? "" : user.getIp();
+			}
 
 			sysLog.setSrvcNm(className);
 			sysLog.setMethodNm(methodName);
@@ -183,8 +182,7 @@ public class EgovSysLogAspect {
 	}
 
 	/**
-	 * 시스템 로그정보를 생성한다.
-	 * sevice Class의 select로 시작되는 Method
+	 * 시스템 로그정보를 생성한다. sevice Class의 select로 시작되는 Method
 	 *
 	 * @param ProceedingJoinPoint
 	 * @return Object
@@ -212,13 +210,13 @@ public class EgovSysLogAspect {
 			String uniqId = "";
 			String ip = "";
 
-	    	/* Authenticated  */
-	        Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
-	    	if(isAuthenticated.booleanValue()) {
-				LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+			/* Authenticated */
+			Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+			if (isAuthenticated.booleanValue()) {
+				LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 				uniqId = (user == null || user.getUniqId() == null) ? "" : user.getUniqId();
 				ip = (user == null || user.getIp() == null) ? "" : user.getIp();
-	    	}
+			}
 
 			sysLog.setSrvcNm(className);
 			sysLog.setMethodNm(methodName);
