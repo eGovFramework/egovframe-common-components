e0fd2f09d51cee09675805c20f10e40b7e4d2303
이백행 <dlqorgod@naver.com>
2025-07-11 06:40:57 +0900

    개정이력 수정

--- src/main/java/egovframework/com/sym/log/lgm/service/EgovSysLogAspect.java
+++ src/main/java/egovframework/com/sym/log/lgm/service/EgovSysLogAspect.java
@@ -9,22 +9,23 @@ import egovframework.com.cmm.LoginVO;
 import egovframework.com.cmm.util.EgovUserDetailsHelper;
 
 /**
- * @Class Name : EgovSysLogAspect.java
- * @Description : 시스템 로그 생성을 위한 ASPECT 클래스
- * @Modification Information
- * 
- *               <pre>
- *    수정일         수정자         수정내용
- *    -------        -------     -------------------
- *    2009. 3. 11.   이삼섭         최초생성
- *    2011. 7. 01.   이기하         패키지 분리(sym.log -> sym.log.lgm)
- *               </pre>
+ * 시스템 로그 생성을 위한 ASPECT 클래스
  * 
  * @author 공통 서비스 개발팀 이삼섭
  * @since 2009. 3. 11.
- * @version
+ * @version 1.0
  * @see
  *
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.03.11  이삼섭          최초 생성
+ *   2011.07.01  이기하          패키지 분리(sym.log -> sym.log.lgm)
+ *   2025.07.11  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-UnnecessaryBoxing(불필요한 WrapperObject 생성)
+ *
+ *      </pre>
  */
 public class EgovSysLogAspect {
 
