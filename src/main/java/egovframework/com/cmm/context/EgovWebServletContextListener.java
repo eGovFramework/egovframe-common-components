d805a9872f56c588519b0e5ef9191cb6b9952acd
이백행 <dlqorgod@naver.com>
2025-07-12 17:01:43 +0900

    개정이력 수정

--- src/main/java/egovframework/com/cmm/context/EgovWebServletContextListener.java
+++ src/main/java/egovframework/com/cmm/context/EgovWebServletContextListener.java
@@ -7,26 +7,25 @@ import egovframework.com.cmm.service.EgovProperties;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * EgovWebServletContextListener 클래스
- * <p>
  * 데이터베이스 설정을 spring.profiles.active 방식으로 처리
  * <p>
  * (공통컴포넌트 특성상 데이터베이스별 분리/개발,검증,운영서버로 분리 가능)
- * <p>
- * N/A
- *
+ * 
  * @author 장동한
  * @since 2016.06.23
  * @version 1.0
  * @see
  *
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
- *   수정일        수정자           수정내용
- *  -------      -------------  ----------------------
- *   2016.06.23  장동한           최초 생성
- *   2017.03.03     조성원 	시큐어코딩(ES)-오류 메시지를 통한 정보노출[CWE-209]
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.03.20  홍길동          최초 생성
+ *   2016.06.23  장동한          최초 생성
+ *   2017.03.03  조성원          시큐어코딩(ES)-오류 메시지를 통한 정보노출[CWE-209]
+ *   2025.07.12  이백행          2025년 컨트리뷰션 `-Dspring.profiles.active=mysql-hikari,security` 동작하지 않아 리팩토링
+ *
  *      </pre>
  */
 @Slf4j
