87d594c823213dc940509c44afa9af8154270afe
rukegithub <rukegg@gmail.com>
2025-07-18 17:41:23 +0900

    PR #619 수정 반영

--- src/main/java/egovframework/com/sym/log/clg/service/EgovLoginLogAspect.java
+++ src/main/java/egovframework/com/sym/log/clg/service/EgovLoginLogAspect.java
@@ -6,85 +6,88 @@ import egovframework.com.cmm.LoginVO;
 import egovframework.com.cmm.util.EgovUserDetailsHelper;
 
 /**
- * @Class Name : EgovLoginLogAspect.java
- * @Description : 시스템 로그 생성을 위한 ASPECT 클래스
- * @Modification Information
- *
- *    수정일         수정자         수정내용
- *    -------        -------     -------------------
- *    2009. 3. 11.   이삼섭         최초생성
- *    2011. 7. 01.   이기하         패키지 분리(sym.log -> sym.log.clg)
- *
+ * 시스템 로그 생성을 위한 ASPECT 클래스
+ * 
  * @author 공통 서비스 개발팀 이삼섭
- * @since 2009. 3. 11.
- * @version
+ * @since 2009.03.11
+ * @version 1.0
  * @see
  *
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.03.11  이삼섭          최초 생성
+ *   2011.07.01  이기하          패키지 분리(sym.log -> sym.log.clg)
+ *   2025.07.09  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-UnnecessaryBoxing(불필요한 WrapperObject 생성)
+ *
+ *      </pre>
  */
 public class EgovLoginLogAspect {
-	
-	@Resource(name="EgovLoginLogService")
+
+	@Resource(name = "EgovLoginLogService")
 	private EgovLoginLogService loginLogService;
 
 	/**
-	 * 로그인 로그정보를 생성한다.
-	 * EgovLoginController.actionMain Method
+	 * 로그인 로그정보를 생성한다. EgovLoginController.actionMain Method
 	 * 
-	 * @param 
+	 * @param
 	 * @return void
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public void logLogin() throws Throwable {
-		
-		String uniqId = "";
-		String ip = "";
+
+		String uniqId = null;
+		String ip = null;
 
 		/* Authenticated  */
-        Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
-    	if(isAuthenticated.booleanValue()) {
-			LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
-			uniqId = (user == null || user.getUniqId() == null) ? "" : user.getUniqId();
-			ip = (user == null || user.getIp() == null) ? "" : user.getIp();
-    	}
+		if (EgovUserDetailsHelper.isAuthenticated()) {
+			LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+			if (loginVO != null) {
+				uniqId = loginVO.getUniqId();
+				ip = loginVO.getIp();
+			}
+		}
 
-    	LoginLog loginLog = new LoginLog();
-    	loginLog.setLoginId(uniqId);
-        loginLog.setLoginIp(ip);
-        loginLog.setLoginMthd("I"); // 로그인:I, 로그아웃:O
-        loginLog.setErrOccrrAt("N");
-        loginLog.setErrorCode("");
-        loginLogService.logInsertLoginLog(loginLog);
+		LoginLog loginLog = new LoginLog();
+		loginLog.setLoginId(uniqId);
+		loginLog.setLoginIp(ip);
+		loginLog.setLoginMthd("I"); // 로그인:I, 로그아웃:O
+		loginLog.setErrOccrrAt("N");
+		loginLog.setErrorCode("");
+		loginLogService.logInsertLoginLog(loginLog);
 
 	}
-	
+
 	/**
-	 * 로그아웃 로그정보를 생성한다.
-	 * EgovLoginController.actionLogout Method
+	 * 로그아웃 로그정보를 생성한다. EgovLoginController.actionLogout Method
 	 * 
-	 * @param 
+	 * @param
 	 * @return void
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public void logLogout() throws Throwable {
-		
-		String uniqId = "";
-		String ip = "";
+
+		String uniqId = null;
+		String ip = null;
 
 		/* Authenticated  */
-        Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
-    	if(isAuthenticated.booleanValue()) {
-			LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
-			uniqId = (user == null || user.getUniqId() == null) ? "" : user.getUniqId();
-			ip = (user == null || user.getIp() == null) ? "" : user.getIp();
-    	}
+		if (EgovUserDetailsHelper.isAuthenticated()) {
+			LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+			if (loginVO != null) {
+				uniqId = loginVO.getUniqId();
+				ip = loginVO.getIp();
+			}
+		}
 
-    	LoginLog loginLog = new LoginLog();
-    	loginLog.setLoginId(uniqId);
-        loginLog.setLoginIp(ip);
-        loginLog.setLoginMthd("O"); // 로그인:I, 로그아웃:O
-        loginLog.setErrOccrrAt("N");
-        loginLog.setErrorCode("");
-        loginLogService.logInsertLoginLog(loginLog);
+		LoginLog loginLog = new LoginLog();
+		loginLog.setLoginId(uniqId);
+		loginLog.setLoginIp(ip);
+		loginLog.setLoginMthd("O"); // 로그인:I, 로그아웃:O
+		loginLog.setErrOccrrAt("N");
+		loginLog.setErrorCode("");
+		loginLogService.logInsertLoginLog(loginLog);
 	}
-	
+
 }
