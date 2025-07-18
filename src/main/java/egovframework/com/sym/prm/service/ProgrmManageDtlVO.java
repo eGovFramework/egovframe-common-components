9edcb7196ea7353993218a70cb587a6af74c92db
이백행 <dlqorgod@naver.com>
2025-07-19 07:02:59 +0900

    개정이력 수정

--- src/main/java/egovframework/com/sym/prm/service/ProgrmManageDtlVO.java
+++ src/main/java/egovframework/com/sym/prm/service/ProgrmManageDtlVO.java
@@ -4,20 +4,20 @@ package egovframework.com.sym.prm.service;
  * 프로그램변경관리 처리를 위한 VO 클래스르를 정의한다
  * 
  * @author 개발환경 개발팀 이용
- * @since 2009.06.01
+ * @since 2009.03.20
  * @version 1.0
  * @see
  *
  *      <pre>
- * << 개정이력(Modification Information) >>
- *   
+ *  == 개정이력(Modification Information) ==
+ *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
- *   2009.03.20  이  용          최초 생성
+ *   2009.03.20  이용           최초 생성
+ *   2025.07.19  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-FormalParameterNamingConventions(변수명에 밑줄 사용)
  *
  *      </pre>
  */
-
 public class ProgrmManageDtlVO {
 
 	/** 프로그램파일명 */
