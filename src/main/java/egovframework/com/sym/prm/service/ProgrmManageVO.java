57d3659ca44b2e32454fb8d1ade6a9542493de0d
이백행 <dlqorgod@naver.com>
2025-07-21 05:20:47 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/sym/prm/service/ProgrmManageVO.java
+++ src/main/java/egovframework/com/sym/prm/service/ProgrmManageVO.java
@@ -2,14 +2,15 @@ package egovframework.com.sym.prm.service;
 
 import javax.validation.constraints.NotEmpty;
 
-/** 
+/**
  * 프로그램목록 처리를 위한 VO 클래스르를 정의한다
+ * 
  * @author 개발환경 개발팀 이용
  * @since 2009.06.01
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *   
  *   수정일      수정자           수정내용
@@ -17,10 +18,10 @@ import javax.validation.constraints.NotEmpty;
  *   2009.03.20  이용			최초 생성
  *   2024.10.29  권태성			필수값 BindingResult 검증을 위한 @NotEmpty 추가
  *
- * </pre>
+ *      </pre>
  */
 
-public class ProgrmManageVO{
+public class ProgrmManageVO {
 
 	/** 프로그램파일명 */
 	@NotEmpty(message = "프로그램파일명{common.required.msg}")
@@ -34,79 +35,98 @@ public class ProgrmManageVO{
 	/** URL */
 	@NotEmpty(message = "URL{common.required.msg}")
 	private String url;
-	/** 프로그램설명	 */	
+	/** 프로그램설명 */
 	@NotEmpty(message = "프로그램설명{common.required.msg}")
 	private String progrmDc;
 
 	/**
 	 * progrmFileNm attribute를 리턴한다.
+	 * 
 	 * @return String
 	 */
 	public String getProgrmFileNm() {
 		return progrmFileNm;
 	}
+
 	/**
 	 * progrmFileNm attribute 값을 설정한다.
+	 * 
 	 * @param progrmFileNm String
 	 */
 	public void setProgrmFileNm(String progrmFileNm) {
 		this.progrmFileNm = progrmFileNm;
 	}
+
 	/**
 	 * progrmStrePath attribute를 리턴한다.
+	 * 
 	 * @return String
 	 */
 	public String getProgrmStrePath() {
 		return progrmStrePath;
 	}
+
 	/**
 	 * progrmStrePath attribute 값을 설정한다.
+	 * 
 	 * @param progrmStrePath String
 	 */
 	public void setProgrmStrePath(String progrmStrePath) {
 		this.progrmStrePath = progrmStrePath;
 	}
+
 	/**
 	 * progrmKoreanNm attribute를 리턴한다.
+	 * 
 	 * @return String
 	 */
 	public String getProgrmKoreanNm() {
 		return progrmKoreanNm;
 	}
+
 	/**
 	 * progrmKoreanNm attribute 값을 설정한다.
+	 * 
 	 * @param progrmKoreanNm String
 	 */
 	public void setProgrmKoreanNm(String progrmKoreanNm) {
 		this.progrmKoreanNm = progrmKoreanNm;
 	}
+
 	/**
 	 * url attribute를 리턴한다.
+	 * 
 	 * @return String
 	 */
 	public String getURL() {
 		return url;
 	}
+
 	/**
 	 * URL attribute 값을 설정한다.
+	 * 
 	 * @param URL String
 	 */
 	public void setURL(String URL) {
 		this.url = URL;
 	}
+
 	/**
 	 * progrmDc attribute를 리턴한다.
+	 * 
 	 * @return String
 	 */
 	public String getProgrmDc() {
 		return progrmDc;
 	}
+
 	/**
 	 * progrmDc attribute 값을 설정한다.
+	 * 
 	 * @param progrmDc String
 	 */
 	public void setProgrmDc(String progrmDc) {
 		this.progrmDc = progrmDc;
 	}
-  
+
 }
