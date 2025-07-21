c777280c58a5361fea009a9b7f466221a21607d4
이백행 <dlqorgod@naver.com>
2025-07-22 07:38:52 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/sym/sym/nwk/service/NtwrkVO.java
+++ src/main/java/egovframework/com/sym/sym/nwk/service/NtwrkVO.java
@@ -1,9 +1,9 @@
 /**
- * 개요
+ * <pre>개요
  * - 네트워크에 대한 Vo 클래스를 정의한다.
  *
  * 상세내용
- * - 네트워크의 목록 항목, 조회조건 등을 관리한다.
+ * - 네트워크의 목록 항목, 조회조건 등을 관리한다.</pre> 
  * @author lee.m.j
  * @version 1.0
  * @created 19-8-2010 오후 4:34:37
@@ -16,24 +16,25 @@ import java.util.List;
 
 public class NtwrkVO extends Ntwrk {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * 관리항목
 	 */
-    private String strManageIem;
+	private String strManageIem;
 	/**
 	 * 사용자명
 	 */
-    private String strUserNm;
+	private String strUserNm;
 	/**
 	 * 네트워크목록
 	 */
-    private List<NtwrkVO> ntwrkList;
+	private List<NtwrkVO> ntwrkList;
 	/**
 	 * 삭제여부
 	 */
-    private String delYn[];
+	private String delYn[];
+
 	/**
 	 * @return the strManageIem
 	 */
@@ -41,36 +42,42 @@ public class NtwrkVO extends Ntwrk {
 	public String getStrManageIem() {
 		return strManageIem;
 	}
+
 	/**
 	 * @param strManageIem the strManageIem to set
 	 */
 	public void setStrManageIem(String strManageIem) {
 		this.strManageIem = strManageIem;
 	}
+
 	/**
 	 * @return the strUserNm
 	 */
 	public String getStrUserNm() {
 		return strUserNm;
 	}
+
 	/**
 	 * @param strUserNm the strUserNm to set
 	 */
 	public void setStrUserNm(String strUserNm) {
 		this.strUserNm = strUserNm;
 	}
+
 	/**
 	 * @return the ntwrkList
 	 */
 	public List<NtwrkVO> getNtwrkList() {
 		return ntwrkList;
 	}
+
 	/**
 	 * @param ntwrkList the ntwrkList to set
 	 */
 	public void setNtwrkList(List<NtwrkVO> ntwrkList) {
 		this.ntwrkList = Collections.unmodifiableList(ntwrkList);
 	}
+
 	/**
 	 * @return the delYn
 	 */
@@ -86,16 +93,16 @@ public class NtwrkVO extends Ntwrk {
 		}
 		return ret;
 	}
+
 	/**
 	 * @param delYn the delYn to set
 	 */
 	public void setDelYn(String[] delYn) {
 		this.delYn = new String[delYn.length];
 
-		for (int i = 0; i <  delYn.length; ++i) {
+		for (int i = 0; i < delYn.length; ++i) {
 			this.delYn[i] = delYn[i];
 		}
 	}
-
 
 }
