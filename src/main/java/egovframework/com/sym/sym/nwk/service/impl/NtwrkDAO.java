7115b9aede9082ecfa0683a39c3e570fa85cc64b
이백행 <dlqorgod@naver.com>
2025-07-23 06:17:52 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/sym/sym/nwk/service/impl/NtwrkDAO.java
+++ src/main/java/egovframework/com/sym/sym/nwk/service/impl/NtwrkDAO.java
@@ -1,10 +1,10 @@
 /**
- * 개요
+ * <pre>개요
  * - 네트워크에 대한 DAO 클래스를 정의한다.
  * 
  * 상세내용
  * - 네트워크에 대한 등록, 수정, 삭제, 조회 기능을 제공한다.
- * - 네트워크의 조회기능은 목록조회, 상세조회로 구분된다.
+ * - 네트워크의 조회기능은 목록조회, 상세조회로 구분된다.</pre>
  * @author lee.m.j
  * @version 1.0
  * @created 19-8-2010 오후 4:34:37
@@ -25,53 +25,59 @@ public class NtwrkDAO extends EgovComAbstractDAO {
 
 	/**
 	 * 네트워크를 관리하기 위해 등록된 네트워크목록을 조회한다.
+	 * 
 	 * @param ntwrkVO - 네트워크 Vo
 	 * @return List - 네트워크 목록
 	 */
 	public List<NtwrkVO> selectNtwrkList(NtwrkVO ntwrkVO) throws Exception {
-        return selectList("ntwrkDAO.selectNtwrkList", ntwrkVO);
-    }
-    
+		return selectList("ntwrkDAO.selectNtwrkList", ntwrkVO);
+	}
+
 	/**
 	 * 네트워크목록 총 개수를 조회한다.
+	 * 
 	 * @param ntwrkVO - 네트워크 Vo
 	 * @return int - 네트워크 카운트 수
 	 */
-    public int selectNtwrkListTotCnt(NtwrkVO ntwrkVO) throws Exception {
-        return ((Integer)selectOne("ntwrkDAO.selectNtwrkListTotCnt", ntwrkVO)).intValue();
-    }
-    
+	public int selectNtwrkListTotCnt(NtwrkVO ntwrkVO) throws Exception {
+		return ((Integer) selectOne("ntwrkDAO.selectNtwrkListTotCnt", ntwrkVO)).intValue();
+	}
+
 	/**
 	 * 등록된 네트워크의 상세정보를 조회한다.
+	 * 
 	 * @param ntwrkVO - 네트워크 Vo
 	 * @return NtwrkVO - 네트워크 Vo
 	 */
-    public NtwrkVO selectNtwrk(NtwrkVO ntwrkVO) throws Exception {
-        return (NtwrkVO)selectOne("ntwrkDAO.selectNtwrk", ntwrkVO);
-    }
-    
+	public NtwrkVO selectNtwrk(NtwrkVO ntwrkVO) throws Exception {
+		return (NtwrkVO) selectOne("ntwrkDAO.selectNtwrk", ntwrkVO);
+	}
+
 	/**
 	 * 네트워크정보를 신규로 등록한다.
+	 * 
 	 * @param ntwrk - 네트워크 model
 	 */
-    public void insertNtwrk(Ntwrk ntwrk) throws Exception {
-        insert("ntwrkDAO.insertNtwrk", ntwrk);
-    }
-    
+	public void insertNtwrk(Ntwrk ntwrk) throws Exception {
+		insert("ntwrkDAO.insertNtwrk", ntwrk);
+	}
+
 	/**
 	 * 기 등록된 네트워크정보를 수정한다.
+	 * 
 	 * @param ntwrk - 네트워크 model
 	 */
-    public void updateNtwrk(Ntwrk ntwrk) throws Exception {
-        update("ntwrkDAO.updateNtwrk", ntwrk);
-    }
-    
+	public void updateNtwrk(Ntwrk ntwrk) throws Exception {
+		update("ntwrkDAO.updateNtwrk", ntwrk);
+	}
+
 	/**
 	 * 기 등록된 네트워크정보를 삭제한다.
+	 * 
 	 * @param ntwrk - 네트워크 model
 	 */
-    public void deleteNtwrk(Ntwrk ntwrk) throws Exception {
-        delete("ntwrkDAO.deleteNtwrk", ntwrk);
-    }
+	public void deleteNtwrk(Ntwrk ntwrk) throws Exception {
+		delete("ntwrkDAO.deleteNtwrk", ntwrk);
+	}
 
 }
