349e88b43a3c2d3217e63f6d5786a0dd1b68b31e
rukegithub <rukegg@gmail.com>
2025-07-23 15:38:00 +0900

    PR #632 수정 반영 - 코드 정리, 필드 명명 규칙 개선, 불필요 타입 캐스트 제거, 리턴 타입 안정성 개선

--- src/main/java/egovframework/com/sym/log/ulg/service/impl/EgovUserLogServiceImpl.java
+++ src/main/java/egovframework/com/sym/log/ulg/service/impl/EgovUserLogServiceImpl.java
@@ -13,26 +13,28 @@ import egovframework.com.sym.log.ulg.service.EgovUserLogService;
 import egovframework.com.sym.log.ulg.service.UserLog;
 
 /**
- * @Class Name : EgovUserLogServiceImpl.java
- * @Description : 사용로그 관리를 위한 서비스 구현 클래스
- * @Modification Information
- *
- *    수정일         수정자         수정내용
- *    -------        -------     -------------------
- *    2009. 3. 11.   이삼섭        최초생성
- *    2011. 7. 01.   이기하        패키지 분리(sym.log -> sym.log.ulg)
- *
+ * 사용로그 관리를 위한 서비스 구현 클래스
+ * 
  * @author 공통 서비스 개발팀 이삼섭
- * @since 2009. 3. 11.
- * @version
+ * @since 2009.03.11
+ * @version 1.0
  * @see
  *
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.03.11  이삼섭          최초 생성
+ *   2011.07.01  이기하          패키지 분리(sym.log -> sym.log.ulg)
+ *   2025.07.14  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(final이 아닌 변수는 밑줄을 포함할 수 없음)
+ *
+ *      </pre>
  */
 @Service("EgovUserLogService")
-public class EgovUserLogServiceImpl extends EgovAbstractServiceImpl implements
-	EgovUserLogService {
+public class EgovUserLogServiceImpl extends EgovAbstractServiceImpl implements EgovUserLogService {
 
-	@Resource(name="userLogDAO")
+	@Resource(name = "userLogDAO")
 	private UserLogDAO userLogDAO;
 
 	/**
@@ -54,7 +56,7 @@ public class EgovUserLogServiceImpl extends EgovAbstractServiceImpl implements
 	 * @throws Exception
 	 */
 	@Override
-	public UserLog selectUserLog(UserLog userLog) throws Exception{
+	public UserLog selectUserLog(UserLog userLog) throws Exception {
 
 		return userLogDAO.selectUserLog(userLog);
 	}
@@ -65,15 +67,15 @@ public class EgovUserLogServiceImpl extends EgovAbstractServiceImpl implements
 	 * @param UserLog
 	 */
 	@Override
-	public Map<?, ?> selectUserLogInf(UserLog userLog) throws Exception {
-		List<UserLog> _result = userLogDAO.selectUserLogInf(userLog);
-		int _cnt = userLogDAO.selectUserLogInfCnt(userLog);
+	public Map<String, Object> selectUserLogInf(UserLog userLog) throws Exception {
+		List<UserLog> resultList = userLogDAO.selectUserLogInf(userLog);
+		int resultCnt = userLogDAO.selectUserLogInfCnt(userLog);
 
-		Map<String, Object> _map = new HashMap<String, Object>();
-		_map.put("resultList", _result);
-		_map.put("resultCnt", Integer.toString(_cnt));
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("resultList", resultList);
+		resultMap.put("resultCnt", resultCnt); // 또는 Integer.toString(resultCnt) 필요시
 
-		return _map;
+		return resultMap;
 	}
 
 }
