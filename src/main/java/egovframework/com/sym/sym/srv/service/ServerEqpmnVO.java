57cff9088d46d98c8b5b022d103227a187c70554
이백행 <dlqorgod@naver.com>
2025-07-24 06:15:07 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/sym/sym/srv/service/ServerEqpmnVO.java
+++ src/main/java/egovframework/com/sym/sym/srv/service/ServerEqpmnVO.java
@@ -4,23 +4,26 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * <pre>
  * 개요
  * - 서버장비에 대한 Vo 클래스를 정의한다.
  *
  * 상세내용
  * - 서버장비의 목록 항목 및 조회조건을 관리한다.
+ * </pre>
+ * 
  * @author 이문준
  * @version 1.0
  * @created 28-6-2010 오전 10:44:55
  * 
- * <pre>
+ *          <pre>
  * << 개정이력(Modification Information) >>
  *
  *  수정일               수정자            수정내용
  *  ----------   --------   ---------------------------
  *  2020-08-28   신용호            보안약점 조치 (Private 배열에 Public 데이터 할당[CWE-496])
  *
- * </pre>
+ *          </pre>
  * 
  */
 public class ServerEqpmnVO extends ServerEqpmn {
@@ -45,30 +48,35 @@ public class ServerEqpmnVO extends ServerEqpmn {
 	public List<?> getServerEqpmnList() {
 		return serverEqpmnList;
 	}
+
 	/**
 	 * @param serverEqpmnList the serverEqpmnList to set
 	 */
 	public void setServerEqpmnList(List<?> serverEqpmnList) {
 		this.serverEqpmnList = Collections.unmodifiableList(serverEqpmnList);
 	}
+
 	/**
 	 * @return the delYn
 	 */
 	public String[] getDelYn() {
 		return delYn;
 	}
+
 	/**
 	 * @param delYn the delYn to set
 	 */
 	public void setDelYn(String[] delYn) {
 		this.delYn = delYn.clone();
 	}
+
 	/**
 	 * @return the strServerEqpmnNm
 	 */
 	public String getStrServerEqpmnNm() {
 		return strServerEqpmnNm;
 	}
+
 	/**
 	 * @param strServerEqpmnNm the strServerEqpmnNm to set
 	 */
