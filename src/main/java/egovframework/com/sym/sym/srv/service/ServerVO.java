c5ed59db1564f2cba0c07a372a3d4b10551d347e
이백행 <dlqorgod@naver.com>
2025-07-25 06:31:47 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/sym/sym/srv/service/ServerVO.java
+++ src/main/java/egovframework/com/sym/sym/srv/service/ServerVO.java
@@ -4,23 +4,26 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * <pre>
  * 개요
  * - 서버정보에 대한 Vo 클래스를 정의한다.
  *
  * 상세내용
  * - 서버정보의 목록 항목 및 조회조건을 관리한다.
+ * </pre>
+ * 
  * @author 이문준
  * @version 1.0
  * @created 28-6-2010 오전 10:44:56
  * 
- * <pre>
+ *          <pre>
  * << 개정이력(Modification Information) >>
  *
  *  수정일               수정자            수정내용
  *  ----------   --------   ---------------------------
  *  2020-08-28   신용호            보안약점 조치 (Private 배열에 Public 데이터 할당[CWE-496])
  *
- * </pre>
+ *          </pre>
  * 
  */
 public class ServerVO extends Server {
@@ -47,30 +50,35 @@ public class ServerVO extends Server {
 	public List<?> getServerList() {
 		return serverList;
 	}
+
 	/**
 	 * @param serverList the serverList to set
 	 */
 	public void setServerList(List<?> serverList) {
 		this.serverList = Collections.unmodifiableList(serverList);
 	}
+
 	/**
 	 * @return the delYn
 	 */
 	public String[] getDelYn() {
 		return delYn;
 	}
+
 	/**
 	 * @param delYn the delYn to set
 	 */
 	public void setDelYn(String[] delYn) {
 		this.delYn = delYn.clone();
 	}
+
 	/**
 	 * @return the strServerNm
 	 */
 	public String getStrServerNm() {
 		return strServerNm;
 	}
+
 	/**
 	 * @param strServerNm the strServerNm to set
 	 */
