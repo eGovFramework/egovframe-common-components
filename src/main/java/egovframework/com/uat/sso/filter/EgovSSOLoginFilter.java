fe38ccf4932ed76091a98afc17294f5995259c3e
이백행 <dlqorgod@naver.com>
2025-07-29 07:42:08 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uat/sso/filter/EgovSSOLoginFilter.java
+++ src/main/java/egovframework/com/uat/sso/filter/EgovSSOLoginFilter.java
@@ -23,22 +23,24 @@ import egovframework.com.uat.sso.service.EgovSSOService;
 import egovframework.com.uat.uia.service.EgovLoginService;
 
 /**
- *
+ * Egov SSO 로그인 필터
+ * 
  * @author 공통서비스 개발팀 서준식
- * @since 2011. 8. 2.
+ * @since 2011.08.02
  * @version 1.0
  * @see
  *
  *      <pre>
- * 개정이력(Modification Information)
+ *  == 개정이력(Modification Information) ==
  *
- *   수정일      수정자          수정내용
+ *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
- *  2011. 8. 2.    서준식        최초생성
+ *   2011.08.02  서준식          최초 생성
+ *   2025.07.29  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-UncommentedEmptyMethodBody(빈 메소드에 빈메소드임을 나타내는 주석을 추가할 것)
+ *   2025.07.29  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-UselessParentheses(불필요한 괄호사용)
  *
  *      </pre>
  */
-
 public class EgovSSOLoginFilter implements Filter {
 
 	private FilterConfig config;
