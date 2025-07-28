c16a7771419db16ba2a71c0d8415289d5f12f417
이백행 <dlqorgod@naver.com>
2025-07-29 07:51:04 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uat/sso/filter/EgovSSOLogoutFilter.java
+++ src/main/java/egovframework/com/uat/sso/filter/EgovSSOLogoutFilter.java
@@ -16,22 +16,23 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 import egovframework.com.uat.sso.service.EgovSSOService;
 
 /**
- *
+ * Egov SSO 로그아웃 필터
+ * 
  * @author 공통서비스 개발팀 서준식
- * @since 2011. 8. 29.
+ * @since 2011.08.29
  * @version 1.0
  * @see
  *
  *      <pre>
- * 개정이력(Modification Information)
+ *  == 개정이력(Modification Information) ==
  *
- *   수정일      수정자          수정내용
+ *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
- *  2011. 8. 29.    서준식        최초생성
+ *   2011.08.29  서준식          최초 생성
+ *   2025.07.29  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-UncommentedEmptyMethodBody(빈 메소드에 빈메소드임을 나타내는 주석을 추가할 것)
  *
  *      </pre>
  */
-
 public class EgovSSOLogoutFilter implements Filter {
 	private FilterConfig config;
 
