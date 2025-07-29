f9516ff279b557f87bda6b3183f05dd3c403b583
이백행 <dlqorgod@naver.com>
2025-07-30 06:22:04 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/uat/uap/filter/EgovLoginPolicyFilter.java
+++ src/main/java/egovframework/com/uat/uap/filter/EgovLoginPolicyFilter.java
@@ -24,12 +24,13 @@ import egovframework.com.utl.sim.service.EgovClntInfo;
 
 /**
  * 로그인 정책 체크 필터
+ * 
  * @author 공통서비스 개발팀 서준식
  * @since 2011.07.01
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자          수정내용
@@ -37,7 +38,7 @@ import egovframework.com.utl.sim.service.EgovClntInfo;
  *  2011.07.01  서준식          최초 생성
  *  2017-02-14  이정은          시큐어코딩(ES) - 시큐어코딩 부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
  *
- *  </pre>
+ *      </pre>
  */
 public class EgovLoginPolicyFilter implements Filter {
 
@@ -45,27 +46,32 @@ public class EgovLoginPolicyFilter implements Filter {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EgovLoginPolicyFilter.class);
 
+	@Override
 	public void destroy() {
 	}
 
 	/**
 	 * IP를 이용해 로그인을 제한하는 메서든
+	 * 
 	 * @param request
 	 * @param response
 	 * @param chain
 	 * @return void
 	 * @exception IOException, ServletException
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 
-		ApplicationContext act = WebApplicationContextUtils.getRequiredWebApplicationContext(config.getServletContext());
+		ApplicationContext act = WebApplicationContextUtils
+				.getRequiredWebApplicationContext(config.getServletContext());
 		EgovLoginPolicyService egovLoginPolicyService = (EgovLoginPolicyService) act.getBean("egovLoginPolicyService");
 		EgovMessageSource egovMessageSource = (EgovMessageSource) act.getBean("egovMessageSource");
 
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 
 		String id = request.getParameter("id");
-		//String password = request.getParameter("password");
+		// String password = request.getParameter("password");
 		String userSe = request.getParameter("userSe");
 		String userIp = "";
 
@@ -99,24 +105,28 @@ public class EgovLoginPolicyFilter implements Filter {
 				chain.doFilter(request, response);
 
 			} else {
-				String message = URLEncoder.encode(egovMessageSource.getMessage("fail.common.login.ip"),"UTF-8");
+				String message = URLEncoder.encode(egovMessageSource.getMessage("fail.common.login.ip"), "UTF-8");
 				((HttpServletRequest) request).setAttribute("loginMessage", message);
-				((HttpServletResponse) response).sendRedirect(httpRequest.getContextPath() + "/uat/uia/egovLoginUsr.do?loginMessage="+message);
+				((HttpServletResponse) response).sendRedirect(
+						httpRequest.getContextPath() + "/uat/uia/egovLoginUsr.do?loginMessage=" + message);
 			}
 
-		} catch (IOException e) {//KISA 보안약점 조치 (2018-10-29, 윤창원)
-			LOGGER.error("["+ e.getClass() +"] : ", e.getMessage());
-			((HttpServletResponse) response).sendRedirect(httpRequest.getContextPath() + "/uat/uia/egovLoginUsr.do?login_error=1");
+		} catch (IOException e) {// KISA 보안약점 조치 (2018-10-29, 윤창원)
+			LOGGER.error("[" + e.getClass() + "] : ", e.getMessage());
+			((HttpServletResponse) response)
+					.sendRedirect(httpRequest.getContextPath() + "/uat/uia/egovLoginUsr.do?login_error=1");
 		} catch (Exception e) {
 //			LOGGER.error("Exception: {}", e.getClass().getName());
 //			LOGGER.error("Exception  Message: {}", e.getMessage());
-			// 2017-02-14  이정은          시큐어코딩(ES) - 시큐어코딩 부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
-			LOGGER.error("["+ e.getClass() +"] : ", e.getMessage());
-			
-			((HttpServletResponse) response).sendRedirect(httpRequest.getContextPath() + "/uat/uia/egovLoginUsr.do?login_error=1");
+			// 2017-02-14 이정은 시큐어코딩(ES) - 시큐어코딩 부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
+			LOGGER.error("[" + e.getClass() + "] : ", e.getMessage());
+
+			((HttpServletResponse) response)
+					.sendRedirect(httpRequest.getContextPath() + "/uat/uia/egovLoginUsr.do?login_error=1");
 		}
 	}
 
+	@Override
 	public void init(FilterConfig config) throws ServletException {
 
 		this.config = config;
