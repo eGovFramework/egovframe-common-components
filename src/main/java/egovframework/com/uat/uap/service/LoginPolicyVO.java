2410682e213db22a6e1c4fed83538a203b63fda2
이백행 <dlqorgod@naver.com>
2025-07-30 06:28:34 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/uat/uap/service/LoginPolicyVO.java
+++ src/main/java/egovframework/com/uat/uap/service/LoginPolicyVO.java
@@ -1,9 +1,9 @@
 /**
- * 개요
+ * <pre>개요
  * - 로그인정책에 대한 VO 클래스를 정의한다.
  * 
  * 상세내용
- * - 로그인정책정보의 목록 항목을 관리한다.
+ * - 로그인정책정보의 목록 항목을 관리한다.</pre>
  * @author lee.m.j
  * @version 1.0
  * @created 03-8-2009 오후 2:08:55
@@ -33,32 +33,34 @@ public class LoginPolicyVO extends LoginPolicy {
 	/**
 	 * 삭제 여부
 	 */
-	String [] delYn;
-	
+	String[] delYn;
+
 	/**
 	 * @return the loginPolicyList
 	 */
 	public List<LoginPolicyVO> getLoginPolicyList() {
 		return loginPolicyList;
 	}
+
 	/**
 	 * @param loginPolicyList the loginPolicyList to set
 	 */
 	public void setLoginPolicyList(List<LoginPolicyVO> loginPolicyList) {
 		this.loginPolicyList = loginPolicyList;
 	}
+
 	/**
 	 * @return the delYn
 	 */
 	public String[] getDelYn() {
 		return delYn;
 	}
+
 	/**
 	 * @param delYn the delYn to set
 	 */
 	public void setDelYn(String[] delYn) {
 		this.delYn = delYn;
 	}
-	
-	
+
 }
