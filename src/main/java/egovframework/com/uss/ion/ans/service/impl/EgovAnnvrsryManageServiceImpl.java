3740445a010eee87486eb0e0b0a3fd169f6c886d
이백행 <dlqorgod@naver.com>
2025-08-02 12:02:00 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/uss/ion/ans/service/impl/EgovAnnvrsryManageServiceImpl.java
+++ src/main/java/egovframework/com/uss/ion/ans/service/impl/EgovAnnvrsryManageServiceImpl.java
@@ -24,16 +24,20 @@ import egovframework.com.utl.fcc.service.EgovDateUtil;
 import egovframework.com.utl.fcc.service.EgovStringUtil;
 
 /**
+ * <pre>
  * 개요
  * - 기념일관리에 대한 ServiceImpl 클래스를 정의한다.
  *
  * 상세내용
  * - 기념일관리에 대한 등록, 수정, 삭제, 조회, 반영확인 기능을 제공한다.
  * - 기념일관리의 조회기능은 목록조회, 상세조회로 구분된다.
+ * </pre>
+ * 
  * @author 이용
  * @version 1.0
- * @created 06-15-2010 오후 2:08:56
- *  * <pre>
+ * @created 06-15-2010 오후 2:08:56 *
+ * 
+ *          <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      	수정자          	수정내용
@@ -42,7 +46,7 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  *  2018.11.30	최두영				selectAnnvrsryManageBnde에서 annvrsryManageVO의 null처리 추가
  *  2022.11.11  김혜준			  	시큐어코딩 처리
  *
- *  </pre>
+ *          </pre>
  */
 
 @Service("egovAnnvrsryManageService")
@@ -60,9 +64,11 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 	/**
 	 * 기념일관리정보를 관리하기 위해 등록된 기념일관리 목록을 조회한다.
+	 * 
 	 * @param annvrsryManageVO - 기념일관리 VO
 	 * @return List - 기념일관리 목록
 	 */
+	@Override
 	public List<AnnvrsryManageVO> selectAnnvrsryManageList(AnnvrsryManageVO annvrsryManageVO) throws Exception {
 
 		List<AnnvrsryManageVO> result = annvrsryManageDAO.selectAnnvrsryManageList(annvrsryManageVO);
@@ -78,18 +84,22 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 	/**
 	 * 기념일관리목록 총 개수를 조회한다.
+	 * 
 	 * @param annvrsryManageVO - 기념일관리 VO
 	 * @return int - 기념일관리 카운트 수
 	 */
+	@Override
 	public int selectAnnvrsryManageListTotCnt(AnnvrsryManageVO annvrsryManageVO) throws Exception {
 		return annvrsryManageDAO.selectAnnvrsryManageListTotCnt(annvrsryManageVO);
 	}
 
 	/**
 	 * 등록된 기념일관리의 상세정보를 조회한다.
+	 * 
 	 * @param annvrsryManageVO - 기념일관리 VO
 	 * @return AnnvrsryManageVO - 기념일관리 VO
 	 */
+	@Override
 	public AnnvrsryManageVO selectAnnvrsryManage(AnnvrsryManageVO annvrsryManageVO) throws Exception {
 		annvrsryManageVO.setAnnvrsryDe(EgovStringUtil.removeMinusChar(annvrsryManageVO.getAnnvrsryDe()));
 		AnnvrsryManageVO annvrsryManageVOTemp = annvrsryManageDAO.selectAnnvrsryManage(annvrsryManageVO);
@@ -99,8 +109,10 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 	/**
 	 * 기념일관리정보를 신규로 등록한다.
+	 * 
 	 * @param annvrsryManage - 기념일관리 model
 	 */
+	@Override
 	public void insertAnnvrsryManage(AnnvrsryManage annvrsryManage) throws Exception {
 		annvrsryManage.setAnnvrsryDe(EgovStringUtil.removeMinusChar(annvrsryManage.getAnnvrsryDe()));
 
@@ -111,8 +123,10 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 	/**
 	 * 기 등록된 기념일관리정보를 수정한다.
+	 * 
 	 * @param annvrsryManage - 기념일관리 model
 	 */
+	@Override
 	public void updateAnnvrsryManage(AnnvrsryManage annvrsryManage) throws Exception {
 		annvrsryManage.setAnnvrsryDe(EgovStringUtil.removeMinusChar(annvrsryManage.getAnnvrsryDe()));
 		annvrsryManageDAO.updateAnnvrsryManage(annvrsryManage);
@@ -120,8 +134,10 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 	/**
 	 * 기 등록된 기념일관리정보를 삭제한다.
+	 * 
 	 * @param annvrsryManage - 기념일관리 model
 	 */
+	@Override
 	public void deleteAnnvrsryManage(AnnvrsryManage annvrsryManage) throws Exception {
 		annvrsryManage.setAnnvrsryDe(EgovStringUtil.removeMinusChar(annvrsryManage.getAnnvrsryDe()));
 		annvrsryManageDAO.deleteAnnvrsryManage(annvrsryManage);
@@ -129,9 +145,11 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 	/**
 	 * 등록된 기념일관리의 상세정보를 조회한다.
+	 * 
 	 * @param annvrsryManageVO - 기념일관리 VO
 	 * @return AnnvrsryManageVO - 기념일관리 VO
 	 */
+	@Override
 	public List<AnnvrsryManageVO> selectAnnvrsryGdcc(AnnvrsryManageVO annvrsryManageVO) throws Exception {
 
 		List<AnnvrsryManageVO> result_temp = annvrsryManageDAO.selectAnnvrsryGdcc(annvrsryManageVO);
@@ -143,7 +161,8 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 			AnnvrsryManageVO annvrsryManageVO1 = result_temp.get(i);
 			lTemp = getDateCount(annvrsryManageVO1);
 
-			if (lTemp >= 0 && lTemp < Long.parseLong(annvrsryManageVO1.getAnnvrsryBeginDe().replaceAll("\\p{Space}", ""))) {
+			if (lTemp >= 0
+					&& lTemp < Long.parseLong(annvrsryManageVO1.getAnnvrsryBeginDe().replaceAll("\\p{Space}", ""))) {
 				annvrsryManageVO1.setAnnvrsryDe(EgovDateUtil.formatDate(annvrsryManageVO1.getAnnvrsryDe(), "-"));
 				result_temp.set(i, annvrsryManageVO1);
 				result.add(result_temp.get(i));
@@ -154,15 +173,18 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 	/**
 	 * 기념일관리목록 총 개수를 조회한다.
+	 * 
 	 * @param annvrsryManageVO - 기념일관리 VO
 	 * @return int - 기념일관리 카운트 수
 	 */
+	@Override
 	public int selectAnnvrsryManageDplctAt(AnnvrsryManage annvrsryManage) throws Exception {
 		return annvrsryManageDAO.selectAnnvrsryManageDplctAt(annvrsryManage);
 	}
 
 	/**
 	 * 해당일자와 현재일자의 일수 계산
+	 * 
 	 * @param annvrsryManageVO
 	 * @return long
 	 */
@@ -170,7 +192,7 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 		/* 날짜 사이의 기간 산출 */
 		long resultDay = 0;
-		Calendar to_day = Calendar.getInstance(); //Calendar객체를 생성합니다.
+		Calendar to_day = Calendar.getInstance(); // Calendar객체를 생성합니다.
 		Calendar target_day = Calendar.getInstance();
 
 		String sAnnvrsryDe = null;
@@ -179,7 +201,11 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 		// 매년반복일 경우
 		if ("1".equals(annvrsryManageVO.getReptitSe())) {
-			sAnnvrsryDe = Integer.toString(to_day.get(Calendar.YEAR)) + (sAnnvrsryDe == null || sAnnvrsryDe.length() < 8 ? to_day.get(Calendar.MONTH) : sAnnvrsryDe.substring(4, 6)) + (sAnnvrsryDe == null || sAnnvrsryDe.length() < 8 ? to_day.get(Calendar.DATE) : sAnnvrsryDe.substring(6, 8));
+			sAnnvrsryDe = Integer.toString(to_day.get(Calendar.YEAR))
+					+ (sAnnvrsryDe == null || sAnnvrsryDe.length() < 8 ? to_day.get(Calendar.MONTH)
+							: sAnnvrsryDe.substring(4, 6))
+					+ (sAnnvrsryDe == null || sAnnvrsryDe.length() < 8 ? to_day.get(Calendar.DATE)
+							: sAnnvrsryDe.substring(6, 8));
 		}
 
 		// 음력인 경우 양력으로 환산
@@ -188,7 +214,8 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 		}
 
 		if (sAnnvrsryDe != null && !sAnnvrsryDe.equals("")) {
-			target_day.set(Integer.parseInt(sAnnvrsryDe.substring(0, 4)), Integer.parseInt(sAnnvrsryDe.substring(4, 6)) - 1, Integer.parseInt(sAnnvrsryDe.substring(6, 8)));
+			target_day.set(Integer.parseInt(sAnnvrsryDe.substring(0, 4)),
+					Integer.parseInt(sAnnvrsryDe.substring(4, 6)) - 1, Integer.parseInt(sAnnvrsryDe.substring(6, 8)));
 		} else {
 			target_day.set(to_day.get(Calendar.YEAR), to_day.get(Calendar.MONTH) + 1, to_day.get(Calendar.DATE));
 		}
@@ -199,87 +226,91 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 		} else {
 			resultDay = -1;
 		}
-		//annvrsryManageVO.setAnnvrsryBeginDe(Long.toString(resultDay));
-		
+		// annvrsryManageVO.setAnnvrsryBeginDe(Long.toString(resultDay));
+
 		return resultDay;
 	}
 
-	/*###  엑셀 일괄처리 프로세스 ###*/
+	/* ### 엑셀 일괄처리 프로세스 ### */
 
 	/**
 	 * 기념일정보 excel생성
-	 * @param  inputStream InputStream
-	 * @return  String
+	 * 
+	 * @param inputStream InputStream
+	 * @return String
 	 * @exception Exception
 	 */
+	@Override
 	public List<AnnvrsryManageVO> selectAnnvrsryManageBnde(InputStream inputStream) throws Exception {
-		//int annvrsrySheetRowCnt = 0;
-		//String xlsFile = null;
+		// int annvrsrySheetRowCnt = 0;
+		// String xlsFile = null;
 
-		String sTempId = null; //사용자ID
-		String sTempNm = null; //사용자명
-		String sTempAnnvrsryDe = null; //기념일자
-		String sTempCldrSe = null; //양/음 구분
-		String sTempAnnvrsrySe = null; //기념일구분
-		String sTempAnnvrsryNm = null; //기념일명
+		String sTempId = null; // 사용자ID
+		String sTempNm = null; // 사용자명
+		String sTempAnnvrsryDe = null; // 기념일자
+		String sTempCldrSe = null; // 양/음 구분
+		String sTempAnnvrsrySe = null; // 기념일구분
+		String sTempAnnvrsryNm = null; // 기념일명
 		String sTempReptitSe = null; // 반복여부
 
 		List<AnnvrsryManageVO> list = new ArrayList<AnnvrsryManageVO>();
 
-		//String sBndtDe = null;
+		// String sBndtDe = null;
 		HSSFWorkbook hssfWB = (HSSFWorkbook) excelZipService.loadWorkbook(inputStream);
 		// 엑셀 파일 시트 개수 확인 sheet = 1
 		if (hssfWB.getNumberOfSheets() == 1) {
-			HSSFSheet annvrsrySheet = hssfWB.getSheetAt(0); //기념일관리 시트 가져오기
-			//HSSFRow annvrsryRow = annvrsrySheet.getRow(1); //기념일 row 가져오기
-			//annvrsrySheetRowCnt = annvrsryRow.getPhysicalNumberOfCells(); //기념일 cell Cnt
-			int rowsCnt = annvrsrySheet.getPhysicalNumberOfRows(); //행 개수 가져오기
+			HSSFSheet annvrsrySheet = hssfWB.getSheetAt(0); // 기념일관리 시트 가져오기
+			// HSSFRow annvrsryRow = annvrsrySheet.getRow(1); //기념일 row 가져오기
+			// annvrsrySheetRowCnt = annvrsryRow.getPhysicalNumberOfCells(); //기념일 cell Cnt
+			int rowsCnt = annvrsrySheet.getPhysicalNumberOfRows(); // 행 개수 가져오기
 
-			//사용자ID	기념일자	양/음 구분	기념일구분	기념일명
-			for (int j = 1; j < rowsCnt; j++) { //row 루프
+			// 사용자ID 기념일자 양/음 구분 기념일구분 기념일명
+			for (int j = 1; j < rowsCnt; j++) { // row 루프
 				AnnvrsryManageVO annvrsryManageVO = new AnnvrsryManageVO();
 				AnnvrsryManageVO annvrsryManageVOTemp = null;
-				HSSFRow row = annvrsrySheet.getRow(j); //row 가져오기
+				HSSFRow row = annvrsrySheet.getRow(j); // row 가져오기
 				if (row != null) {
-					//int cells = row.getPhysicalNumberOfCells(); //cell 개수 가져오기
+					// int cells = row.getPhysicalNumberOfCells(); //cell 개수 가져오기
 					HSSFCell cell = null;
-					cell = row.getCell(0); //사용자ID
+					cell = row.getCell(0); // 사용자ID
 					if (cell != null) {
-						sTempId = (String) cell.getStringCellValue();
+						sTempId = cell.getStringCellValue();
 					}
 
-					cell = row.getCell(1); //사용자명
+					cell = row.getCell(1); // 사용자명
 					if (cell != null) {
-						sTempNm = (String) cell.getStringCellValue();
+						sTempNm = cell.getStringCellValue();
 					}
 
-					cell = row.getCell(2); //기념일자
+					cell = row.getCell(2); // 기념일자
 					if (cell != null) {
-						sTempAnnvrsryDe = (String) cell.getStringCellValue();
+						sTempAnnvrsryDe = cell.getStringCellValue();
 					}
 
-					cell = row.getCell(3); //양/음구분
+					cell = row.getCell(3); // 양/음구분
 					if (cell != null) {
-						sTempCldrSe = (String) cell.getStringCellValue();
+						sTempCldrSe = cell.getStringCellValue();
 					}
 
-					cell = row.getCell(4); //기념일구분
+					cell = row.getCell(4); // 기념일구분
 					if (cell != null) {
-						sTempAnnvrsrySe = (String) cell.getStringCellValue();
+						sTempAnnvrsrySe = cell.getStringCellValue();
 					}
-					cell = row.getCell(5); //기념일명
+					cell = row.getCell(5); // 기념일명
 					if (cell != null) {
-						sTempAnnvrsryNm = (String) cell.getStringCellValue();
+						sTempAnnvrsryNm = cell.getStringCellValue();
 					}
-					cell = row.getCell(6); //반복여부
+					cell = row.getCell(6); // 반복여부
 					if (cell != null) {
-						sTempReptitSe = (String) cell.getStringCellValue();
+						sTempReptitSe = cell.getStringCellValue();
 					}
 					annvrsryManageVO.setUsid(sTempId); // 당직자ID
 					annvrsryManageVO.setAnnvrsryTemp1(sTempNm); // 당직자명
 					annvrsryManageVO.setAnnvrsrySe(sTempAnnvrsrySe);
 					annvrsryManageVOTemp = annvrsryManageDAO.selectAnnvrsryManageBnde(annvrsryManageVO);
-					if (annvrsryManageVOTemp != null) annvrsryManageVO = annvrsryManageVOTemp; // 기존에 등록되어 있는경우
+					if (annvrsryManageVOTemp != null) {
+						annvrsryManageVO = annvrsryManageVOTemp; // 기존에 등록되어 있는경우
+					}
 					annvrsryManageVO.setAnnvrsrySe(sTempAnnvrsrySe);
 					annvrsryManageVO.setAnnvrsryDe(EgovDateUtil.formatDate(sTempAnnvrsryDe, "-"));
 					annvrsryManageVO.setCldrSe(sTempCldrSe);
@@ -295,10 +326,13 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 	/**
 	 * 기념일정보를 일괄등록처리한다.
+	 * 
 	 * @param annvrsryManageVO - 기념일관리 VO
-	 * @param String - 기념일정보
-	*/
-	public void insertAnnvrsryManageBnde(AnnvrsryManageVO annvrsryManageVO, String checkedAnnvrsryManageForInsert) throws Exception {
+	 * @param String           - 기념일정보
+	 */
+	@Override
+	public void insertAnnvrsryManageBnde(AnnvrsryManageVO annvrsryManageVO, String checkedAnnvrsryManageForInsert)
+			throws Exception {
 		AnnvrsryManage annvrsryManage;
 
 		// 2022.11.11 시큐어코딩 처리
@@ -331,13 +365,15 @@ public class EgovAnnvrsryManageServiceImpl extends EgovAbstractServiceImpl imple
 
 	/**
 	 * 기념일관리 건수를 조회한다.
+	 * 
 	 * @param annvrsryManage - 기념일관리
 	 * @return int
 	 * @exception Exception
-
-	public int selectAnnvrsryManageMonthCnt(AnnvrsryManageVO annvrsryManageVO) throws Exception {
-		return annvrsryManageDAO.selectAnnvrsryManageMonthCnt(annvrsryManageVO);
-	}
+	 * 
+	 *                      public int selectAnnvrsryManageMonthCnt(AnnvrsryManageVO
+	 *                      annvrsryManageVO) throws Exception { return
+	 *                      annvrsryManageDAO.selectAnnvrsryManageMonthCnt(annvrsryManageVO);
+	 *                      }
 	 */
 
 }
