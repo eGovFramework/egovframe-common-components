ebc9505caabaff2b1f06e121c80b05b8c942ec37
이백행 <dlqorgod@naver.com>
2025-08-04 05:44:17 +0900

    PMD로 소프트웨어 보안약점 진단하고 제거하기-BannerVO

--- src/main/java/egovframework/com/uss/ion/bnr/service/BannerVO.java
+++ src/main/java/egovframework/com/uss/ion/bnr/service/BannerVO.java
@@ -13,24 +13,32 @@ package egovframework.com.uss.ion.bnr.service;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class BannerVO extends Banner {
 
 	/**
 	 * serialVersionUID
 	 */
 	private static final long serialVersionUID = 1L;
+
 	/**
 	 * 배너 목록
 	 */
-	List<BannerVO> bannerList;
+	private List<BannerVO> bannerList;
+
 	/**
 	 * 삭제대상 목록
 	 */
-	String[] delYn;
+	@Getter
+	@Setter
+	private String[] delYn;
+
 	/**
 	 * 결과 반영 타입 vertical : 세로 horizontal : 가로
 	 */
-	String resultType = "horizontal";
+	private String resultType = "horizontal";
 
 	/**
 	 * @return the bannerList
@@ -44,20 +52,6 @@ public class BannerVO extends Banner {
 	 */
 	public void setBannerList(List<BannerVO> bannerList) {
 		this.bannerList = bannerList;
-	}
-
-	/**
-	 * @return the delYn
-	 */
-	public String[] getDelYn() {
-		return delYn;
-	}
-
-	/**
-	 * @param delYn the delYn to set
-	 */
-	public void setDelYn(String[] delYn) {
-		this.delYn = delYn;
 	}
 
 	/**
