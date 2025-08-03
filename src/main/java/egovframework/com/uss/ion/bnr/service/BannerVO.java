5d47d1a707b1a0fb1a9a0e2784e82ea7d2716e44
이백행 <dlqorgod@naver.com>
2025-08-04 05:46:32 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/ion/bnr/service/BannerVO.java
+++ src/main/java/egovframework/com/uss/ion/bnr/service/BannerVO.java
@@ -1,14 +1,3 @@
-/**
- * 개요
- * - 배너에 대한 Vo 클래스를 정의한다.
- * 
- * 상세내용
- * - 배너의 목록 항목을 관리한다.
- * @author 이문준
- * @version 1.0
- * @created 03-8-2009 오후 2:07:13
- */
-
 package egovframework.com.uss.ion.bnr.service;
 
 import java.util.List;
@@ -16,6 +5,31 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * <pre>
+ * 개요
+ * - 배너에 대한 Vo 클래스를 정의한다.
+ * 
+ * 상세내용
+ * - 배너의 목록 항목을 관리한다.
+ * </pre>
+ * 
+ * @author 이문준
+ * @since 2009.08.03
+ * @version 1.0
+ * @see
+ *
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.08.03  이문준          최초 생성
+ *   2025.08.04  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-MethodReturnsInternalArray(Private 배열에 Public 데이터 할당)
+ *   2025.08.04  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-ArrayIsStoredDirectly(Public 메소드부터 반환된 Private 배열)
+ *
+ *      </pre>
+ */
 public class BannerVO extends Banner {
 
 	/**
