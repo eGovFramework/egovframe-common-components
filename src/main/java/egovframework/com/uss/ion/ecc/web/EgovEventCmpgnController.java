6b083071650d97dcfa3210eea1d82a32698bb490
이백행 <dlqorgod@naver.com>
2025-08-05 07:20:44 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/ion/ecc/web/EgovEventCmpgnController.java
+++ src/main/java/egovframework/com/uss/ion/ecc/web/EgovEventCmpgnController.java
@@ -37,16 +37,16 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  * @see
  *
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.03.20  장동한          최초 생성
- *   2011.8.26	정진오			IncludedInfo annotation 추가
+ *   2011.08.26  정진오          IncludedInfo annotation 추가
+ *   2025.08.05  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(final이 아닌 변수는 밑줄을 포함할 수 없음)
  *
  *      </pre>
  */
-
 @Controller
 public class EgovEventCmpgnController {
 
