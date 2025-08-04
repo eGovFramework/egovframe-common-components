ca364bca127b0be2151c0877f8d0ed682497d7e2
rukegithub <rukegg@gmail.com>
2025-08-04 13:24:26 +0900

    PR #651 수정 반영 - 코드 정리, 배열 복사 로직 개선

--- src/main/java/egovframework/com/sym/sym/nwk/service/NtwrkVO.java
+++ src/main/java/egovframework/com/sym/sym/nwk/service/NtwrkVO.java
@@ -1,39 +1,54 @@
+package egovframework.com.sym.sym.nwk.service;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
+ * <pre>
  * 개요
  * - 네트워크에 대한 Vo 클래스를 정의한다.
  *
  * 상세내용
  * - 네트워크의 목록 항목, 조회조건 등을 관리한다.
+ * </pre>
+ * 
  * @author lee.m.j
+ * @since 2010.08.19
  * @version 1.0
- * @created 19-8-2010 오후 4:34:37
+ * @see
+ *
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2010.08.19  lee.m.j       최초 생성
+ *   2025.07.22  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-AvoidArrayLoops(배열의 값을 루프문을 이용하여 복사하는 것 보다, System.arraycopy() 메소드를 이용하여 복사하는 것이 효율적이며 수행 속도가 빠름)
+ *
+ *      </pre>
  */
-
-package egovframework.com.sym.sym.nwk.service;
-
-import java.util.Collections;
-import java.util.List;
-
 public class NtwrkVO extends Ntwrk {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * 관리항목
 	 */
-    private String strManageIem;
+	private String strManageIem;
 	/**
 	 * 사용자명
 	 */
-    private String strUserNm;
+	private String strUserNm;
 	/**
 	 * 네트워크목록
 	 */
-    private List<NtwrkVO> ntwrkList;
+	private List<NtwrkVO> ntwrkList;
 	/**
 	 * 삭제여부
 	 */
-    private String delYn[];
+	private String delYn[];
+
 	/**
 	 * @return the strManageIem
 	 */
@@ -41,61 +56,61 @@ public class NtwrkVO extends Ntwrk {
 	public String getStrManageIem() {
 		return strManageIem;
 	}
+
 	/**
 	 * @param strManageIem the strManageIem to set
 	 */
 	public void setStrManageIem(String strManageIem) {
 		this.strManageIem = strManageIem;
 	}
+
 	/**
 	 * @return the strUserNm
 	 */
 	public String getStrUserNm() {
 		return strUserNm;
 	}
+
 	/**
 	 * @param strUserNm the strUserNm to set
 	 */
 	public void setStrUserNm(String strUserNm) {
 		this.strUserNm = strUserNm;
 	}
+
 	/**
 	 * @return the ntwrkList
 	 */
 	public List<NtwrkVO> getNtwrkList() {
 		return ntwrkList;
 	}
+
 	/**
 	 * @param ntwrkList the ntwrkList to set
 	 */
 	public void setNtwrkList(List<NtwrkVO> ntwrkList) {
 		this.ntwrkList = Collections.unmodifiableList(ntwrkList);
 	}
+
 	/**
 	 * @return the delYn
 	 */
 	public String[] getDelYn() {
-		String[] ret = null;
-
-		if (delYn != null) {
-			ret = new String[delYn.length];
-
-			for (int i = 0; i < delYn.length; i++) {
-				ret[i] = delYn[i];
-			}
+		if (delYn == null) {
+			return null;
 		}
-		return ret;
+		return Arrays.copyOf(delYn, delYn.length);  // System.arraycopy 대신 사용
 	}
+	
 	/**
 	 * @param delYn the delYn to set
 	 */
 	public void setDelYn(String[] delYn) {
-		this.delYn = new String[delYn.length];
-
-		for (int i = 0; i <  delYn.length; ++i) {
-			this.delYn[i] = delYn[i];
+		if (delYn == null) {
+			this.delYn = null;
+		} else {
+			this.delYn = Arrays.copyOf(delYn, delYn.length);
 		}
 	}
-
 
 }
