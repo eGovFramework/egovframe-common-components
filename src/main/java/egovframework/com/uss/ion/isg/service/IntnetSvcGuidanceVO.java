d5e676de2654c4f1bb8c07d3fc2b570867823a34
이백행 <dlqorgod@naver.com>
2025-08-06 08:34:29 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/ion/isg/service/IntnetSvcGuidanceVO.java
+++ src/main/java/egovframework/com/uss/ion/isg/service/IntnetSvcGuidanceVO.java
@@ -1,14 +1,3 @@
-/**
- * 개요
- * - 인터넷서비스안내에 대한 VO 클래스를 정의한다.
- * 
- * 상세내용
- * - 인터넷서비스안내의 목록 항목을 관리한다.
- * @author lee.m.j
- * @version 1.0
- * @created 03-8-2009 오후 2:08:52
- */
-
 package egovframework.com.uss.ion.isg.service;
 
 import java.util.List;
@@ -16,6 +5,31 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * <pre>
+ * 개요
+ * - 인터넷서비스안내에 대한 VO 클래스를 정의한다.
+ * 
+ * 상세내용
+ * - 인터넷서비스안내의 목록 항목을 관리한다.
+ * </pre>
+ * 
+ * @author 공통컴포넌트 개발팀 홍길동
+ * @since 2010.08.03
+ * @version 1.0
+ * @see
+ *
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2010.08.03  이문준          최초 생성
+ *   2025.08.06  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-MethodReturnsInternalArray(Private 배열에 Public 데이터 할당)
+ *   2025.08.06  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-ArrayIsStoredDirectly(Public 메소드부터 반환된 Private 배열)
+ *
+ *      </pre>
+ */
 public class IntnetSvcGuidanceVO extends IntnetSvcGuidance {
 
 	/**
