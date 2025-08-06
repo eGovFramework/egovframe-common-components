234f832bb389ed8774d652a3020b99d64b33a476
이백행 <dlqorgod@naver.com>
2025-08-07 07:44:58 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/uss/ion/msi/service/MainImageVO.java
+++ src/main/java/egovframework/com/uss/ion/msi/service/MainImageVO.java
@@ -21,12 +21,12 @@ public class MainImageVO extends MainImage {
 	private static final long serialVersionUID = 1L;
 	/**
 	 * 메인화면이미지 목록
-	 */	
+	 */
 	List<MainImageVO> mainImageList;
 	/**
 	 * 삭제대상 목록
-	 */		
-    String[] delYn;
+	 */
+	String[] delYn;
 
 	/**
 	 * @return the mainImageList
@@ -34,25 +34,26 @@ public class MainImageVO extends MainImage {
 	public List<MainImageVO> getMainImageList() {
 		return mainImageList;
 	}
+
 	/**
 	 * @param mainImageList the mainImageList to set
 	 */
 	public void setMainImageList(List<MainImageVO> mainImageList) {
 		this.mainImageList = mainImageList;
 	}
+
 	/**
 	 * @return the delYn
 	 */
 	public String[] getDelYn() {
 		return delYn;
 	}
+
 	/**
 	 * @param delYn the delYn to set
 	 */
 	public void setDelYn(String[] delYn) {
 		this.delYn = delYn;
 	}
-    
-    
-    
+
 }
