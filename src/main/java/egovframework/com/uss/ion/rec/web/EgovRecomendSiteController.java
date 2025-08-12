638e4fdedadf4b6c9cfa720cc30d7d0b98b9518f
이백행 <dlqorgod@naver.com>
2025-08-12 16:28:23 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/ion/rec/web/EgovRecomendSiteController.java
+++ src/main/java/egovframework/com/uss/ion/rec/web/EgovRecomendSiteController.java
@@ -25,7 +25,6 @@ import egovframework.com.uss.ion.rec.service.RecomendSiteVO;
 import egovframework.com.utl.fcc.service.EgovStringUtil;
 
 /**
- *
  * 추천사이트처리를 하는 Controller 클래스
  * 
  * @author 공통서비스 개발팀 박정규
@@ -34,17 +33,17 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  * @see
  *
  *      <pre>
-* << 개정이력(Modification Information) >>
-*
-*   수정일      수정자           수정내용
-*  -------    --------    ---------------------------
-*   2009.04.01  박정규          최초 생성
-*   2011.8.26	 정진오			IncludedInfo annotation 추가
-*   2016.08.22	 김연호			표준프레임워크 3.6 개선
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.04.01  박정규          최초 생성
+ *   2011.08.26  정진오          IncludedInfo annotation 추가
+ *   2016.08.22  김연호          표준프레임워크 3.6 개선
+ *   2025.08.12  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(final이 아닌 변수는 밑줄을 포함할 수 없음)
  *
  *      </pre>
  */
-
 @Controller
 public class EgovRecomendSiteController {
 
