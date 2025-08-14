7840abcce177aaa26ef9edf4462221378ddca390
이백행 <dlqorgod@naver.com>
2025-08-14 19:07:23 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/uss/ion/rss/service/impl/RssTagManageDao.java
+++ src/main/java/egovframework/com/uss/ion/rss/service/impl/RssTagManageDao.java
@@ -22,10 +22,13 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
 
 /**
  * RSS태그관리를 처리하는 Dao Class 구현
+ * 
  * @author 공통콤포넌트 장동한
  * @since 2010.06.16
  * @version 1.0
- * @see <pre>
+ * @see
+ * 
+ *      <pre>
  * &lt;&lt; 개정이력(Modification Information) &gt;&gt;
  *
  *   수정일      수정자           수정내용
@@ -35,8 +38,8 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  *   2018.10.22  신용호          connection close 관련 수정
  *   2018.12.05  신용호          selectRssTagManageTableList(),selectRssTagManageTableColumnList() 테이블 목록 화이트리스트 제약
  *   2019.05.10  신용호          WhiteList 기능 보완
- * </pre>
- */ 
+ *      </pre>
+ */
 @Repository("rssManageDao")
 public class RssTagManageDao extends EgovComAbstractDAO {
 
@@ -46,6 +49,7 @@ public class RssTagManageDao extends EgovComAbstractDAO {
 
 	/**
 	 * JDBC 테이블 목록을조회한다.
+	 * 
 	 * @return List -조회한목록이담긴List
 	 * @throws Exception
 	 */
@@ -70,18 +74,20 @@ public class RssTagManageDao extends EgovComAbstractDAO {
 
 				// KISA 보안약점 조치 (2018-12-05, 신용호)
 				String tableName = tables.getString(TABLE_NAME);
-				if (tableName==null) tableName="";
+				if (tableName == null) {
+					tableName = "";
+				}
 				// WhiteList 기능 보완 (2019-05-10, 신용호)
-				if ( tableWhiteList.contains(tableName.toLowerCase()) == true ) {
+				if (tableWhiteList.contains(tableName.toLowerCase()) == true) {
 					ComDefaultCodeVO codeVO = new ComDefaultCodeVO();
 					codeVO.setCode(tables.getString(TABLE_NAME));
 					codeVO.setCodeNm(tables.getString(TABLE_SCHEMA));
-					arrListResult.add((ComDefaultCodeVO) codeVO);
+					arrListResult.add(codeVO);
 				}
 			}
 
 		} finally {
-			//EgovResourceCloseHelper.closeDBObjects(tables, conn);
+			// EgovResourceCloseHelper.closeDBObjects(tables, conn);
 			EgovResourceCloseHelper.closeDBObjects(tables);
 		}
 
@@ -91,14 +97,15 @@ public class RssTagManageDao extends EgovComAbstractDAO {
 
 	/**
 	 * JDBC 테이블 컬럼 목록을 조회한다.
+	 * 
 	 * @param map - 컬럼조회정보
 	 * @return List -조회한목록이담긴List
 	 * @throws Exception
 	 */
 	public List<Map<String, String>> selectRssTagManageTableColumnList(Map<String, String> map) throws Exception {
 
-		String sTableName = (String) map.get("tableName");
-		String sDbType = (String) map.get("dbType");
+		String sTableName = map.get("tableName");
+		String sDbType = map.get("dbType");
 		String sSQL = "";
 		ArrayList<Map<String, String>> arrListResult = new ArrayList<Map<String, String>>();
 
@@ -107,42 +114,44 @@ public class RssTagManageDao extends EgovComAbstractDAO {
 		ResultSet rs = null;
 
 		try {
-			conn = getSqlSession().getConnection(); //getSqlMapClientTemplate().getDataSource().getConnection();
+			conn = getSqlSession().getConnection(); // getSqlMapClientTemplate().getDataSource().getConnection();
 
 			// KISA 보안약점 조치 (2018-12-05, 신용호)
 			// WhiteList 기능 보완 (2019-05-10, 신용호)
-			if ( tableWhiteList.contains(sTableName.toLowerCase()) == true ) {
-			
-				if ( sDbType.equals("mysql") || sDbType.equals("maria") || sDbType.equals("postgres") ) {
+			if (tableWhiteList.contains(sTableName.toLowerCase()) == true) {
+
+				if (sDbType.equals("mysql") || sDbType.equals("maria") || sDbType.equals("postgres")) {
 					sSQL = "SELECT * FROM (sTableName) LIMIT 1 ";
-					sSQL = sSQL.replace("(sTableName)",sTableName);
-					
+					sSQL = sSQL.replace("(sTableName)", sTableName);
+
 				} else {
 					sSQL = "SELECT * FROM (sTableName) WHERE ROWNUM <= 1 ";
-					sSQL = sSQL.replace("(sTableName)",sTableName);
+					sSQL = sSQL.replace("(sTableName)", sTableName);
 				}
-	
-				if (!sDbType.equals("altibase")) {//2011.10.18
+
+				if (!sDbType.equals("altibase")) {// 2011.10.18
 					st = conn.prepareStatement(sSQL, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 				} else {
 					st = conn.prepareStatement(sSQL);
 				}
 
 				rs = st.executeQuery();
-	
+
 				ResultSetMetaData rsMetaData = rs.getMetaData();
 				int numberOfColumns = rsMetaData == null ? 0 : rsMetaData.getColumnCount();
-	
+
 				for (int i = 1; i < numberOfColumns + 1; i++) {
 					Map<String, String> hmResult = new HashMap<String, String>();
-					hmResult.put("code", (String) (rsMetaData == null ? "" : EgovStringUtil.isNullToString(rsMetaData.getTableName(i))) );
-					hmResult.put("codeNm", (String) (rsMetaData == null ? "" : EgovStringUtil.isNullToString(rsMetaData.getColumnName(i))) );
-	
+					hmResult.put("code",
+							rsMetaData == null ? "" : EgovStringUtil.isNullToString(rsMetaData.getTableName(i)));
+					hmResult.put("codeNm",
+							rsMetaData == null ? "" : EgovStringUtil.isNullToString(rsMetaData.getColumnName(i)));
+
 					arrListResult.add(hmResult);
 				}
 			}
 		} finally {
-			//EgovResourceCloseHelper.closeDBObjects(rs, st, conn);
+			// EgovResourceCloseHelper.closeDBObjects(rs, st, conn);
 			EgovResourceCloseHelper.closeDBObjects(rs, st);
 		}
 
@@ -151,17 +160,19 @@ public class RssTagManageDao extends EgovComAbstractDAO {
 
 	/**
 	 * RSS태그관리를(을) 목록을 한다.
+	 * 
 	 * @param rssManage -조회할 정보가 담긴 객체
 	 * @return -조회한목록이담긴List
 	 * @throws Exception
 	 */
 	public List<?> selectRssTagManageList(RssManage rssManage) throws Exception {
-		return (List<?>) selectList("RssTagManage.selectRssTagManage", rssManage);
+		return selectList("RssTagManage.selectRssTagManage", rssManage);
 
 	}
 
 	/**
 	 * RSS태그관리를(을) 목록 전체 건수를(을) 조회한다.
+	 * 
 	 * @param rssManage -조회할 정보가 담긴 객체
 	 * @return -조회한건수가담긴Integer
 	 * @throws Exception
@@ -172,6 +183,7 @@ public class RssTagManageDao extends EgovComAbstractDAO {
 
 	/**
 	 * RSS태그관리를(을) 상세조회 한다.
+	 * 
 	 * @param rssManage -RSS태그관리 정보가 담김 객체
 	 * @return RssManage -RSS태그관리 정보가 담김 객체
 	 * @throws Exception
@@ -182,6 +194,7 @@ public class RssTagManageDao extends EgovComAbstractDAO {
 
 	/**
 	 * RSS태그관리를(을) 등록한다.
+	 * 
 	 * @param rssManage -RSS태그관리 정보가 담김 객체
 	 * @throws Exception
 	 */
@@ -191,6 +204,7 @@ public class RssTagManageDao extends EgovComAbstractDAO {
 
 	/**
 	 * RSS태그관리를(을) 수정한다.
+	 * 
 	 * @param rssManage -RSS태그관리 정보가 담김 객체
 	 * @throws Exception
 	 */
@@ -200,6 +214,7 @@ public class RssTagManageDao extends EgovComAbstractDAO {
 
 	/**
 	 * RSS태그관리를(을) 삭제한다.
+	 * 
 	 * @param rssManage -RSS태그관리 정보가 담김 객체
 	 * @throws Exception
 	 */
