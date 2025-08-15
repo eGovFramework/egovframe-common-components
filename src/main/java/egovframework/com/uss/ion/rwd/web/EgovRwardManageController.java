915bc7482148d4c57b02891fbe3400b3f6415cbc
이백행 <dlqorgod@naver.com>
2025-08-15 10:46:06 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/ion/rwd/web/EgovRwardManageController.java
+++ src/main/java/egovframework/com/uss/ion/rwd/web/EgovRwardManageController.java
@@ -43,21 +43,22 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  * </pre>
  * 
  * @author 이용
+ * @since 2010.06.15
  * @version 1.0
- * @created 06-15-2010 오후 2:08:56
- * 
- *          <pre>
- *  << 개정이력(Modification Information) >>
+ * @see
+ *
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
+ *   2010.06.15  이용           최초 생성
  *   2011.08.16  정진오          VcatnManageVO Dependency 제거, 사용하지 않는 객체 선언
- *   2011.8.26	정진오			IncludedInfo annotation 추가
+ *   2011.08.26  정진오          IncludedInfo annotation 추가
+ *   2025.08.15  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(final이 아닌 변수는 밑줄을 포함할 수 없음)
  *
- *          </pre>
- * 
+ *      </pre>
  */
-
 @Controller
 public class EgovRwardManageController {
 
