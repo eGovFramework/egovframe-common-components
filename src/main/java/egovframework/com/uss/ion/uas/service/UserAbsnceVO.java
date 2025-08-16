ad6115fb7b478f59f949a4096adde13bb358583e
이백행 <dlqorgod@naver.com>
2025-08-16 15:17:49 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/uss/ion/uas/service/UserAbsnceVO.java
+++ src/main/java/egovframework/com/uss/ion/uas/service/UserAbsnceVO.java
@@ -17,7 +17,7 @@ public class UserAbsnceVO extends UserAbsnce {
 	/**
 	 * serialVersionUID
 	 */
-	private static final long serialVersionUID = 1L;	
+	private static final long serialVersionUID = 1L;
 	/**
 	 * 사용자부재 목록
 	 */
@@ -28,47 +28,49 @@ public class UserAbsnceVO extends UserAbsnce {
 	String[] delYn;
 	/**
 	 * 부재여부 조회조건
-	 */	
+	 */
 	String selAbsnceAt;
-	
+
 	/**
 	 * @return the userAbsnceList
 	 */
 	public List<UserAbsnceVO> getUserAbsnceList() {
 		return userAbsnceList;
 	}
+
 	/**
 	 * @param userAbsnceList the userAbsnceList to set
 	 */
 	public void setUserAbsnceList(List<UserAbsnceVO> userAbsnceList) {
 		this.userAbsnceList = userAbsnceList;
 	}
+
 	/**
 	 * @return the delYn
 	 */
 	public String[] getDelYn() {
 		return delYn;
 	}
+
 	/**
 	 * @param delYn the delYn to set
 	 */
 	public void setDelYn(String[] delYn) {
 		this.delYn = delYn;
 	}
+
 	/**
 	 * @return the selAbsnceAt
 	 */
 	public String getSelAbsnceAt() {
 		return selAbsnceAt;
 	}
+
 	/**
 	 * @param selAbsnceAt the selAbsnceAt to set
 	 */
 	public void setSelAbsnceAt(String selAbsnceAt) {
 		this.selAbsnceAt = selAbsnceAt;
 	}
-	
 
-	
-	
 }
