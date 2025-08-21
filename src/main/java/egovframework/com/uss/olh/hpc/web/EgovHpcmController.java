ccca67d3e983d71aeeb15e97f3f13d3d71f7d75b
이백행 <dlqorgod@naver.com>
2025-08-21 10:48:34 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/olh/hpc/web/EgovHpcmController.java
+++ src/main/java/egovframework/com/uss/olh/hpc/web/EgovHpcmController.java
@@ -28,7 +28,6 @@ import egovframework.com.uss.olh.hpc.service.HpcmVO;
 import egovframework.com.utl.fcc.service.EgovStringUtil;
 
 /**
- *
  * 도움말을 처리하는 비즈니스 구현 클래스
  * 
  * @author 공통서비스 개발팀 박정규
@@ -37,17 +36,18 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  * @see
  *
  *      <pre>
-* << 개정이력(Modification Information) >>
-*
-*   수정일      수정자           수정내용
-*  -------    --------    ---------------------------
-*   2009.04.01  박정규          최초 생성
-*   2011.8.26	 정진오			IncludedInfo annotation 추가
-*   2011.09.28  서준식          return값 오류 수정
-*   2016.08.02  김연호          표준프레임워크 3.6 개선
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.04.01  박정규          최초 생성
+ *   2011.08.26  정진오          IncludedInfo annotation 추가
+ *   2011.09.28  서준식          return값 오류 수정
+ *   2016.08.02  김연호          표준프레임워크 3.6 개선
+ *   2025.08.21  이백행          2125년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(final이 아닌 변수는 밑줄을 포함할 수 없음)
+ *
  *      </pre>
  */
-
 @Controller
 public class EgovHpcmController {
 
