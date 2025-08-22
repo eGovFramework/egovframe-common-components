479396166f4d620634037d047a41c88bb019e4a2
이백행 <dlqorgod@naver.com>
2025-08-22 12:11:52 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/olp/cns/web/EgovCnsltManageController.java
+++ src/main/java/egovframework/com/uss/olp/cns/web/EgovCnsltManageController.java
@@ -40,7 +40,6 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
 import egovframework.com.utl.sim.service.EgovFileScrty;
 
 /**
- *
  * 상담내용을 처리하는 컨트롤러 클래스
  * 
  * @author 공통서비스 개발팀 박정규
@@ -49,16 +48,17 @@ import egovframework.com.utl.sim.service.EgovFileScrty;
  * @see
  *
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.04.01  박정규          최초 생성
- *   2011.8.26	정진오			IncludedInfo annotation 추가
+ *   2011.08.26  정진오          IncludedInfo annotation 추가
+ *   2025.08.22  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(final이 아닌 변수는 밑줄을 포함할 수 없음)
+ *   2025.08.22  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-UnnecessarySemicolon(필요없는 ; 문장 존재)
  *
  *      </pre>
  */
-
 @Controller
 public class EgovCnsltManageController {
 
