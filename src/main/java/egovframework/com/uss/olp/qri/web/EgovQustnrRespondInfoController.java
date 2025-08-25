1672418284b4ad9cb7d3799e016959a9d94e8cea
이백행 <dlqorgod@naver.com>
2025-08-25 16:38:02 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/olp/qri/web/EgovQustnrRespondInfoController.java
+++ src/main/java/egovframework/com/uss/olp/qri/web/EgovQustnrRespondInfoController.java
@@ -44,17 +44,17 @@ import egovframework.com.uss.olp.qrm.service.QustnrRespondManageVO;
  * @see
  *
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
- *  수정일               수정자             수정내용
- *  ----------   --------   ---------------------------
- *  2009.03.20   장동한            최초 생성
- *  2011.08.26   정진오            IncludedInfo annotation 추가
- *  2019.05.16 	 신용호             egovQustnrRespondInfoManageTemplate() 메소드 삭제 (보안취약점 대응)
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.03.20  장동한          최초 생성
+ *   2011.08.26  정진오          IncludedInfo annotation 추가
+ *   2019.05.16  신용호          egovQustnrRespondInfoManageTemplate() 메소드 삭제 (보안취약점 대응)
+ *   2025.08.25  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(final이 아닌 변수는 밑줄을 포함할 수 없음)
  *
  *      </pre>
  */
-
 @Controller
 public class EgovQustnrRespondInfoController {
 
