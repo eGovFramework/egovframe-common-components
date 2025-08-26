747d291d52e0733c289492af5d8da2f5f7d28327
이백행 <dlqorgod@naver.com>
2025-08-26 16:11:38 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/olp/qtm/service/QustnrTmplatManageVO.java
+++ src/main/java/egovframework/com/uss/olp/qtm/service/QustnrTmplatManageVO.java
@@ -14,11 +14,12 @@ import lombok.Setter;
  * @see
  *
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.03.20  장동한          최초 생성
+ *   2025.08.26  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-AvoidArrayLoops(배열의 값을 루프문을 이용하여 복사하는 것 보다, System.arraycopy() 메소드를 이용하여 복사하는 것이 효율적이며 수행 속도가 빠름)
  *
  *      </pre>
  */
