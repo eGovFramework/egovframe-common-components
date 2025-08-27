23c28d9341278ec9ad326e21663be635501050ed
이백행 <dlqorgod@naver.com>
2025-08-27 13:49:57 +0900

    개정이력 수정

--- src/main/java/egovframework/com/uss/sam/stp/web/EgovStplatManageController.java
+++ src/main/java/egovframework/com/uss/sam/stp/web/EgovStplatManageController.java
@@ -26,7 +26,6 @@ import egovframework.com.uss.sam.stp.service.StplatManageVO;
 import egovframework.com.utl.fcc.service.EgovStringUtil;
 
 /**
- *
  * 약관내용을 처리하는 비즈니스 구현 클래스
  * 
  * @author 공통서비스 개발팀 박정규
@@ -35,13 +34,14 @@ import egovframework.com.utl.fcc.service.EgovStringUtil;
  * @see
  *
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.04.01  박정규          최초 생성
- *   2011.8.26	정진오			IncludedInfo annotation 추가
- *   2016.06.13   장동한         표준프레임워크 v3.6 개선
+ *   2011.08.26  정진오          IncludedInfo annotation 추가
+ *   2016.06.13  장동한          표준프레임워크 v3.6 개선
+ *   2025.08.27  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(final이 아닌 변수는 밑줄을 포함할 수 없음)
  *
  *      </pre>
  */
