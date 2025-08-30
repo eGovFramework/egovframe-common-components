df126c2bf67a47f05831a73c60f58ce1c0db3b62
이백행 <dlqorgod@naver.com>
2025-08-30 20:30:41 +0900

    개정이력 수정

--- src/main/java/egovframework/com/utl/fcc/service/EgovDateUtil.java
+++ src/main/java/egovframework/com/utl/fcc/service/EgovDateUtil.java
@@ -14,7 +14,6 @@ import java.util.TimeZone;
 import com.ibm.icu.util.ChineseCalendar;
 
 /**
- *
  * Date 에 대한 Util 클래스
  * 
  * @author 공통서비스 개발팀 이중호
@@ -23,11 +22,14 @@ import com.ibm.icu.util.ChineseCalendar;
  * @see
  *
  *      <pre>
- * << 개정이력(Modification Information) >>
+ *  == 개정이력(Modification Information) ==
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.02.01  이중호          최초 생성
+ *   2025.08.30  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-UselessParentheses(불필요한 괄호사용)
+ *   2025.08.30  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-LocalVariableNamingConventions(final이 아닌 변수는 밑줄을 포함할 수 없음)
+ *   2025.08.30  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-AvoidReassigningParameters(넘겨받는 메소드 parameter 값을 직접 변경하는 코드 탐지)
  *
  *      </pre>
  */
