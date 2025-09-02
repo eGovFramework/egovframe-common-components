e22720aaa08e5b2e2322f7e6ec0fdfffb17602b1
이백행 <dlqorgod@naver.com>
2025-09-03 05:38:30 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/utl/pao/service/PrntngOutptVO.java
+++ src/main/java/egovframework/com/utl/pao/service/PrntngOutptVO.java
@@ -5,19 +5,20 @@ import java.io.Serializable;
 /**
  *
  * 관인이미지 모델 클래스
+ * 
  * @author 공통서비스 개발팀 이중호
  * @since 2009.02.01
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.02.01  이중호          최초 생성
  *
- * </pre>
+ *      </pre>
  */
 public class PrntngOutptVO implements Serializable {
 
@@ -26,25 +27,26 @@ public class PrntngOutptVO implements Serializable {
 	/*
 	 * 이미지정보
 	 */
-    private byte[] imgInfo;
+	private byte[] imgInfo;
 
-    /*
-     * 이미지타입
-     */
+	/*
+	 * 이미지타입
+	 */
 	private String imgType;
 
 	/*
 	 * 기관코드
 	 */
-    private String orgCode;
+	private String orgCode;
 
-    /*
-     * 관인구분
-     */
-    private String erncslSe;
+	/*
+	 * 관인구분
+	 */
+	private String erncslSe;
 
 	/**
 	 * imgInfo attribute 를 리턴한다.
+	 * 
 	 * @return byte[]
 	 */
 	public byte[] getImgInfo() {
@@ -62,18 +64,20 @@ public class PrntngOutptVO implements Serializable {
 
 	/**
 	 * imgInfo attribute 값을 설정한다.
+	 * 
 	 * @param imgInfo byte[]
 	 */
 	public void setImgInfo(byte[] imgInfo) {
 		this.imgInfo = new byte[imgInfo.length];
 
-		for (int i = 0; i <  imgInfo.length; ++i) {
+		for (int i = 0; i < imgInfo.length; ++i) {
 			this.imgInfo[i] = imgInfo[i];
 		}
 	}
 
 	/**
 	 * imgType attribute 를 리턴한다.
+	 * 
 	 * @return String
 	 */
 	public String getImgType() {
@@ -82,6 +86,7 @@ public class PrntngOutptVO implements Serializable {
 
 	/**
 	 * imgType attribute 값을 설정한다.
+	 * 
 	 * @param imgType String
 	 */
 	public void setImgType(String imgType) {
@@ -90,6 +95,7 @@ public class PrntngOutptVO implements Serializable {
 
 	/**
 	 * orgCode attribute 를 리턴한다.
+	 * 
 	 * @return String
 	 */
 	public String getOrgCode() {
@@ -98,6 +104,7 @@ public class PrntngOutptVO implements Serializable {
 
 	/**
 	 * orgCode attribute 값을 설정한다.
+	 * 
 	 * @param orgCode String
 	 */
 	public void setOrgCode(String orgCode) {
@@ -106,6 +113,7 @@ public class PrntngOutptVO implements Serializable {
 
 	/**
 	 * erncslSe attribute 를 리턴한다.
+	 * 
 	 * @return String
 	 */
 	public String getErncslSe() {
@@ -114,11 +122,11 @@ public class PrntngOutptVO implements Serializable {
 
 	/**
 	 * erncslSe attribute 값을 설정한다.
+	 * 
 	 * @param erncslSe String
 	 */
 	public void setErncslSe(String erncslSe) {
 		this.erncslSe = erncslSe;
 	}
-
 
 }
