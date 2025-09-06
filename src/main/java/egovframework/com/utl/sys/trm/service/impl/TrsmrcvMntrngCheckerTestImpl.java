9bc05d54390190488e2fcedc8b3ed426d41dce0e
Samuel-Lee-Kr <sungyu623@gmail.com>
2025-09-06 10:22:16 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/utl/sys/trm/service/impl/TrsmrcvMntrngCheckerTestImpl.java
+++ src/main/java/egovframework/com/utl/sys/trm/service/impl/TrsmrcvMntrngCheckerTestImpl.java
@@ -1,6 +1,5 @@
 package egovframework.com.utl.sys.trm.service.impl;
 
-
 import java.security.SecureRandom;
 
 import org.slf4j.Logger;
@@ -13,12 +12,14 @@ import egovframework.com.utl.sys.trm.service.TrsmrcvMntrngResult;
  * @Class Name : EgovTrsmrcvMntrngCheckerImpl.java
  * @Description : 송수신모니터링을 위한 Check interface 예제 구현클래스
  * @Modification Information
- *
+ * 
+ *               <pre>
  *    수정일       수정자         수정내용
  *    -------        -------     -------------------
  *    2010.08.16     김진만   최초생성
- *
- * @author  김진만
+ *               </pre>
+ * 
+ * @author 김진만
  * @version
  * @see
  *
@@ -27,23 +28,24 @@ import egovframework.com.utl.sys.trm.service.TrsmrcvMntrngResult;
 public class TrsmrcvMntrngCheckerTestImpl implements TrsmrcvMntrngChecker {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(TrsmrcvMntrngCheckerTestImpl.class);
-	private static SecureRandom oRandom = new SecureRandom();		// 221115	김혜준	2022 시큐어코딩 조치
+	private static SecureRandom oRandom = new SecureRandom(); // 221115 김혜준 2022 시큐어코딩 조치
 
 	/**
 	 * 송수신모니터링을 수행한다.
 	 *
-	 * 연계ID를 이용하여 연계기관과 통신에 필요한 정보를 얻은 다음 연계기관과 통신을 수행한다.
-	 * 통신결과를 TrsmrcvMntrngResult 클래스 객체에 담아서 리턴한다.
+	 * 연계ID를 이용하여 연계기관과 통신에 필요한 정보를 얻은 다음 연계기관과 통신을 수행한다. 통신결과를 TrsmrcvMntrngResult
+	 * 클래스 객체에 담아서 리턴한다.
 	 *
-	 * 통신결과가 true일때 : TrsmrcvMntrngResult의 nrmltAt에 true, cause에 null을 저장.
-	 * 통신결과가 false일때: TrsmrcvMntrngResult의 nrmltAt에 false, cause에 에러원인 Exception을 저장한다.
+	 * 통신결과가 true일때 : TrsmrcvMntrngResult의 nrmltAt에 true, cause에 null을 저장. 통신결과가
+	 * false일때: TrsmrcvMntrngResult의 nrmltAt에 false, cause에 에러원인 Exception을 저장한다.
 	 *
 	 *
 	 * @return 모니터링결과
 	 *
-	 * @param cntcId   모니터링 대상 연계ID
+	 * @param cntcId 모니터링 대상 연계ID
 	 *
 	 */
+	@Override
 	public TrsmrcvMntrngResult check(String cntcId) {
 		Boolean b = oRandom.nextBoolean();
 		TrsmrcvMntrngResult result = null;
@@ -51,9 +53,10 @@ public class TrsmrcvMntrngCheckerTestImpl implements TrsmrcvMntrngChecker {
 		if (b.booleanValue()) {
 			result = new TrsmrcvMntrngResult(b.booleanValue(), null);
 		} else {
-			result = new TrsmrcvMntrngResult(b.booleanValue(), new UnsupportedOperationException("송수신샘플Check클래스에서 발생한 Exception입니다."));
+			result = new TrsmrcvMntrngResult(b.booleanValue(),
+					new UnsupportedOperationException("송수신샘플Check클래스에서 발생한 Exception입니다."));
 		}
 		LOGGER.debug("result cause : {}", result.getCause());
-		return result ;
+		return result;
 	}
 }
