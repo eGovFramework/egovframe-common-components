c88015f7a4509c35474757360134c77b6fba0cdc
이백행 <dlqorgod@naver.com>
2025-09-11 04:39:47 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/utl/sim/service/EgovXMLDoc.java
+++ src/main/java/egovframework/com/utl/sim/service/EgovXMLDoc.java
@@ -48,7 +48,7 @@ public class EgovXMLDoc {
 
 	// 파일구분자
 	static final char FILE_SEPARATOR = File.separatorChar;
-	
+
 	static final String ACCESS_EXTERNAL_DTD = "http://javax.xml.XMLConstants/property/accessExternalDTD";
 	static final String ACCESS_EXTERNAL_STYLESHEET = "http://javax.xml.XMLConstants/property/accessExternalStylesheet";
 	static final String EXTERNAL_GENERAL_ENTITIES = "http://xml.org/sax/features/external-general-entities";
@@ -56,6 +56,7 @@ public class EgovXMLDoc {
 
 	/**
 	 * XML파일을 파싱하여 메일발송 클래스(임의)에 내용을 담아 반환
+	 * 
 	 * @param file XML파일
 	 * @return SndngMailDocument mailDoc 메일발송 클래스(XML스키마를 통해 생성된 자바클래스)
 	 */
@@ -65,7 +66,7 @@ public class EgovXMLDoc {
 
 		String storePathString = EgovProperties.getProperty("Globals.fileStorePath");
 		try {
-			File xmlFile = new File(storePathString,FilenameUtils.getName(file));
+			File xmlFile = new File(storePathString, FilenameUtils.getName(file));
 			if (xmlFile.exists() && xmlFile.isFile()) {
 				fis = new FileInputStream(xmlFile);
 				mailDoc = SndngMailDocument.Factory.parse(xmlFile);
@@ -80,8 +81,9 @@ public class EgovXMLDoc {
 
 	/**
 	 * XML데이터를 XML파일로 저장
+	 * 
 	 * @param mailDoc 사용자 임의 클래스(XML스키마를 통해 생성된 자바클래스)
-	 * @param file 저장될 파일
+	 * @param file    저장될 파일
 	 * @return boolean 저장여부 True / False
 	 */
 	public static boolean getClassToXML(SndngMailDocument mailDoc, String file) throws Exception {
@@ -91,8 +93,8 @@ public class EgovXMLDoc {
 		String storePathString = EgovProperties.getProperty("Globals.fileStorePath");
 
 		try {
-			file = EgovFileTool.createNewFile(storePathString,FilenameUtils.getName(file));
-			File xmlFile = new File(storePathString,FilenameUtils.getName(file));
+			file = EgovFileTool.createNewFile(storePathString, FilenameUtils.getName(file));
+			File xmlFile = new File(storePathString, FilenameUtils.getName(file));
 			fos = new FileOutputStream(xmlFile);
 
 			XmlOptions xmlOptions = new XmlOptions();
@@ -112,6 +114,7 @@ public class EgovXMLDoc {
 
 	/**
 	 * XML 파일을 파싱하여 데이터를 조작할 수 있는 Document 객체를 반환
+	 * 
 	 * @param xml XML파일
 	 * @return Document document 문서객체
 	 */
@@ -121,7 +124,7 @@ public class EgovXMLDoc {
 		String storePathString = EgovProperties.getProperty("Globals.fileStorePath");
 
 		try {
-			File srcFile = new File(storePathString,FilenameUtils.getName(xml));
+			File srcFile = new File(storePathString, FilenameUtils.getName(xml));
 			if (srcFile.exists() && srcFile.isFile()) {
 				fis = new FileInputStream(srcFile);
 				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -145,6 +148,7 @@ public class EgovXMLDoc {
 
 	/**
 	 * Document의 최상의 Element로 이동
+	 * 
 	 * @param document XML데이터
 	 * @return Element root 루트
 	 */
@@ -154,9 +158,10 @@ public class EgovXMLDoc {
 
 	/**
 	 * 하위에 새로운 Elemenet를 생성
+	 * 
 	 * @param document XML데이터
-	 * @param rt 추가될위치
-	 * @param id 생성될 Element의 ID
+	 * @param rt       추가될위치
+	 * @param id       생성될 Element의 ID
 	 * @return Element element 추가된 Element
 	 */
 	public static Element insertElement(Document document, Element rt, String id) throws Exception {
@@ -176,10 +181,11 @@ public class EgovXMLDoc {
 
 	/**
 	 * 하위에 문자열을 가지는 새로운 Elemenet를 생성
+	 * 
 	 * @param document XML데이터
-	 * @param rt 추가 위치
-	 * @param id 생성될 Element의 ID
-	 * @param text Element 하위에 들어갈 문자열
+	 * @param rt       추가 위치
+	 * @param id       생성될 Element의 ID
+	 * @param text     Element 하위에 들어갈 문자열
 	 * @return Element element 추가된 Element
 	 */
 	public static Element insertElement(Document document, Element rt, String id, String text) throws Exception {
@@ -202,9 +208,10 @@ public class EgovXMLDoc {
 
 	/**
 	 * 하위에 문자열을 추가
+	 * 
 	 * @param document XML데이터
-	 * @param rt 추가 위치
-	 * @param text Element 하위에 들어갈 문자열
+	 * @param rt       추가 위치
+	 * @param text     Element 하위에 들어갈 문자열
 	 * @return Element element 추가된 Element
 	 */
 	public static Text insertText(Document document, Element rt, String text) throws Exception {
@@ -224,6 +231,7 @@ public class EgovXMLDoc {
 
 	/**
 	 * 마지막으로 입력되었거나 참조된 XML Node의 상위 Element를 리턴
+	 * 
 	 * @param current 현재노드
 	 * @return Element parent 상위노드
 	 */
@@ -234,15 +242,16 @@ public class EgovXMLDoc {
 
 	/**
 	 * Document 객체를 XML파일로 저장
+	 * 
 	 * @param document 문서객체
-	 * @param file 저장될 파일
+	 * @param file     저장될 파일
 	 * @return boolean 저장여부 True / False
 	 */
 	public static boolean getXMLFile(Document document, String file) throws Exception {
 		boolean retVal = false;
 		String storePathString = EgovProperties.getProperty("Globals.fileStorePath");
 
-		File srcFile = new File(storePathString,FilenameUtils.getName(file));
+		File srcFile = new File(storePathString, FilenameUtils.getName(file));
 		if (srcFile.exists() && srcFile.isFile()) {
 			Source source = new DOMSource(document);
 			Result result = new StreamResult(srcFile);
