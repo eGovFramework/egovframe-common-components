82e36c493eb39f604699e0c35ce10a046a824a06
이백행 <dlqorgod@naver.com>
2025-09-12 12:01:56 +0900

    개정이력 수정

--- src/main/java/egovframework/com/utl/sys/dbm/service/DbMntrngChecker.java
+++ src/main/java/egovframework/com/utl/sys/dbm/service/DbMntrngChecker.java
@@ -12,25 +12,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
 /**
- * @Class Name : EgovDbMntrngChecker.java
- * @Description : DB서비스모니터링을 위한 Check 클래스
- * @Modification Information
- * 
- *               <pre>
- *    수정일       	    수정자         수정내용
- *    ----------  -------   -------------------
- *    2010.07.13   김진만         	최초생성
- *    2017-02-08   이정은         	시큐어코딩(ES) - 시큐어코딩 부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
- *    2019-12-11   신용호         	KISA 보안약점 조치 (오류 상황 대응 부재)
- *               </pre>
+ * DB서비스모니터링을 위한 Check 클래스
  * 
  * @author 김진만
  * @since 2010.07.13
- * @version
+ * @version 1.0
  * @see
  *
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2010.07.13  김진만          최초 생성
+ *   2017.02.08  이정은          시큐어코딩(ES) - 시큐어코딩 부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
+ *   2019.12.11  신용호          KISA 보안약점 조치 (오류 상황 대응 부재)
+ *   2025.09.12  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-CloseResource(부적절한 자원 해제)
+ *
+ *      </pre>
  */
-
 public class DbMntrngChecker {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(DbMntrngChecker.class);
