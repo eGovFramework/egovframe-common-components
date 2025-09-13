cb02a5843ccd83e8719568ceec21be0a1e850da7
이백행 <dlqorgod@naver.com>
2025-09-13 12:20:59 +0900

    개정이력 수정

--- src/main/java/egovframework/com/utl/sys/htm/service/HttpMntrngChecker.java
+++ src/main/java/egovframework/com/utl/sys/htm/service/HttpMntrngChecker.java
@@ -7,24 +7,25 @@ import egovframework.com.cmm.EgovWebUtil;
 import egovframework.com.cmm.service.Globals;
 
 /**
- * @Class Name : HttpMntrngChecker.java
- * @Description : HTTP서비스모니터링을 위한 Check 클래스
- * @Modification Information
- * 
- *               <pre>
- *    수정일                수정자         수정내용
- *    ----------   -------   -------------------
- *    2010.09.06   박종선          최초생성
- *    2019.12.05   신용호          KISA 보안약점 조치 (경로조작및 자원 삽입,부적절한 예외처리)
- *               </pre>
+ * HTTP서비스모니터링을 위한 Check 클래스
  * 
  * @author 박종선
  * @since 2010.05.01
- * @version
+ * @version 1.0
  * @see
  *
+ *      <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2010.09.06  박종선          최초생성
+ *   2019.12.05  신용호          KISA 보안약점 조치 (경로조작및 자원 삽입,부적절한 예외처리)
+ *   2025.09.13  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-AvoidUsingHardCodedIP(하드코드된 중요정보)
+ *   2025.09.13  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-AvoidReassigningParameters(넘겨받는 메소드 parameter 값을 직접 변경하는 코드 탐지)
+ *
+ *      </pre>
  */
-
 public class HttpMntrngChecker {
 
 	/**
