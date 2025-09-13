f2f4308c91f279b7f4efaed6817a4c4f81449571
이백행 <dlqorgod@naver.com>
2025-09-13 12:13:36 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/utl/sys/htm/service/HttpMntrngChecker.java
+++ src/main/java/egovframework/com/utl/sys/htm/service/HttpMntrngChecker.java
@@ -10,13 +10,15 @@ import egovframework.com.cmm.EgovWebUtil;
  * @Class Name : HttpMntrngChecker.java
  * @Description : HTTP서비스모니터링을 위한 Check 클래스
  * @Modification Information
- *
+ * 
+ *               <pre>
  *    수정일                수정자         수정내용
  *    ----------   -------   -------------------
  *    2010.09.06   박종선          최초생성
  *    2019.12.05   신용호          KISA 보안약점 조치 (경로조작및 자원 삽입,부적절한 예외처리)
- *
- * @author  박종선
+ *               </pre>
+ * 
+ * @author 박종선
  * @since 2010.05.01
  * @version
  * @see
@@ -25,40 +27,40 @@ import egovframework.com.cmm.EgovWebUtil;
 
 public class HttpMntrngChecker {
 
-	//@Resource(name = "HttpMonService")
-	//private EgovHttpMonService httpMonService;	
+	// @Resource(name = "HttpMonService")
+	// private EgovHttpMonService httpMonService;
 
 	// 파일구분자
 	static final char FILE_SEPARATOR = File.separatorChar;
 
 	// 최대 문자길이
 	static final int MAX_STR_LEN = 1024;
-	
+
 	// 화이트 리스트
-	static private String[] whiteListURL = { "wwww.egovwebserver.go.kr"
-												,"wwww.egovwasserver.go.kr"
-												,"192.168.100.133" };
+	static private String[] whiteListURL = { "wwww.egovwebserver.go.kr", "wwww.egovwasserver.go.kr",
+			"192.168.100.133" };
 
 	/**
 	 * 시스템에 존재하는 서버의 실행상태 정보를 조회하는 기능
+	 * 
 	 * @param String sitUrl 사용포트
 	 * @return String status 실행상태
 	 * @exception Exception
-	*/
+	 */
 	public static String getPrductStatus(String siteUrl) throws IOException {
 
 		boolean isAuth = false;
 
-		for (String urlPattern : whiteListURL)
-		{
-			if (siteUrl.contains(urlPattern))
-			{
+		for (String urlPattern : whiteListURL) {
+			if (siteUrl.contains(urlPattern)) {
 				isAuth = true;
 				break;
 			}
 		}
-		if ( !isAuth ) throw new IOException("UnRegistered site URL : "+siteUrl);
-		
+		if (!isAuth) {
+			throw new IOException("UnRegistered site URL : " + siteUrl);
+		}
+
 		String httpSttusCd = null;
 
 		try {
