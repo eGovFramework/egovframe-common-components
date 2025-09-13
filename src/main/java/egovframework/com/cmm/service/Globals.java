356789163745a1c7899b730896f9ce37568f2189
이백행 <dlqorgod@naver.com>
2025-09-13 12:40:31 +0900

    주석 수정

--- src/main/java/egovframework/com/cmm/service/Globals.java
+++ src/main/java/egovframework/com/cmm/service/Globals.java
@@ -19,48 +19,100 @@ package egovframework.com.cmm.service;
  *      </pre>
  */
 public class Globals {
-	// OS 유형
+
+	/**
+	 * OS 유형
+	 */
 	public static final String OS_TYPE = EgovProperties.getProperty("Globals.OsType");
-	// DB 유형
+
+	/**
+	 * DB 유형
+	 */
 	public static final String DB_TYPE = EgovProperties.getProperty("Globals.DbType");
-	// 메인 페이지
+
+	/**
+	 * 메인 페이지
+	 */
 	public static final String MAIN_PAGE = EgovProperties.getProperty("Globals.MainPage");
-	// ShellFile 경로
+
+	/**
+	 * ShellFile 경로
+	 */
 	public static final String SHELL_FILE_PATH = EgovProperties.getPathProperty("Globals.ShellFilePath");
-	// 퍼로퍼티 파일 위치
+
+	/**
+	 * 퍼로퍼티 파일 위치
+	 */
 	public static final String CONF_PATH = EgovProperties.getPathProperty("Globals.ConfPath");
-	// Server정보 프로퍼티 위치
+
+	/**
+	 * Server정보 프로퍼티 위치
+	 */
 	public static final String SERVER_CONF_PATH = EgovProperties.getPathProperty("Globals.ServerConfPath");
-	// Client정보 프로퍼티 위치
+
+	/**
+	 * Client정보 프로퍼티 위치
+	 */
 	public static final String CLIENT_CONF_PATH = EgovProperties.getPathProperty("Globals.ClientConfPath");
-	// 파일포맷 정보 프로퍼티 위치
+
+	/**
+	 * 파일포맷 정보 프로퍼티 위치
+	 */
 	public static final String FILE_FORMAT_PATH = EgovProperties.getPathProperty("Globals.FileFormatPath");
 
-	// 파일 업로드 원 파일명
+	/**
+	 * 파일 업로드 원 파일명
+	 */
 	public static final String ORIGIN_FILE_NM = "originalFileName";
-	// 파일 확장자
+
+	/**
+	 * 파일 확장자
+	 */
 	public static final String FILE_EXT = "fileExtension";
-	// 파일크기
+
+	/**
+	 * 파일크기
+	 */
 	public static final String FILE_SIZE = "fileSize";
-	// 업로드된 파일명
+
+	/**
+	 * 업로드된 파일명
+	 */
 	public static final String UPLOAD_FILE_NM = "uploadFileName";
-	// 파일경로
+
+	/**
+	 * 파일경로
+	 */
 	public static final String FILE_PATH = "filePath";
 
-	// 메일발송요청 XML파일경로
+	/**
+	 * 메일발송요청 XML파일경로
+	 */
 	public static final String MAIL_REQUEST_PATH = EgovProperties.getPathProperty("Globals.MailRequestPath");
-	// 메일발송응답 XML파일경로
+
+	/**
+	 * 메일발송응답 XML파일경로
+	 */
 	public static final String MAIL_RESPONSE_PATH = EgovProperties.getPathProperty("Globals.MailRResponsePath");
 
-	// G4C 연결용 IP (localhost)
+	/**
+	 * G4C 연결용 IP (localhost)
+	 */
 	public static final String LOCAL_IP = EgovProperties.getProperty("Globals.LocalIp");
 
-	// SMS 정보 프로퍼티 위치
+	/**
+	 * SMS 정보 프로퍼티 위치
+	 */
 	public static final String SMSDB_CONF_PATH = EgovProperties.getPathProperty("Globals.SmsDbConfPath");
 
-	// 파일 업로드 가능 확장자들
+	/**
+	 * 파일 업로드 가능 확장자들
+	 */
 	public static final String FILE_UP_EXTS = EgovProperties.getProperty("Globals.fileUpload.Extensions");
-	// 파일 업로드 최대 용량
+
+	/**
+	 * 파일 업로드 최대 용량
+	 */
 	public static final String FILE_UP_MAX_SIZE = EgovProperties.getProperty("Globals.fileUpload.maxSize");
 
 	/**
