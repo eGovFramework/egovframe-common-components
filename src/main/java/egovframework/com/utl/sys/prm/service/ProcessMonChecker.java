9a3f4554c95b3db2c4526705697c6efa5b6e188e
이백행 <dlqorgod@naver.com>
2025-09-15 00:14:21 +0900

    이클립스 > Source > Format

--- src/main/java/egovframework/com/utl/sys/prm/service/ProcessMonChecker.java
+++ src/main/java/egovframework/com/utl/sys/prm/service/ProcessMonChecker.java
@@ -10,16 +10,19 @@ import egovframework.com.cmm.service.Globals;
 import egovframework.com.cmm.util.EgovResourceCloseHelper;
 
 /**
+ * <pre>
  * 개요
  * - 프로세스 모니터링을 위한 Check 클래스
  *
  * 상세내용
  * - 프로세스의 상태 결과를 제공한다.
+ * </pre>
+ * 
  * @author 박종선
  * @version 1.0
  * @created 7-9-2010
  * 
- * <pre>
+ *          <pre>
  * << 개정이력(Modification Information) >>
  *
  *  수정일               수정자            수정내용
@@ -27,19 +30,21 @@ import egovframework.com.cmm.util.EgovResourceCloseHelper;
  *  2019.12.06   신용호             KISA 보안약점 조치 (부적절한 예외처리)
  *  2022.11.11   김혜준             시큐어코딩 처리
  *
- * </pre>
+ *          </pre>
  * 
  */
 
 public class ProcessMonChecker {
 
 	// Log
-	//private static final Logger LOGGER = LoggerFactory.getLogger(ProcessMonChecker.class);
+	// private static final Logger LOGGER =
+	// LoggerFactory.getLogger(ProcessMonChecker.class);
 
 	/**
 	 * <pre>
 	 * Comment : 프로세스 정보를 확인한다. (
 	 * </pre>
+	 * 
 	 * @param String processName
 	 * @return List<String[]> 프로세스 정보를 리턴한다.
 	 * @version 1.0 (2009.01.12.)
@@ -59,14 +64,14 @@ public class ProcessMonChecker {
 			// 2011.10.10 보안점검 후속조치 끝
 
 			if ("WINDOWS".equals(Globals.OS_TYPE)) {
-				cnt = -1; //윈도우의 경우 정상 프로세스 일때 두번째 줄에 결과를 리턴한다.
+				cnt = -1; // 윈도우의 경우 정상 프로세스 일때 두번째 줄에 결과를 리턴한다.
 				String execStr = "tasklist /fo table /nh /fi \"imagename eq " + processNm + "\"";
 				// 2022.11.11 시큐어코딩 처리
 				FileSystemUtils util = new FileSystemUtils();
 				p = util.processOperate("EgovNetworkState", execStr);
 
 			} else if ("UNIX".equals(Globals.OS_TYPE)) {
-				String cmd = "/bin/csh" + "-c" + "ps -A | grep " + EgovWebUtil.removeOSCmdRisk(processNm); 
+				String cmd = "/bin/csh" + "-c" + "ps -A | grep " + EgovWebUtil.removeOSCmdRisk(processNm);
 				// 2022.11.11 시큐어코딩 처리
 				FileSystemUtils util = new FileSystemUtils();
 				p = util.processOperate("EgovNetworkState", cmd);
