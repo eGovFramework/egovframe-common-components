f50a594f2ba213f42f93e31ed75021916d4f297b
dasomel <dasomell@gmail.com>
2026-05-20 18:49:46 +0900

    refactor: apply Lombok @Getter/@Setter to core cmm VO classes
    Replace manual getter/setter methods with Lombok annotations in
    LoginVO, ComDefaultCodeVO, and IncludedCompInfoVO. All accessors
    were pure field delegates with no business logic, making them
    safe to replace. Existing validation annotations (@EgovNullCheck,
    @EgovEmailCheck), serialVersionUID, and toString() overrides are
    preserved as-is.
    
    - LoginVO: removed 26 accessor methods (13 getters + 13 setters)
    - ComDefaultCodeVO: removed 14 accessor methods (7 getters + 7 setters)
    - IncludedCompInfoVO: removed 8 accessor methods (4 getters + 4 setters)
    
    ComDefaultVO and SessionVO are intentionally excluded: ComDefaultVO
    contains null-guarding logic in its accessors, and SessionVO uses
    mismatched setter parameter names that Lombok cannot replicate.

--- src/main/java/egovframework/com/cmm/IncludedCompInfoVO.java
+++ src/main/java/egovframework/com/cmm/IncludedCompInfoVO.java
@@ -1,12 +1,15 @@
 package egovframework.com.cmm;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * IncludedCompInfoVO 클래스
- * 
+ *
  * <p>
  *  Description : IncludedInfo annotation을 바탕으로 화면에 표시할 정보를 구성하기 위한 VO 클래스
  * </p>
- * 
+ *
  * @author 공통컴포넌트 정진오
  * @since 2011.08.26
  * @version 2.0.0
@@ -14,42 +17,20 @@ package egovframework.com.cmm;
  *
  * <pre>
  * << 개정이력(Modification Information) >>
- *   
+ *
  *  수정일		수정자		수정내용
  *  -------    	--------    ---------------------------
  *  2011.08.26	정진오 		최초 생성
  * </pre>
- * 
+ *
  */
+@Getter
+@Setter
 public class IncludedCompInfoVO {
-	
+
 	private String name;
 	private String listUrl;
 	private int order;
 	private int gid;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getListUrl() {
-		return listUrl;
-	}
-	public void setListUrl(String listUrl) {
-		this.listUrl = listUrl;
-	}
-	public int getOrder() {
-		return order;
-	}
-	public void setOrder(int order) {
-		this.order = order;
-	}
-	public int getGid() {
-		return gid;
-	}
-	public void setGid(int gid) {
-		this.gid = gid;
-	}
+
 }
--- src/main/java/egovframework/com/cmm/LoginVO.java
+++ src/main/java/egovframework/com/cmm/LoginVO.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import org.egovframe.rte.ptl.reactive.validation.EgovNullCheck;
 import org.egovframe.rte.ptl.reactive.validation.EgovEmailCheck;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @Class Name : LoginVO.java
  * @Description : Login VO class
@@ -23,15 +26,17 @@ import org.egovframe.rte.ptl.reactive.validation.EgovEmailCheck;
  *  @since 2009.03.03
  *  @version 1.0
  *  @see
- *  
+ *
  */
+@Getter
+@Setter
 public class LoginVO implements Serializable{
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -8274004534207618049L;
-	
+
 	/** 아이디 */
 	@EgovNullCheck
 	private String id;
@@ -70,229 +75,5 @@ public class LoginVO implements Serializable{
 	private String onepassUserkey;
 	/** 디지털원패스 사용자세션값 */
 	private String onepassIntfToken;
-
-	/**
-	 * id attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getId() {
-		return id;
-	}
-	/**
-	 * id attribute 값을 설정한다.
-	 * @param id String
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
-	/**
-	 * name attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getName() {
-		return name;
-	}
-	/**
-	 * name attribute 값을 설정한다.
-	 * @param name String
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	/**
-	 * ihidNum attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getIhidNum() {
-		return ihidNum;
-	}
-	/**
-	 * ihidNum attribute 값을 설정한다.
-	 * @param ihidNum String
-	 */
-	public void setIhidNum(String ihidNum) {
-		this.ihidNum = ihidNum;
-	}
-	/**
-	 * email attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getEmail() {
-		return email;
-	}
-	/**
-	 * email attribute 값을 설정한다.
-	 * @param email String
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	/**
-	 * password attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getPassword() {
-		return password;
-	}
-	/**
-	 * password attribute 값을 설정한다.
-	 * @param password String
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	/**
-	 * passwordHint attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getPasswordHint() {
-		return passwordHint;
-	}
-	/**
-	 * passwordHint attribute 값을 설정한다.
-	 * @param passwordHint String
-	 */
-	public void setPasswordHint(String passwordHint) {
-		this.passwordHint = passwordHint;
-	}
-	/**
-	 * passwordCnsr attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getPasswordCnsr() {
-		return passwordCnsr;
-	}
-	/**
-	 * passwordCnsr attribute 값을 설정한다.
-	 * @param passwordCnsr String
-	 */
-	public void setPasswordCnsr(String passwordCnsr) {
-		this.passwordCnsr = passwordCnsr;
-	}
-	/**
-	 * userSe attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getUserSe() {
-		return userSe;
-	}
-	/**
-	 * userSe attribute 값을 설정한다.
-	 * @param userSe String
-	 */
-	public void setUserSe(String userSe) {
-		this.userSe = userSe;
-	}
-	/**
-	 * orgnztId attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getOrgnztId() {
-		return orgnztId;
-	}
-	/**
-	 * orgnztId attribute 값을 설정한다.
-	 * @param orgnztId String
-	 */
-	public void setOrgnztId(String orgnztId) {
-		this.orgnztId = orgnztId;
-	}
-	/**
-	 * uniqId attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getUniqId() {
-		return uniqId;
-	}
-	/**
-	 * uniqId attribute 값을 설정한다.
-	 * @param uniqId String
-	 */
-	public void setUniqId(String uniqId) {
-		this.uniqId = uniqId;
-	}
-	/**
-	 * url attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getUrl() {
-		return url;
-	}
-	/**
-	 * url attribute 값을 설정한다.
-	 * @param url String
-	 */
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	/**
-	 * ip attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getIp() {
-		return ip;
-	}
-	/**
-	 * ip attribute 값을 설정한다.
-	 * @param ip String
-	 */
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
-	/**
-	 * dn attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getDn() {
-		return dn;
-	}
-	/**
-	 * dn attribute 값을 설정한다.
-	 * @param dn String
-	 */
-	public void setDn(String dn) {
-		this.dn = dn;
-	}
-	/**
-	 * @return the orgnztNm
-	 */
-	public String getOrgnztNm() {
-		return orgnztNm;
-	}
-	/**
-	 * @param orgnztNm the orgnztNm to set
-	 */
-	public void setOrgnztNm(String orgnztNm) {
-		this.orgnztNm = orgnztNm;
-	}
-	
-	/**
-	 * 디지털원패스 사용자키를 리턴한다.
-	 * @return onepassUserkey
-	 */
-	public String getOnepassUserkey() {
-		return onepassUserkey;
-	}
-	/**
-	 * 디지털원패스 사용자키를 설정한다.
-	 * @param onepassUserkey
-	 */
-	public void setOnepassUserkey(String onepassUserkey) {
-		this.onepassUserkey = onepassUserkey;
-	}
-	/**
-	 * 디지털원패스 사용자세션값을 리턴한다.
-	 * @return
-	 */
-	public String getOnepassIntfToken() {
-		return onepassIntfToken;
-	}
-	/**
-	 * 디지털원패스 사용자세션값을 설정한다.
-	 * @param onepassIntfToken
-	 */
-	public void setOnepassIntfToken(String onepassIntfToken) {
-		this.onepassIntfToken = onepassIntfToken;
-	}
 
 }
