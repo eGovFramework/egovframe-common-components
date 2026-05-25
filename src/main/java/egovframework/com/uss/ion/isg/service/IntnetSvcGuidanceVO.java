7fca9df87d0c267c85079342ffdfe313207be147
dasomel <dasomell@gmail.com>
2026-05-26 01:11:02 +0900

    refactor: uss/ion (evt/isg) 패키지 VO에 Lombok @Getter/@Setter 적용
    uss/ion 하위 evt, isg 패키지 VO 클래스에 수동으로 작성된 getter/setter를
    Lombok 어노테이션으로 대체하여 보일러플레이트를 제거한다.
    
    - EventManageVO: @Getter @Setter 클래스 레벨 적용, 수동 getter/setter 제거
    - EventAtdrnVO: @Getter @Setter 클래스 레벨 적용, 수동 getter/setter 제거
    - IntnetSvcGuidanceVO: 기존 필드별 어노테이션을 클래스 레벨로 통합, 수동 getter/setter 제거
    - pom.xml: maven-compiler-plugin에 annotationProcessorPaths(lombok) 추가

--- src/main/java/egovframework/com/uss/ion/isg/service/IntnetSvcGuidanceVO.java
+++ src/main/java/egovframework/com/uss/ion/isg/service/IntnetSvcGuidanceVO.java
@@ -13,11 +13,11 @@ import lombok.Setter;
  * <pre>
  * 개요
  * - 인터넷서비스안내에 대한 VO 클래스를 정의한다.
- * 
+ *
  * 상세내용
  * - 인터넷서비스안내의 일련번호, 인터넷서비스명, 인터넷서비스설명, 반영여부 항목을 관리한다.
  * </pre>
- * 
+ *
  * @author 공통컴포넌트 개발팀 홍길동
  * @since 2010.08.03
  * @version 1.0
@@ -34,32 +34,34 @@ import lombok.Setter;
  *
  *      </pre>
  */
+@Getter
+@Setter
 public class IntnetSvcGuidanceVO extends ComDefaultVO {
 
 	/**
 	 * serialVersionUID
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * 인터넷서비스ID
-	 */	
+	 */
 	private String intnetSvcId;
 	/**
 	 * 인터넷서비스명
-	 */		
+	 */
 	@EgovNullCheck
 	@Size(max=20)
 	private String intnetSvcNm;
 	/**
 	 * 인터넷서비스설명
-	 */	
+	 */
 	@EgovNullCheck
 	@Size(max=200)
 	private String intnetSvcDc;
 	/**
 	 * 반영여부
-	 */		
+	 */
 	private String reflctAt;
 	/**
 	 * 사용자 ID
@@ -79,95 +81,6 @@ public class IntnetSvcGuidanceVO extends ComDefaultVO {
 	/**
 	 * 삭제여부
 	 */
-	@Getter
-	@Setter
 	private String[] delYn;
-
-	/**
-	 * @return the intnetSvcId
-	 */
-	public String getIntnetSvcId() {
-		return intnetSvcId;
-	}
-	/**
-	 * @param intnetSvcId the intnetSvcId to set
-	 */
-	public void setIntnetSvcId(String intnetSvcId) {
-		this.intnetSvcId = intnetSvcId;
-	}
-	/**
-	 * @return the intnetSvcNm
-	 */
-	public String getIntnetSvcNm() {
-		return intnetSvcNm;
-	}
-	/**
-	 * @param intnetSvcNm the intnetSvcNm to set
-	 */
-	public void setIntnetSvcNm(String intnetSvcNm) {
-		this.intnetSvcNm = intnetSvcNm;
-	}
-	/**
-	 * @return the intnetSvcDc
-	 */
-	public String getIntnetSvcDc() {
-		return intnetSvcDc;
-	}
-	/**
-	 * @param intnetSvcDc the intnetSvcDc to set
-	 */
-	public void setIntnetSvcDc(String intnetSvcDc) {
-		this.intnetSvcDc = intnetSvcDc;
-	}
-	/**
-	 * @return the reflctAt
-	 */
-	public String getReflctAt() {
-		return reflctAt;
-	}
-	/**
-	 * @param reflctAt the reflctAt to set
-	 */
-	public void setReflctAt(String reflctAt) {
-		this.reflctAt = reflctAt;
-	}
-	/**
-	 * @return the userId
-	 */
-	public String getUserId() {
-		return userId;
-	}
-	/**
-	 * @param userId the userId to set
-	 */
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	/**
-	 * @return the frstRegisterPnttm
-	 */
-	public String getFrstRegisterPnttm() {
-		return frstRegisterPnttm;
-	}
-	/**
-	 * @param frstRegisterPnttm the frstRegisterPnttm to set
-	 */
-	public void setFrstRegisterPnttm(String frstRegisterPnttm) {
-		this.frstRegisterPnttm = frstRegisterPnttm;
-	}
-
-	/**
-	 * @return the intnetSvcGuidanceList
-	 */
-	public List<IntnetSvcGuidanceVO> getIntnetSvcGuidanceList() {
-		return intnetSvcGuidanceList;
-	}
-
-	/**
-	 * @param intnetSvcGuidanceList the intnetSvcGuidanceList to set
-	 */
-	public void setIntnetSvcGuidanceList(List<IntnetSvcGuidanceVO> intnetSvcGuidanceList) {
-		this.intnetSvcGuidanceList = intnetSvcGuidanceList;
-	}
 
 }
