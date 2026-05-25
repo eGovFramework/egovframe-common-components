5ddd02a97d3c6ee7a77201faef8829963f7cbb5d
dasomel <dasomell@gmail.com>
2026-05-26 00:27:38 +0900

    refactor: cop/sms·smt(djm·lsm·mrm) 모듈 VO에 Lombok @Getter/@Setter 적용
    수동으로 작성된 getter/setter 보일러플레이트를 제거하고
    @Getter, @Setter 어노테이션으로 대체한다.
    pom.xml maven-compiler-plugin에 annotationProcessorPaths 추가.
    
    대상 파일:
    - cop/sms: SmsVO
    - cop/smt/djm: ChargerVO, DeptJobBxVO, DeptJobVO, DeptVO
    - cop/smt/lsm: EmplyrVO, LeaderSchdulVO, LeaderSttusVO
    - cop/smt/mrm: MemoReprtVO, ReportrVO

--- src/main/java/egovframework/com/cop/sms/service/SmsVO.java
+++ src/main/java/egovframework/com/cop/sms/service/SmsVO.java
@@ -1,5 +1,7 @@
 package egovframework.com.cop.sms.service;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
@@ -11,24 +13,27 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  *
  * <pre>
  * << 개정이력(Modification Information) >>
- *   
+ *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.06.18  한성곤          최초 생성
+ *   2025.05.26  기여자          Lombok @Getter/@Setter 적용으로 보일러플레이트 제거
  *
  * </pre>
  */
+@Getter
+@Setter
 @SuppressWarnings("serial")
 public class SmsVO extends Sms {
     /** 검색조건 */
     private String searchCnd = "";
-    
+
     /** 검색단어 */
     private String searchWrd = "";
-    
+
     /** 정렬순서(DESC,ASC) */
     private String sortOrdr = "";
-    
+
     /** 현재페이지 */
     private int pageIndex = 1;
 
@@ -51,169 +56,10 @@ public class SmsVO extends Sms {
     private int rowNo = 0;
 
     /**
-     * searchCnd attribute를 리턴한다.
-     * @return the searchCnd
-     */
-    public String getSearchCnd() {
-        return searchCnd;
-    }
-
-    /**
-     * searchCnd attribute 값을 설정한다.
-     * @param searchCnd the searchCnd to set
-     */
-    public void setSearchCnd(String searchCnd) {
-        this.searchCnd = searchCnd;
-    }
-
-    /**
-     * searchWrd attribute를 리턴한다.
-     * @return the searchWrd
-     */
-    public String getSearchWrd() {
-        return searchWrd;
-    }
-
-    /**
-     * searchWrd attribute 값을 설정한다.
-     * @param searchWrd the searchWrd to set
-     */
-    public void setSearchWrd(String searchWrd) {
-        this.searchWrd = searchWrd;
-    }
-
-    /**
-     * sortOrdr attribute를 리턴한다.
-     * @return the sortOrdr
-     */
-    public String getSortOrdr() {
-        return sortOrdr;
-    }
-
-    /**
-     * sortOrdr attribute 값을 설정한다.
-     * @param sortOrdr the sortOrdr to set
-     */
-    public void setSortOrdr(String sortOrdr) {
-        this.sortOrdr = sortOrdr;
-    }
-
-    /**
-     * pageIndex attribute를 리턴한다.
-     * @return the pageIndex
-     */
-    public int getPageIndex() {
-        return pageIndex;
-    }
-
-    /**
-     * pageIndex attribute 값을 설정한다.
-     * @param pageIndex the pageIndex to set
-     */
-    public void setPageIndex(int pageIndex) {
-        this.pageIndex = pageIndex;
-    }
-
-    /**
-     * pageUnit attribute를 리턴한다.
-     * @return the pageUnit
-     */
-    public int getPageUnit() {
-        return pageUnit;
-    }
-
-    /**
-     * pageUnit attribute 값을 설정한다.
-     * @param pageUnit the pageUnit to set
-     */
-    public void setPageUnit(int pageUnit) {
-        this.pageUnit = pageUnit;
-    }
-
-    /**
-     * pageSize attribute를 리턴한다.
-     * @return the pageSize
-     */
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    /**
-     * pageSize attribute 값을 설정한다.
-     * @param pageSize the pageSize to set
-     */
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    /**
-     * firstIndex attribute를 리턴한다.
-     * @return the firstIndex
-     */
-    public int getFirstIndex() {
-        return firstIndex;
-    }
-
-    /**
-     * firstIndex attribute 값을 설정한다.
-     * @param firstIndex the firstIndex to set
-     */
-    public void setFirstIndex(int firstIndex) {
-        this.firstIndex = firstIndex;
-    }
-
-    /**
-     * lastIndex attribute를 리턴한다.
-     * @return the lastIndex
-     */
-    public int getLastIndex() {
-        return lastIndex;
-    }
-
-    /**
-     * lastIndex attribute 값을 설정한다.
-     * @param lastIndex the lastIndex to set
-     */
-    public void setLastIndex(int lastIndex) {
-        this.lastIndex = lastIndex;
-    }
-
-    /**
-     * recordCountPerPage attribute를 리턴한다.
-     * @return the recordCountPerPage
-     */
-    public int getRecordCountPerPage() {
-        return recordCountPerPage;
-    }
-
-    /**
-     * recordCountPerPage attribute 값을 설정한다.
-     * @param recordCountPerPage the recordCountPerPage to set
-     */
-    public void setRecordCountPerPage(int recordCountPerPage) {
-        this.recordCountPerPage = recordCountPerPage;
-    }
-
-    /**
-     * rowNo attribute를 리턴한다.
-     * @return the rowNo
-     */
-    public int getRowNo() {
-        return rowNo;
-    }
-
-    /**
-     * rowNo attribute 값을 설정한다.
-     * @param rowNo the rowNo to set
-     */
-    public void setRowNo(int rowNo) {
-        this.rowNo = rowNo;
-    }
-    
-    /**
      * toString 메소드를 대치한다.
      */
+    @Override
     public String toString() {
-	return ToStringBuilder.reflectionToString(this);
+        return ToStringBuilder.reflectionToString(this);
     }
 }
