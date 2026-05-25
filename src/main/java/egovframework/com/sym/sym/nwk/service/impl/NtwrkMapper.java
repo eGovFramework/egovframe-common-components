1b0885eddf85c4d4bd88de08bc85a620d92ff8fe
dasomel <dasomell@gmail.com>
2026-05-26 07:22:51 +0900

    refactor: sym/sym/nwk NtwrkDAO를 @EgovMapper 인터페이스 방식으로 전환
    NtwrkDAO를 EgovComAbstractDAO 상속 방식에서 @EgovMapper 어노테이션 인터페이스
    위임 방식으로 전환했다. 8개 RDBMS XML namespace를 인터페이스 FQN으로 변경하고
    MapperScannerConfigurer 빈을 context-mapper.xml에 추가했다. SQL 무변경.

--- src/main/java/egovframework/com/sym/sym/nwk/service/impl/NtwrkMapper.java
+++ src/main/java/egovframework/com/sym/sym/nwk/service/impl/NtwrkMapper.java
@@ -2,16 +2,15 @@ package egovframework.com.sym.sym.nwk.service.impl;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
+import org.egovframe.rte.psl.dataaccess.mapper.EgovMapper;
 
 import egovframework.com.sym.sym.nwk.service.Ntwrk;
 import egovframework.com.sym.sym.nwk.service.NtwrkVO;
-import jakarta.annotation.Resource;
 
 /**
  * <pre>
  * 개요
- * - 네트워크에 대한 DAO 클래스를 정의한다.
+ * - 네트워크에 대한 Mapper 인터페이스를 정의한다.
  *
  * 상세내용
  * - 네트워크에 대한 등록, 수정, 삭제, 조회 기능을 제공한다.
@@ -29,16 +28,12 @@ import jakarta.annotation.Resource;
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.03.20  lee.m.j       최초 생성
- *   2025.07.23  이백행          2025년 컨트리뷰션 PMD로 소프트웨어 보안약점 진단하고 제거하기-UnnecessaryBoxing(불필요한 WrapperObject 생성)
  *   2026.05.26  dasomel       @EgovMapper 인터페이스 방식으로 전환
  *
  *      </pre>
  */
-@Repository("ntwrkDAO")
-public class NtwrkDAO {
-
-	@Resource(name = "ntwrkDAO")
-	private NtwrkMapper ntwrkMapper;
+@EgovMapper("ntwrkDAO")
+public interface NtwrkMapper {
 
 	/**
 	 * 네트워크를 관리하기 위해 등록된 네트워크목록을 조회한다.
@@ -46,9 +41,7 @@ public class NtwrkDAO {
 	 * @param ntwrkVO - 네트워크 Vo
 	 * @return List - 네트워크 목록
 	 */
-	public List<NtwrkVO> selectNtwrkList(NtwrkVO ntwrkVO) throws Exception {
-		return ntwrkMapper.selectNtwrkList(ntwrkVO);
-	}
+	List<NtwrkVO> selectNtwrkList(NtwrkVO ntwrkVO) throws Exception;
 
 	/**
 	 * 네트워크목록 총 개수를 조회한다.
@@ -56,9 +49,7 @@ public class NtwrkDAO {
 	 * @param ntwrkVO - 네트워크 Vo
 	 * @return int - 네트워크 카운트 수
 	 */
-	public int selectNtwrkListTotCnt(NtwrkVO ntwrkVO) throws Exception {
-		return ntwrkMapper.selectNtwrkListTotCnt(ntwrkVO);
-	}
+	int selectNtwrkListTotCnt(NtwrkVO ntwrkVO) throws Exception;
 
 	/**
 	 * 등록된 네트워크의 상세정보를 조회한다.
@@ -66,35 +57,27 @@ public class NtwrkDAO {
 	 * @param ntwrkVO - 네트워크 Vo
 	 * @return NtwrkVO - 네트워크 Vo
 	 */
-	public NtwrkVO selectNtwrk(NtwrkVO ntwrkVO) throws Exception {
-		return ntwrkMapper.selectNtwrk(ntwrkVO);
-	}
+	NtwrkVO selectNtwrk(NtwrkVO ntwrkVO) throws Exception;
 
 	/**
 	 * 네트워크정보를 신규로 등록한다.
 	 *
 	 * @param ntwrk - 네트워크 model
 	 */
-	public void insertNtwrk(Ntwrk ntwrk) throws Exception {
-		ntwrkMapper.insertNtwrk(ntwrk);
-	}
+	void insertNtwrk(Ntwrk ntwrk) throws Exception;
 
 	/**
 	 * 기 등록된 네트워크정보를 수정한다.
 	 *
 	 * @param ntwrk - 네트워크 model
 	 */
-	public void updateNtwrk(Ntwrk ntwrk) throws Exception {
-		ntwrkMapper.updateNtwrk(ntwrk);
-	}
+	void updateNtwrk(Ntwrk ntwrk) throws Exception;
 
 	/**
 	 * 기 등록된 네트워크정보를 삭제한다.
 	 *
 	 * @param ntwrk - 네트워크 model
 	 */
-	public void deleteNtwrk(Ntwrk ntwrk) throws Exception {
-		ntwrkMapper.deleteNtwrk(ntwrk);
-	}
+	void deleteNtwrk(Ntwrk ntwrk) throws Exception;
 
 }
