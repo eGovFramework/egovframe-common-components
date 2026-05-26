c5249d5f18a7600088fd32ea1b0570672adc36f3
dasomel <dasomell@gmail.com>
2026-05-27 01:34:30 +0900

    refactor: 기념일관리 AnnvrsryManageDAO를 @EgovMapper 인터페이스 방식으로 전환
    eGovFrame 5.0에서 도입된 @EgovMapper 어노테이션 방식을 기념일관리(uss/ion/ans) 컴포넌트에 적용한다.
    기존 EgovComAbstractDAO 상속 클래스 방식은 sqlSession을 직접 다루는 레거시 패턴으로,
    인터페이스 기반 @EgovMapper 방식이 더 명확하고 유지보수성이 높다.
    
    - AnnvrsryManageDAO: @Repository 클래스 -> @EgovMapper 인터페이스로 전환
      EgovComAbstractDAO 상속 제거, selectList/selectOne/insert/update/delete 직접 호출 제거
    - EgovAnnvrsryManage_SQL_*.xml (8개 DB): namespace를 인터페이스 FQN으로 변경
      (SQL 내용 변경 없음, DB별 XML 유지)
    - context-mapper.xml: MapperScannerConfigurer 빈 추가
      basePackage=egovframework.com, annotationClass=EgovMapper

--- src/main/java/egovframework/com/uss/ion/ans/service/impl/AnnvrsryManageDAO.java
+++ src/main/java/egovframework/com/uss/ion/ans/service/impl/AnnvrsryManageDAO.java
@@ -2,15 +2,14 @@ package egovframework.com.uss.ion.ans.service.impl;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
+import org.egovframe.rte.psl.dataaccess.mapper.EgovMapper;
 
-import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
 import egovframework.com.uss.ion.ans.service.AnnvrsryManage;
 import egovframework.com.uss.ion.ans.service.AnnvrsryManageVO;
 
 /**
  * 개요
- * - 기념일관리에 대한 DAO 클래스를 정의한다.
+ * - 기념일관리에 대한 DAO 인터페이스를 정의한다.
  *
  * 상세내용
  * - 기념일관리에 대한 등록, 수정, 삭제, 조회, 반영확인 기능을 제공한다.
@@ -18,89 +17,78 @@ import egovframework.com.uss.ion.ans.service.AnnvrsryManageVO;
  * @author 이용
  * @version 1.0
  * @created 06-15-2010 오후 2:08:56
+ *
+ * <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2010.06.15  이용           최초 생성
+ *   2026.05.27  dasomel        @EgovMapper 인터페이스 방식으로 전환
+ *
+ * </pre>
  */
-
-@Repository("annvrsryManageDAO")
-public class AnnvrsryManageDAO extends EgovComAbstractDAO {
+@EgovMapper("annvrsryManageDAO")
+public interface AnnvrsryManageDAO {
 
 	/**
 	 * 기념일관리정보를 관리하기 위해 등록된 기념일관리 목록을 조회한다.
 	 * @param annvrsryManageVO - 기념일관리 VO
 	 * @return List - 기념일관리 목록
-	 */	
-	public List<AnnvrsryManageVO> selectAnnvrsryManageList(AnnvrsryManageVO annvrsryManageVO) throws Exception {
-		return selectList("annvrsryManageDAO.selectAnnvrsryManageList", annvrsryManageVO);
-	}
+	 */
+	List<AnnvrsryManageVO> selectAnnvrsryManageList(AnnvrsryManageVO annvrsryManageVO);
 
-    /**
+	/**
 	 * 기념일관리목록 총 개수를 조회한다.
 	 * @param annvrsryManageVO - 기념일관리 VO
 	 * @return int
-	 * @exception Exception
 	 */
-    public int selectAnnvrsryManageListTotCnt(AnnvrsryManageVO annvrsryManageVO) throws Exception {
-        return (Integer)selectOne("annvrsryManageDAO.selectAnnvrsryManageListTotCnt", annvrsryManageVO);
-    }
+	int selectAnnvrsryManageListTotCnt(AnnvrsryManageVO annvrsryManageVO);
 
 	/**
 	 * 등록된 기념일관리의 상세정보를 조회한다.
 	 * @param annvrsryManageVO - 기념일관리 VO
 	 * @return AnnvrsryManageVO - 기념일관리 VO
 	 */
-	public AnnvrsryManageVO selectAnnvrsryManage(AnnvrsryManageVO annvrsryManageVO)  throws Exception {
-		return (AnnvrsryManageVO) selectOne("annvrsryManageDAO.selectAnnvrsryManage", annvrsryManageVO);
-	}
+	AnnvrsryManageVO selectAnnvrsryManage(AnnvrsryManageVO annvrsryManageVO);
 
 	/**
 	 * 기념일관리정보를 신규로 등록한다.
 	 * @param annvrsryManage - 기념일관리 model
 	 */
-	public void insertAnnvrsryManage(AnnvrsryManage annvrsryManage) throws Exception {
-		insert("annvrsryManageDAO.insertAnnvrsryManage", annvrsryManage);
-	}
+	void insertAnnvrsryManage(AnnvrsryManage annvrsryManage);
 
 	/**
 	 * 기 등록된 기념일관리정보를 수정한다.
 	 * @param annvrsryManage - 기념일관리 model
 	 */
-	public void updateAnnvrsryManage(AnnvrsryManage annvrsryManage) throws Exception {
-		update("annvrsryManageDAO.updateAnnvrsryManage", annvrsryManage);
-	}
+	void updateAnnvrsryManage(AnnvrsryManage annvrsryManage);
 
 	/**
 	 * 기 등록된 기념일관리정보를 삭제한다.
 	 * @param annvrsryManage - 기념일관리 model
 	 */
-	public void deleteAnnvrsryManage(AnnvrsryManage annvrsryManage) throws Exception {
-        delete("annvrsryManageDAO.deleteAnnvrsryManage",annvrsryManage);
-	}
+	void deleteAnnvrsryManage(AnnvrsryManage annvrsryManage);
 
 	/**
 	 * 등록된 기념일관리의 상세정보를 조회한다.
 	 * @param annvrsryManageVO - 기념일관리 VO
-	 * @return AnnvrsryManageVO - 기념일관리 VO
-	 */	
-	public List<AnnvrsryManageVO> selectAnnvrsryGdcc(AnnvrsryManageVO annvrsryManageVO)  throws Exception {
-		return selectList("annvrsryManageDAO.selectAnnvrsryGdcc", annvrsryManageVO);
-	}
+	 * @return List - 기념일관리 목록
+	 */
+	List<AnnvrsryManageVO> selectAnnvrsryGdcc(AnnvrsryManageVO annvrsryManageVO);
 
-    /**
+	/**
 	 * 기념일관리 등록시 중복여부를 조회한다.
-	 * @param annvrsryManage - 기념일관리 VO
+	 * @param annvrsryManage - 기념일관리 model
 	 * @return int
-	 * @exception Exception
 	 */
-    public int selectAnnvrsryManageDplctAt(AnnvrsryManage annvrsryManage) throws Exception {
-        return (Integer)selectOne("annvrsryManageDAO.selectAnnvrsryManageDplctAt", annvrsryManage);
-    }
+	int selectAnnvrsryManageDplctAt(AnnvrsryManage annvrsryManage);
 
 	/**
 	 * 등록된 기념일관리의 상세정보를 조회한다.
 	 * @param annvrsryManageVO - 기념일관리 VO
 	 * @return AnnvrsryManageVO - 기념일관리 VO
 	 */
-	public AnnvrsryManageVO selectAnnvrsryManageBnde(AnnvrsryManageVO annvrsryManageVO)  throws Exception {
-		return (AnnvrsryManageVO) selectOne("annvrsryManageDAO.selectAnnvrsryManageBnde", annvrsryManageVO);
-	}
+	AnnvrsryManageVO selectAnnvrsryManageBnde(AnnvrsryManageVO annvrsryManageVO);
 
 }
