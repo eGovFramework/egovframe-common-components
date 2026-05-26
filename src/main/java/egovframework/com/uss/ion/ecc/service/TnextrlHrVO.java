540195c28f16f61f03c2d8e8ffc81bfad3e17b22
dasomel <dasomell@gmail.com>
2026-05-27 01:07:23 +0900

    refactor: uss/ion (ecc/ism/sit) 패키지 VO에 Lombok @Getter/@Setter 적용
    - EventCmpgnVO, TnextrlHrVO (ecc), SanctnerVO (ism), SiteVO (sit) 수동 getter/setter 제거
    - 클래스 레벨 @Getter @Setter 어노테이션으로 대체
    - isEventSvcDateRangeValid() 비즈니스 검증 메서드 보존
    - pom.xml maven-compiler-plugin에 annotationProcessorPaths 추가

--- src/main/java/egovframework/com/uss/ion/ecc/service/TnextrlHrVO.java
+++ src/main/java/egovframework/com/uss/ion/ecc/service/TnextrlHrVO.java
@@ -1,6 +1,8 @@
 package egovframework.com.uss.ion.ecc.service;
 
 import egovframework.com.cmm.ComDefaultVO;
+import lombok.Getter;
+import lombok.Setter;
 
 import org.egovframe.rte.ptl.reactive.validation.EgovNullCheck;
 import org.egovframe.rte.ptl.reactive.validation.EgovEmailCheck;
@@ -22,6 +24,8 @@ import jakarta.validation.constraints.Size;
  *
  * </pre>
  */
+@Getter
+@Setter
 public class TnextrlHrVO extends ComDefaultVO {
 
 	private static final long serialVersionUID = 1L;
@@ -31,7 +35,7 @@ public class TnextrlHrVO extends ComDefaultVO {
 	 */
 	@EgovNullCheck
 	private String sexdstnCode = "";
-	
+
 	/**
 	 * 성별코드명
 	 */
@@ -55,7 +59,7 @@ public class TnextrlHrVO extends ComDefaultVO {
 	 */
 	@EgovNullCheck
 	private String occpTyCode = "";
-	
+
 	/**
 	 * 직업유형코드명
 	 */
@@ -76,7 +80,7 @@ public class TnextrlHrVO extends ComDefaultVO {
 	 */
 	@EgovNullCheck
 	private String eventId = "";
-	
+
 	/**
 	 * 행사/이벤트/캠페인내용
 	 */
@@ -135,7 +139,7 @@ public class TnextrlHrVO extends ComDefaultVO {
 	 * 최초등록ID
 	 */
 	private String frstRegisterId = "";
-	
+
 	/**
 	 * 최초등록ID
 	 */
@@ -150,361 +154,5 @@ public class TnextrlHrVO extends ComDefaultVO {
 	 * 최종수정ID
 	 */
 	private String lastUpdusrId = "";
-
-	/**
-	 * sexdstnCode attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getSexdstnCode() {
-		return sexdstnCode;
-	}
-
-	/**
-	 * sexdstnCode attribute 값을 설정한다.
-	 * @return sexdstnCode String
-	 */
-	public void setSexdstnCode(String sexdstnCode) {
-		this.sexdstnCode = sexdstnCode;
-	}
-	
-	/**
-	 * sexdstnCodeNm attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getSexdstnCodeNm() {
-		return sexdstnCodeNm;
-	}
-	
-	/**
-	 * sexdstnCodeNm attribute 값을 설정한다.
-	 * @return sexdstnCodeNm String
-	 */
-	public void setSexdstnCodeNm(String sexdstnCodeNm) {
-		this.sexdstnCodeNm = sexdstnCodeNm;
-	}
-
-	/**
-	 * extrlHrNm attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getExtrlHrNm() {
-		return extrlHrNm;
-	}
-
-	/**
-	 * extrlHrNm attribute 값을 설정한다.
-	 * @return extrlHrNm String
-	 */
-	public void setExtrlHrNm(String extrlHrNm) {
-		this.extrlHrNm = extrlHrNm;
-	}
-
-	/**
-	 * emailAdres attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getEmailAdres() {
-		return emailAdres;
-	}
-
-	/**
-	 * emailAdres attribute 값을 설정한다.
-	 * @return emailAdres String
-	 */
-	public void setEmailAdres(String emailAdres) {
-		this.emailAdres = emailAdres;
-	}
-
-	/**
-	 * occpTyCode attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getOccpTyCode() {
-		return occpTyCode;
-	}
-
-	/**
-	 * occpTyCode attribute 값을 설정한다.
-	 * @return occpTyCode String
-	 */
-	public void setOccpTyCode(String occpTyCode) {
-		this.occpTyCode = occpTyCode;
-	}
-	
-	/**
-	 * occpTyCodeNm attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getOccpTyCodeNm() {
-		return occpTyCodeNm;
-	}
-	
-	/**
-	 * occpTyCodeNm attribute 값을 설정한다.
-	 * @return occpTyCodeNm String
-	 */
-	public void setOccpTyCodeNm(String occpTyCodeNm) {
-		this.occpTyCodeNm = occpTyCodeNm;
-	}
-
-	/**
-	 * psitnInsttNm attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getPsitnInsttNm() {
-		return psitnInsttNm;
-	}
-
-	/**
-	 * psitnInsttNm attribute 값을 설정한다.
-	 * @return psitnInsttNm String
-	 */
-	public void setPsitnInsttNm(String psitnInsttNm) {
-		this.psitnInsttNm = psitnInsttNm;
-	}
-
-	/**
-	 * extrlHrId attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getExtrlHrId() {
-		return extrlHrId;
-	}
-
-	/**
-	 * extrlHrId attribute 값을 설정한다.
-	 * @return extrlHrId String
-	 */
-	public void setExtrlHrId(String extrlHrId) {
-		this.extrlHrId = extrlHrId;
-	}
-
-	/**
-	 * eventId attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getEventId() {
-		return eventId;
-	}
-
-	/**
-	 * eventId attribute 값을 설정한다.
-	 * @return eventId String
-	 */
-	public void setEventId(String eventId) {
-		this.eventId = eventId;
-	}
-	
-	/**
-	 * eventCn attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getEventCn() {
-		return eventCn;
-	}
-	
-	/**
-	 * eventCn attribute 값을 설정한다.
-	 * @return eventCn String
-	 */
-	public void setEventCn(String eventCn) {
-		this.eventCn = eventCn;
-	}
-
-	/**
-	 * brth attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getBrth() {
-		return brth;
-	}
-
-	/**
-	 * brth attribute 값을 설정한다.
-	 * @return brth String
-	 */
-	public void setBrth(String brth) {
-		this.brth = brth;
-	}
-
-	/**
-	 * areaNo attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getAreaNo() {
-		return areaNo;
-	}
-
-	/**
-	 * areaNo attribute 값을 설정한다.
-	 * @return areaNo String
-	 */
-	public void setAreaNo(String areaNo) {
-		this.areaNo = areaNo;
-	}
-
-	/**
-	 * middleTelno attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getMiddleTelno() {
-		return middleTelno;
-	}
-
-	/**
-	 * middleTelno attribute 값을 설정한다.
-	 * @return middleTelno String
-	 */
-	public void setMiddleTelno(String middleTelno) {
-		this.middleTelno = middleTelno;
-	}
-
-	/**
-	 * endTelno attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getEndTelno() {
-		return endTelno;
-	}
-
-	/**
-	 * endTelno attribute 값을 설정한다.
-	 * @return endTelno String
-	 */
-	public void setEndTelno(String endTelno) {
-		this.endTelno = endTelno;
-	}
-
-	/**
-	 * brthYYYY attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getBrthYYYY() {
-		return brthYYYY;
-	}
-
-	/**
-	 * brthYYYY attribute 값을 설정한다.
-	 * @return brthYYYY String
-	 */
-	public void setBrthYYYY(String brthYYYY) {
-		this.brthYYYY = brthYYYY;
-	}
-
-	/**
-	 * brthMM attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getBrthMM() {
-		return brthMM;
-	}
-
-	/**
-	 * brthMM attribute 값을 설정한다.
-	 * @return brthMM String
-	 */
-	public void setBrthMM(String brthMM) {
-		this.brthMM = brthMM;
-	}
-
-	/**
-	 * brthDD attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getBrthDD() {
-		return brthDD;
-	}
-
-	/**
-	 * brthDD attribute 값을 설정한다.
-	 * @return brthDD String
-	 */
-	public void setBrthDD(String brthDD) {
-		this.brthDD = brthDD;
-	}
-
-	/**
-	 * frstRegisterPnttm attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getFrstRegisterPnttm() {
-		return frstRegisterPnttm;
-	}
-
-	/**
-	 * frstRegisterPnttm attribute 값을 설정한다.
-	 * @return frstRegisterPnttm String
-	 */
-	public void setFrstRegisterPnttm(String frstRegisterPnttm) {
-		this.frstRegisterPnttm = frstRegisterPnttm;
-	}
-
-	/**
-	 * frstRegisterId attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getFrstRegisterId() {
-		return frstRegisterId;
-	}
-
-	/**
-	 * frstRegisterId attribute 값을 설정한다.
-	 * @return frstRegisterId String
-	 */
-	public void setFrstRegisterId(String frstRegisterId) {
-		this.frstRegisterId = frstRegisterId;
-	}
-	
-	/**
-	 * frstRegisterNm attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getFrstRegisterNm() {
-		return frstRegisterNm;
-	}
-	
-	/**
-	 * frstRegisterNm attribute 값을 설정한다.
-	 * @return frstRegisterNm String
-	 */
-	public void setFrstRegisterNm(String frstRegisterNm) {
-		this.frstRegisterNm = frstRegisterNm;
-	}
-
-	/**
-	 * lastUpdusrPnttm attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getLastUpdusrPnttm() {
-		return lastUpdusrPnttm;
-	}
-
-	/**
-	 * lastUpdusrPnttm attribute 값을 설정한다.
-	 * @return lastUpdusrPnttm String
-	 */
-	public void setLastUpdusrPnttm(String lastUpdusrPnttm) {
-		this.lastUpdusrPnttm = lastUpdusrPnttm;
-	}
-
-	/**
-	 * lastUpdusrId attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getLastUpdusrId() {
-		return lastUpdusrId;
-	}
-
-	/**
-	 * lastUpdusrId attribute 값을 설정한다.
-	 * @return lastUpdusrId String
-	 */
-	public void setLastUpdusrId(String lastUpdusrId) {
-		this.lastUpdusrId = lastUpdusrId;
-	}
-
-
-
-
 
 }
