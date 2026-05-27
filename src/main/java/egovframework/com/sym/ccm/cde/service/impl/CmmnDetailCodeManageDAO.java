3cd4466a007f752c3eaa0f0cff9b9cf3e9cbb17f
dasomel <dasomell@gmail.com>
2026-05-28 00:05:57 +0900

    refactor: CmmnDetailCodeManageDAO를 @EgovMapper 인터페이스 방식으로 전환
    공통상세코드 관리 CmmnDetailCodeManageDAO를 EgovComAbstractDAO 상속 클래스에서
    @EgovMapper 인터페이스로 전환한다.
    
    - CmmnDetailCodeManageDAO: @Repository 클래스 → @EgovMapper 인터페이스로 변경
    - EgovCmmnDetailCodeManage_SQL_*.xml: namespace를 단순명(CmmnDetailCodeManage)에서
      인터페이스 FQN(egovframework.com.sym.ccm.cde.service.impl.CmmnDetailCodeManageDAO)으로 변경
    - context-mapper.xml: MapperScannerConfigurer 빈 추가
      (basePackage=egovframework.com, annotationClass=EgovMapper)
    
    XML 매퍼 파일은 DB 종류별(altibase/cubrid/goldilocks/maria/mysql/oracle/postgres/tibero) SQL
    차이가 있어 유지하고 namespace만 변경한다.

--- src/main/java/egovframework/com/sym/ccm/cde/service/impl/CmmnDetailCodeManageDAO.java
+++ src/main/java/egovframework/com/sym/ccm/cde/service/impl/CmmnDetailCodeManageDAO.java
@@ -2,15 +2,14 @@ package egovframework.com.sym.ccm.cde.service.impl;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
+import org.egovframe.rte.psl.dataaccess.mapper.EgovMapper;
 
 import egovframework.com.cmm.service.CmmnDetailCode;
-import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
 import egovframework.com.sym.ccm.cde.service.CmmnDetailCodeVO;
 
 /**
 *
-* 공통상세코드에 대한 데이터 접근 클래스를 정의한다
+* 공통상세코드에 대한 데이터 접근 인터페이스를 정의한다
 * @author 공통서비스 개발팀 이중호
 * @since 2009.04.01
 * @version 1.0
@@ -22,69 +21,55 @@ import egovframework.com.sym.ccm.cde.service.CmmnDetailCodeVO;
 *   수정일      수정자           수정내용
 *  -------    --------    ---------------------------
 *   2009.04.01  이중호          최초 생성
+*   2026.05.28  dasomel        @EgovMapper 인터페이스 방식으로 전환
 *
 * </pre>
 */
 
-@Repository("CmmnDetailCodeManageDAO")
-public class CmmnDetailCodeManageDAO extends EgovComAbstractDAO {
+@EgovMapper("CmmnDetailCodeManageDAO")
+public interface CmmnDetailCodeManageDAO {
 
     /**
 	 * 공통상세코드 총 개수를 조회한다.
      * @param searchVO
      * @return int(공통상세코드 총 개수)
      */
-    public int selectCmmnDetailCodeListTotCnt(CmmnDetailCodeVO searchVO) throws Exception {
-        return (Integer)selectOne("CmmnDetailCodeManage.selectCmmnDetailCodeListTotCnt", searchVO);
-    }
-    
+    int selectCmmnDetailCodeListTotCnt(CmmnDetailCodeVO searchVO) throws Exception;
+
     /**
 	 * 공통상세코드 목록을 조회한다.
      * @param searchVO
      * @return List(공통상세코드 목록)
      * @throws Exception
      */
-    public List<CmmnDetailCodeVO> selectCmmnDetailCodeList(CmmnDetailCodeVO searchVO) throws Exception {
-        return selectList("CmmnDetailCodeManage.selectCmmnDetailCodeList", searchVO);
-    }
+    List<CmmnDetailCodeVO> selectCmmnDetailCodeList(CmmnDetailCodeVO searchVO) throws Exception;
 
 	/**
 	 * 공통상세코드 상세항목을 조회한다.
 	 * @param cmmnDetailCodeVO
-	 * @return CmmnDetailCodeVO(공통상세코드)
+	 * @return CmmnDetailCode(공통상세코드)
 	 */
-	public CmmnDetailCode selectCmmnDetailCodeDetail(CmmnDetailCodeVO cmmnDetailCodeVO) throws Exception{
-		return (CmmnDetailCode) selectOne("CmmnDetailCodeManage.selectCmmnDetailCodeDetail", cmmnDetailCodeVO);
-	}
-	
+	CmmnDetailCode selectCmmnDetailCodeDetail(CmmnDetailCodeVO cmmnDetailCodeVO) throws Exception;
+
 	/**
 	 * 공통상세코드를 삭제한다.
 	 * @param cmmnDetailCodeVO
 	 * @throws Exception
 	 */
-	public void deleteCmmnDetailCode(CmmnDetailCodeVO cmmnDetailCodeVO) throws Exception{
-		delete("CmmnDetailCodeManage.deleteCmmnDetailCode", cmmnDetailCodeVO);
-		
-	}
+	void deleteCmmnDetailCode(CmmnDetailCodeVO cmmnDetailCodeVO) throws Exception;
 
 	/**
 	 * 공통상세코드를 등록한다.
 	 * @param cmmnDetailCodeVO
 	 * @throws Exception
 	 */
-	public void insertCmmnDetailCode(CmmnDetailCodeVO cmmnDetailCodeVO) throws Exception{
-		insert("CmmnDetailCodeManage.insertCmmnDetailCode", cmmnDetailCodeVO);
-		
-	}
+	void insertCmmnDetailCode(CmmnDetailCodeVO cmmnDetailCodeVO) throws Exception;
 
 	/**
 	 * 공통상세코드를 수정한다.
 	 * @param cmmnDetailCodeVO
 	 * @throws Exception
 	 */
-	public void updateCmmnDetailCode(CmmnDetailCodeVO cmmnDetailCodeVO) throws Exception{
-		insert("CmmnDetailCodeManage.updateCmmnDetailCode", cmmnDetailCodeVO);
-		
-	}
-    
+	void updateCmmnDetailCode(CmmnDetailCodeVO cmmnDetailCodeVO) throws Exception;
+
 }
