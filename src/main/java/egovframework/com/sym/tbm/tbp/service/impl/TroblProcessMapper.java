9ca573940a4e9fa05c59ad09eb1461fb902d0da5
dasomel <dasomell@gmail.com>
2026-05-28 07:50:06 +0900

    refactor: sym/tbm/tbp TroblProcessDAO를 @EgovMapper 인터페이스 방식으로 전환
    신규 TroblProcessMapper 인터페이스 추가, DAO @Resource 위임 전환.
    8개 RDBMS XML namespace를 인터페이스 FQN으로 변경. SQL 무변경.

--- src/main/java/egovframework/com/sym/tbm/tbp/service/impl/TroblProcessMapper.java
+++ src/main/java/egovframework/com/sym/tbm/tbp/service/impl/TroblProcessMapper.java
@@ -2,15 +2,14 @@ package egovframework.com.sym.tbm.tbp.service.impl;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
+import org.egovframe.rte.psl.dataaccess.mapper.EgovMapper;
 
 import egovframework.com.sym.tbm.tbp.service.TroblProcess;
 import egovframework.com.sym.tbm.tbp.service.TroblProcessVO;
-import jakarta.annotation.Resource;
 
 /**
  * 개요
- * - 장애처리결과 관리정보에 대한 DAO 클래스를 정의한다.
+ * - 장애처리결과 관리정보에 대한 Mapper 인터페이스를 정의한다.
  *
  * 상세내용
  * - 장애처리결과 관리정보에 대한 등록, 수정, 삭제, 조회 등의 기능을 제공한다.
@@ -19,53 +18,40 @@ import jakarta.annotation.Resource;
  * @version 1.0
  * @created 28-6-2010 오전 10:44:58
  */
-@Repository("troblProcessDAO")
-public class TroblProcessDAO {
-
-	@Resource(name = "troblProcessMapper")
-	private TroblProcessMapper troblProcessMapper;
+@EgovMapper
+public interface TroblProcessMapper {
 
 	/**
 	 * 장애처리정보를 관리하기 위해 대상 장애처리목록을 조회한다.
 	 * @param troblProcessVO - 장애처리결과 Vo
 	 * @return List - 장애처리결과 목록
 	 */
-	public List<TroblProcessVO> selectTroblProcessList(TroblProcessVO troblProcessVO) throws Exception {
-		return troblProcessMapper.selectTroblProcessList(troblProcessVO);
-	}
+	List<TroblProcessVO> selectTroblProcessList(TroblProcessVO troblProcessVO) throws Exception;
 
 	/**
 	 * 장애처리목록 총 개수를 조회한다.
 	 * @param troblProcessVO - 장애처리결과 Vo
 	 * @return int - 장애처리결과 카운트 수
 	 */
-	public int selectTroblProcessListTotCnt(TroblProcessVO troblProcessVO) throws Exception {
-		return troblProcessMapper.selectTroblProcessListTotCnt(troblProcessVO);
-	}
+	int selectTroblProcessListTotCnt(TroblProcessVO troblProcessVO) throws Exception;
 
 	/**
 	 * 등록된 장애처리의 상세정보를 조회한다.
 	 * @param troblProcessVO - 장애처리결과 Vo
 	 * @return troblProcessVO - 장애처리결과 Vo
 	 */
-	public TroblProcessVO selectTroblProcess(TroblProcessVO troblProcessVO) throws Exception {
-		return troblProcessMapper.selectTroblProcess(troblProcessVO);
-	}
+	TroblProcessVO selectTroblProcess(TroblProcessVO troblProcessVO) throws Exception;
 
 	/**
-	 * 장애처리정보를 신규로 등록한다.
+	 * 장애처리정보를 신규으로 등록한다.
 	 * @param troblProcess - 장애처리결과 model
 	 */
-	public void insertTroblProcess(TroblProcess troblProcess) throws Exception {
-		troblProcessMapper.insertTroblProcess(troblProcess);
-	}
+	void insertTroblProcess(TroblProcess troblProcess) throws Exception;
 
 	/**
 	 * 기 등록된 장애처리정보를 삭제한다.
 	 * @param troblProcess - 장애처리결과 model
 	 */
-	public void deleteTroblProcess(TroblProcess troblProcess) throws Exception {
-		troblProcessMapper.deleteTroblProcess(troblProcess);
-	}
+	void deleteTroblProcess(TroblProcess troblProcess) throws Exception;
 
 }
