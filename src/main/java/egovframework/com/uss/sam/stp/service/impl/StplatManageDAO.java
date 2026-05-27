9abebe6ebd0b79de493e989c7ea8d12fb4dfe5aa
dasomel <dasomell@gmail.com>
2026-05-28 07:59:02 +0900

    refactor: 약관관리 StplatManageDAO를 @EgovMapper 인터페이스 방식으로 전환
    eGovFrame 5.0에서 도입된 @EgovMapper 어노테이션 방식을 약관관리(uss/sam/stp) 컴포넌트에 적용한다.
    기존 EgovComAbstractDAO 상속 클래스 방식은 sqlSession을 직접 다루는 레거시 패턴으로,
    인터페이스 기반 @EgovMapper 방식이 더 명확하고 유지보수성이 높다.
    
    - StplatManageDAO: @Repository 클래스 -> @EgovMapper 인터페이스로 전환
      EgovComAbstractDAO 상속 제거, selectList/selectOne/insert/update/delete 직접 호출 제거
      throws Exception 제거 (MyBatis 인터페이스는 checked exception 불필요)
    - EgovStplatManage_SQL_*.xml (8개 DB): namespace를 인터페이스 FQN으로 변경
      (SQL 내용 변경 없음, DB별 XML 유지)
    - context-mapper.xml: MapperScannerConfigurer 빈 추가
      basePackage=egovframework.com, annotationClass=EgovMapper

--- src/main/java/egovframework/com/uss/sam/stp/service/impl/StplatManageDAO.java
+++ src/main/java/egovframework/com/uss/sam/stp/service/impl/StplatManageDAO.java
@@ -2,101 +2,71 @@ package egovframework.com.uss.sam.stp.service.impl;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
+import org.egovframe.rte.psl.dataaccess.mapper.EgovMapper;
 
-import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
 import egovframework.com.uss.sam.stp.service.StplatManageDefaultVO;
 import egovframework.com.uss.sam.stp.service.StplatManageVO;
 
 
 /**
  *
- * 약관내용을 처리하는 DAO 클래스
+ * 약관내용을 처리하는 DAO 인터페이스
  * @author 공통서비스 개발팀 박정규
  * @since 2009.04.01
  * @version 1.0
  * @see
  *
  * <pre>
- * << 개정이력(Modification Information) >>
+ * == 개정이력(Modification Information) ==
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.04.01  박정규          최초 생성
  *   2016.06.13  장동한          표준프레임워크 v3.6 개선
+ *   2026.05.28  dasomel        @EgovMapper 인터페이스 방식으로 전환
  *
  * </pre>
  */
-@Repository("StplatManageDAO")
-public class StplatManageDAO extends EgovComAbstractDAO {
-
+@EgovMapper("StplatManageDAO")
+public interface StplatManageDAO {
 
     /**
 	 * 약관정보 글 목록에 대한 상세내용을 조회한다.
 	 * @param vo
 	 * @return 조회한 글
-	 * @exception Exception
 	 */
-    public StplatManageVO selectStplatDetail(StplatManageVO vo) throws Exception {
-
-        return (StplatManageVO) selectOne("StplatManage.selectStplatDetail", vo);
-
-    }
+    StplatManageVO selectStplatDetail(StplatManageVO vo);
 
     /**
 	 * 약관정보 글 목록을 조회한다.
 	 * @param searchVO
 	 * @return 글 목록
-	 * @exception Exception
 	 */
-    public List<StplatManageVO> selectStplatList(StplatManageDefaultVO searchVO) throws Exception {
-
-        return selectList("StplatManage.selectStplatList", searchVO);
-
-    }
+    List<StplatManageVO> selectStplatList(StplatManageDefaultVO searchVO);
 
     /**
 	 * 약관정보 글 총 개수를 조회한다.
 	 * @param searchVO
 	 * @return 글 총 개수
 	 */
-    public int selectStplatListTotCnt(StplatManageDefaultVO searchVO) {
-
-        return (Integer)selectOne("StplatManage.selectStplatListTotCnt", searchVO);
-
-    }
+    int selectStplatListTotCnt(StplatManageDefaultVO searchVO);
 
 	/**
 	 * 약관정보 글을 등록한다.
 	 * @param vo
-	 * @exception Exception
 	 */
-    public void insertStplatCn(StplatManageVO vo) throws Exception {
-
-        insert("StplatManage.insertStplatCn", vo);
-
-    }
+    void insertStplatCn(StplatManageVO vo);
 
 	/**
 	 * 약관정보 글을 수정한다.
 	 * @param vo
-	 * @exception Exception
 	 */
-    public void updateStplatCn(StplatManageVO vo) throws Exception {
-
-        update("StplatManage.updateStplatCn", vo);
-
-    }
+    void updateStplatCn(StplatManageVO vo);
 
 	/**
 	 * 약관정보 글을 삭제한다.
 	 * @param vo
-	 * @exception Exception
 	 */
-    public void deleteStplatCn(StplatManageVO vo) throws Exception {
-
-        delete("StplatManage.deleteStplatCn", vo);
-
-    }
+    void deleteStplatCn(StplatManageVO vo);
 
 }
