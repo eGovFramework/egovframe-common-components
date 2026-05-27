00e939e5531d61afe3812620ae8c1a5d15a122bd
dasomel <dasomell@gmail.com>
2026-05-28 08:00:47 +0900

    refactor: 메인화면이미지 MainImageDAO를 @EgovMapper 인터페이스 방식으로 전환
    eGovFrame 5.0에서 도입된 @EgovMapper 어노테이션 방식을 메인화면이미지(uss/ion/msi) 컴포넌트에 적용한다.
    기존 EgovComAbstractDAO 상속 클래스 방식은 sqlSession을 직접 다루는 레거시 패턴으로,
    인터페이스 기반 @EgovMapper 방식이 더 명확하고 유지보수성이 높다.
    
    - MainImageDAO: @Repository 클래스 -> @EgovMapper 인터페이스로 전환
      EgovComAbstractDAO 상속 제거, selectList/selectOne/insert/update/delete 직접 호출 제거
    - EgovMainImage_SQL_*.xml (8개 DB): namespace를 인터페이스 FQN으로 변경
      (SQL 내용 변경 없음, DB별 XML 유지)
    - context-mapper.xml: MapperScannerConfigurer 빈 추가
      basePackage=egovframework.com, annotationClass=EgovMapper

--- src/main/java/egovframework/com/uss/ion/msi/service/impl/MainImageDAO.java
+++ src/main/java/egovframework/com/uss/ion/msi/service/impl/MainImageDAO.java
@@ -1,91 +1,87 @@
 /**
  * 개요
- * - 메인화면이미지에 대한 DAO 클래스를 정의한다.
- * 
+ * - 메인화면이미지에 대한 DAO 인터페이스를 정의한다.
+ *
  * 상세내용
  * - 메인화면이미지에 대한 등록, 수정, 삭제, 조회, 반영확인 기능을 제공한다.
  * - 메인화면이미지의 조회기능은 목록조회, 상세조회로 구분된다.
  * @author 이문준
  * @version 1.0
  * @created 03-8-2009 오후 2:08:58
+ *
+ * <pre>
+ *  == 개정이력(Modification Information) ==
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.08.03  이문준           최초 생성
+ *   2026.05.28  dasomel        @EgovMapper 인터페이스 방식으로 전환
+ *
+ * </pre>
  */
 
 package egovframework.com.uss.ion.msi.service.impl;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
+import org.egovframe.rte.psl.dataaccess.mapper.EgovMapper;
 
 import egovframework.com.cmm.service.FileVO;
-import egovframework.com.cmm.service.impl.EgovComAbstractDAO;
 import egovframework.com.uss.ion.msi.service.MainImageVO;
 
-@Repository("mainImageDAO")
-public class MainImageDAO extends EgovComAbstractDAO {
-	
+@EgovMapper("mainImageDAO")
+public interface MainImageDAO {
+
 	/**
 	 * 메인화면이미지정보를 관리하기 위해 등록된 메인화면이미지 목록을 조회한다.
 	 * @param mainImageVO - 메인이미지 VO
 	 * @return List - 메인이미지 목록
 	 */
-	public List<MainImageVO> selectMainImageList(MainImageVO mainImageVO) throws Exception {
-		return selectList("mainImageDAO.selectMainImageList", mainImageVO);
-	}
+	List<MainImageVO> selectMainImageList(MainImageVO mainImageVO);
 
-    /**
+	/**
 	 * 메인화면이미지목록 총 개수를 조회한다.
 	 * @param mainImageVO - 메인화면이미지 VO
 	 * @return int
-	 * @exception Exception
 	 */
-    public int selectMainImageListTotCnt(MainImageVO mainImageVO) throws Exception {
-        return (Integer)selectOne("mainImageDAO.selectMainImageListTotCnt", mainImageVO);
-    }
+	int selectMainImageListTotCnt(MainImageVO mainImageVO);
 
 	/**
 	 * 등록된 메인화면이미지의 상세정보를 조회한다.
 	 * @param mainImageVO - 메인이미지 VO
 	 * @return MainImageVO - 메인이미지 VO
 	 */
-	public MainImageVO selectMainImage(MainImageVO mainImageVO) throws Exception {
-		return (MainImageVO) selectOne("mainImageDAO.selectMainImage", mainImageVO);
-	}
+	MainImageVO selectMainImage(MainImageVO mainImageVO);
 
 	/**
 	 * 메인화면이미지정보를 신규로 등록한다.
-	 * @param mainImage - 메인이미지 model
+	 * @param mainImageVO - 메인이미지 VO
 	 */
-	public void insertMainImage(MainImageVO mainImageVO) throws Exception {
-		insert("mainImageDAO.insertMainImage", mainImageVO);
-	}
+	void insertMainImage(MainImageVO mainImageVO);
 
 	/**
 	 * 기 등록된 메인화면이미지정보를 수정한다.
+	 * @param mainImageVO - 메인이미지 VO
 	 */
-	public void updateMainImage(MainImageVO mainImageVO) throws Exception {
-		update("mainImageDAO.updateMainImage", mainImageVO);
-	}
+	void updateMainImage(MainImageVO mainImageVO);
 
 	/**
 	 * 기 등록된 메인화면이미지정보를 삭제한다.
+	 * @param mainImageVO - 메인이미지 VO
 	 */
-	public void deleteMainImage(MainImageVO mainImageVO) throws Exception {
-		delete("mainImageDAO.deleteMainImage", mainImageVO);
-	}
+	void deleteMainImage(MainImageVO mainImageVO);
 
 	/**
 	 * 기 등록된 메인화면이미지정보의 이미지파일을 삭제하기 위해 파일정보를 조회한다.
+	 * @param mainImageVO - 메인이미지 VO
+	 * @return FileVO - 파일 VO
 	 */
-	public FileVO selectMainImageFile(MainImageVO mainImageVO) throws Exception {
-		return (FileVO) selectOne("mainImageDAO.selectMainImageFile", mainImageVO);
-	}
+	FileVO selectMainImageFile(MainImageVO mainImageVO);
 
 	/**
 	 * 메인화면이미지가 특정화면에 반영된 결과를 조회한다.
 	 * @param mainImageVO - 메인이미지 VO
-	 * @return MainImageVO - 메인이미지 VO
+	 * @return List - 메인이미지 목록
 	 */
-	public List<MainImageVO> selectMainImageResult(MainImageVO mainImageVO) throws Exception {
-		return selectList("mainImageDAO.selectMainImageResult", mainImageVO);
-	}
+	List<MainImageVO> selectMainImageResult(MainImageVO mainImageVO);
 }
