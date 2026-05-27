bff27894ee55620e0ce8845b2450e147dc069134
dasomel <dasomell@gmail.com>
2026-05-28 07:54:18 +0900

    refactor: sym/ccm/zip DAO를 @EgovMapper 인터페이스 방식으로 전환
    ZipManageDAO와 RdnmadZipDAO가 EgovComAbstractDAO를 상속하던 방식에서
    @EgovMapper 인터페이스(ZipManageMapper, RdnmadZipMapper)를 통한
    MyBatis 매퍼 방식으로 전환한다.
    
    - ZipManageMapper, RdnmadZipMapper 인터페이스 신설 (@EgovMapper)
    - 각 DAO는 매퍼 인터페이스를 @Resource로 주입받아 위임하도록 재작성
    - 16개 매퍼 XML의 namespace를 인터페이스 FQN으로 변경
    - context-mapper.xml에 MapperConfigurer 빈 추가 (basePackage: egovframework.com)

--- src/main/java/egovframework/com/sym/ccm/zip/service/impl/RdnmadZipMapper.java
+++ src/main/java/egovframework/com/sym/ccm/zip/service/impl/RdnmadZipMapper.java
@@ -2,16 +2,14 @@ package egovframework.com.sym.ccm.zip.service.impl;
 
 import java.util.List;
 
+import org.egovframe.rte.psl.dataaccess.mapper.EgovMapper;
 import org.egovframe.rte.psl.dataaccess.util.EgovMap;
-import org.springframework.stereotype.Repository;
 
 import egovframework.com.sym.ccm.zip.service.Zip;
 import egovframework.com.sym.ccm.zip.service.ZipVO;
-import jakarta.annotation.Resource;
 
 /**
- *
- * 우편번호에 대한 데이터 접근 클래스를 정의한다
+ * 도로명주소 우편번호 관리에 대한 데이터 접근 매퍼 인터페이스
  * @author 공통서비스 개발팀 이기하
  * @since 2011.11.21
  * @version 1.0
@@ -26,80 +24,56 @@ import jakarta.annotation.Resource;
  *
  * </pre>
  */
-@Repository("RdnmadZipDAO")
-public class RdnmadZipDAO {
-
-	@Resource(name = "RdnmadZipMapper")
-	private RdnmadZipMapper rdnmadZipMapper;
+@EgovMapper("RdnmadZipMapper")
+public interface RdnmadZipMapper {
 
 	/**
 	 * 우편번호를 삭제한다.
 	 * @param zip
-	 * @throws Exception
 	 */
-	public void deleteZip(Zip zip) throws Exception {
-		rdnmadZipMapper.deleteZip(zip);
-	}
+	void deleteZip(Zip zip);
 
 	/**
 	 * 우편번호 전체를 삭제한다.
-	 * @throws Exception
 	 */
-	public void deleteAllZip() throws Exception {
-		rdnmadZipMapper.deleteAllZip();
-	}
+	void deleteAllZip();
 
 	/**
 	 * 우편번호를 등록한다.
 	 * @param zip
 	 */
-	public void insertZip(Zip zip) {
-		rdnmadZipMapper.insertZip(zip);
-	}
+	void insertZip(Zip zip);
 
 	/**
 	 * 우편번호 엑셀파일을 등록한다.
-	 * @throws Exception
 	 */
-	public void insertExcelZip() throws Exception {
-		rdnmadZipMapper.insertExcelZip();
-	}
+	void insertExcelZip();
 
 	/**
 	 * 우편번호 상세항목을 조회한다.
 	 * @param zip
 	 * @return Zip(우편번호)
 	 */
-	public Zip selectZipDetail(Zip zip) throws Exception {
-		return rdnmadZipMapper.selectZipDetail(zip);
-	}
+	Zip selectZipDetail(Zip zip);
 
 	/**
 	 * 우편번호 목록을 조회한다.
 	 * @param searchVO
 	 * @return List(우편번호 목록)
-	 * @throws Exception
 	 */
-	public List<EgovMap> selectZipList(ZipVO searchVO) throws Exception {
-		return rdnmadZipMapper.selectZipList(searchVO);
-	}
+	List<EgovMap> selectZipList(ZipVO searchVO);
 
 	/**
 	 * 우편번호 총 개수를 조회한다.
 	 * @param searchVO
 	 * @return int(우편번호 총 개수)
 	 */
-	public int selectZipListTotCnt(ZipVO searchVO) throws Exception {
-		return rdnmadZipMapper.selectZipListTotCnt(searchVO);
-	}
+	int selectZipListTotCnt(ZipVO searchVO);
 
 	/**
 	 * 우편번호를 수정한다.
 	 * @param zip
-	 * @throws Exception
 	 */
-	public void updateZip(Zip zip) throws Exception {
-		rdnmadZipMapper.updateZip(zip);
-	}
+	void updateZip(Zip zip);
 
 }
--- src/main/java/egovframework/com/sym/ccm/zip/service/impl/ZipManageMapper.java
+++ src/main/java/egovframework/com/sym/ccm/zip/service/impl/ZipManageMapper.java
@@ -2,16 +2,14 @@ package egovframework.com.sym.ccm.zip.service.impl;
 
 import java.util.List;
 
+import org.egovframe.rte.psl.dataaccess.mapper.EgovMapper;
 import org.egovframe.rte.psl.dataaccess.util.EgovMap;
-import org.springframework.stereotype.Repository;
 
 import egovframework.com.sym.ccm.zip.service.Zip;
 import egovframework.com.sym.ccm.zip.service.ZipVO;
-import jakarta.annotation.Resource;
 
 /**
- *
- * 우편번호에 대한 데이터 접근 클래스를 정의한다
+ * 우편번호 관리에 대한 데이터 접근 매퍼 인터페이스
  * @author 공통서비스 개발팀 이중호
  * @since 2009.04.01
  * @version 1.0
@@ -26,80 +24,56 @@ import jakarta.annotation.Resource;
  *
  * </pre>
  */
-@Repository("ZipManageDAO")
-public class ZipManageDAO {
-
-	@Resource(name = "ZipManageMapper")
-	private ZipManageMapper zipManageMapper;
+@EgovMapper("ZipManageMapper")
+public interface ZipManageMapper {
 
 	/**
 	 * 우편번호를 삭제한다.
 	 * @param zip
-	 * @throws Exception
 	 */
-	public void deleteZip(Zip zip) throws Exception {
-		zipManageMapper.deleteZip(zip);
-	}
+	void deleteZip(Zip zip);
 
 	/**
 	 * 우편번호 전체를 삭제한다.
-	 * @throws Exception
 	 */
-	public void deleteAllZip() throws Exception {
-		zipManageMapper.deleteAllZip();
-	}
+	void deleteAllZip();
 
 	/**
 	 * 우편번호를 등록한다.
 	 * @param zip
 	 */
-	public void insertZip(Zip zip) {
-		zipManageMapper.insertZip(zip);
-	}
+	void insertZip(Zip zip);
 
 	/**
 	 * 우편번호 엑셀파일을 등록한다.
-	 * @throws Exception
 	 */
-	public void insertExcelZip() throws Exception {
-		zipManageMapper.insertExcelZip();
-	}
+	void insertExcelZip();
 
 	/**
 	 * 우편번호 상세항목을 조회한다.
 	 * @param zip
 	 * @return Zip(우편번호)
 	 */
-	public Zip selectZipDetail(Zip zip) throws Exception {
-		return zipManageMapper.selectZipDetail(zip);
-	}
+	Zip selectZipDetail(Zip zip);
 
 	/**
 	 * 우편번호 목록을 조회한다.
 	 * @param searchVO
 	 * @return List(우편번호 목록)
-	 * @throws Exception
 	 */
-	public List<EgovMap> selectZipList(ZipVO searchVO) throws Exception {
-		return zipManageMapper.selectZipList(searchVO);
-	}
+	List<EgovMap> selectZipList(ZipVO searchVO);
 
 	/**
 	 * 우편번호 총 개수를 조회한다.
 	 * @param searchVO
 	 * @return int(우편번호 총 개수)
 	 */
-	public int selectZipListTotCnt(ZipVO searchVO) throws Exception {
-		return zipManageMapper.selectZipListTotCnt(searchVO);
-	}
+	int selectZipListTotCnt(ZipVO searchVO);
 
 	/**
 	 * 우편번호를 수정한다.
 	 * @param zip
-	 * @throws Exception
 	 */
-	public void updateZip(Zip zip) throws Exception {
-		zipManageMapper.updateZip(zip);
-	}
+	void updateZip(Zip zip);
 
 }
