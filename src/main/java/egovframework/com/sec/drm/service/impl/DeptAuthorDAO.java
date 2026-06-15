1be18d2a0921cccea927080ae10451ad28f00827
이백행 <dlqorgod@naver.com>
2026-06-15 12:50:10 +0900

    @exception Exception 제거

--- src/main/java/egovframework/com/sec/drm/service/impl/DeptAuthorDAO.java
+++ src/main/java/egovframework/com/sec/drm/service/impl/DeptAuthorDAO.java
@@ -33,7 +33,6 @@ public class DeptAuthorDAO extends EgovComAbstractDAO {
 	 * 부서별 할당된 권한목록 조회
 	 * @param deptAuthorVO DeptAuthorVO
 	 * @return List<DeptAuthorVO>
-	 * @exception Exception
 	 */
 	public List<DeptAuthorVO> selectDeptAuthorList(DeptAuthorVO deptAuthorVO) {
 		return selectList("deptAuthorDAO.selectDeptAuthorList", deptAuthorVO);
@@ -42,7 +41,6 @@ public class DeptAuthorDAO extends EgovComAbstractDAO {
 	/**
 	 * 부서에 해당하는 사용자에게 시스템 메뉴/접근권한을 일괄 할당
 	 * @param deptAuthor DeptAuthor
-	 * @exception Exception
 	 */
 	public void insertDeptAuthor(DeptAuthor deptAuthor) {
 		insert("deptAuthorDAO.insertDeptAuthor", deptAuthor);
@@ -51,7 +49,6 @@ public class DeptAuthorDAO extends EgovComAbstractDAO {
 	/**
 	 * 부서별 시스템 메뉴 접근권한을 수정하여 항목의 정합성을 체크하고 수정된 데이터를 데이터베이스에 반영
 	 * @param deptAuthor DeptAuthor
-	 * @exception Exception
 	 */
 	public void updateDeptAuthor(DeptAuthor deptAuthor) {
 		update("deptAuthorDAO.updateDeptAuthor", deptAuthor);
@@ -60,7 +57,6 @@ public class DeptAuthorDAO extends EgovComAbstractDAO {
 	/**
 	 * 불필요한 부서권한를 조회하여 데이터베이스에서 삭제
 	 * @param deptAuthor DeptAuthor
-	 * @exception Exception
 	 */
 	public void deleteDeptAuthor(DeptAuthor deptAuthor) {
 		delete("deptAuthorDAO.deleteDeptAuthor", deptAuthor);
@@ -70,7 +66,6 @@ public class DeptAuthorDAO extends EgovComAbstractDAO {
 	 * 부서권한 목록조회 카운트를 반환한다
 	 * @param deptAuthorVO DeptAuthorVO
 	 * @return int
-	 * @exception Exception
 	 */
 	public int selectDeptAuthorListTotCnt(DeptAuthorVO deptAuthorVO) {
 		return (Integer)selectOne("deptAuthorDAO.selectDeptAuthorListTotCnt", deptAuthorVO);
@@ -80,7 +75,6 @@ public class DeptAuthorDAO extends EgovComAbstractDAO {
 	 * 부서목록 조회
 	 * @param deptAuthorVO DeptAuthorVO
 	 * @return List<DeptAuthorVO>
-	 * @exception Exception
 	 */
 	public List<DeptAuthorVO> selectDeptList(DeptAuthorVO deptAuthorVO) {
 		return selectList("deptAuthorDAO.selectDeptList", deptAuthorVO);
@@ -90,7 +84,6 @@ public class DeptAuthorDAO extends EgovComAbstractDAO {
 	 * 부서목록 조회 카운트를 반환한다
 	 * @param deptAuthorVO DeptAuthorVO
 	 * @return int
-	 * @exception Exception
 	 */
 	public int selectDeptListTotCnt(DeptAuthorVO deptAuthorVO) {
 		return (Integer)selectOne("deptAuthorDAO.selectDeptListTotCnt", deptAuthorVO);
