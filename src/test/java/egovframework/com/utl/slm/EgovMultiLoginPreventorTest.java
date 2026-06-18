b1361635e4773e8f73b58104b29cab290f91293e
z3rotig4r <zerotiger4764@gmail.com>
2026-06-18 20:14:11 +0900

    test: 미사용 import 제거

--- src/test/java/egovframework/com/utl/slm/EgovMultiLoginPreventorTest.java
+++ src/test/java/egovframework/com/utl/slm/EgovMultiLoginPreventorTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
