3f1458a348d1166c7bd26b9a2d260e1b2629883d
dasomel <dasomell@gmail.com>
2026-06-19 02:51:02 +0900

    refactor: uss/olp 온라인설문/상담 모듈 VO Lombok @Getter/@Setter 적용
    uss/olp 하위 8개 VO 클래스의 순수 위임 접근자(return x / this.x = x)를
    Lombok @Getter/@Setter 어노테이션으로 교체하여 보일러플레이트를 제거한다.
    커스텀 로직이 있는 접근자는 존재하지 않아 전체 접근자를 대체하였다.
    
    대상 클래스:
    - CnsltManageDefaultVO, CnsltManageVO (cns: 온라인상담)
    - MeetingManageVO (mgt: 회의관리)
    - QustnrItemManageVO (qim: 설문항목)
    - QustnrManageVO (qmc: 설문관리)
    - QustnrQestnManageVO (qqm: 설문문항)
    - QustnrRespondInfoVO (qri: 설문응답정보)
    - QustnrRespondManageVO (qrm: 설문응답자관리)

--- src/main/java/egovframework/com/uss/olp/qqm/service/QustnrQestnManageVO.java
+++ src/main/java/egovframework/com/uss/olp/qqm/service/QustnrQestnManageVO.java
@@ -5,6 +5,10 @@ import java.io.Serializable;
 import org.egovframe.rte.ptl.reactive.validation.EgovNullCheck;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 설문문항 VO Class 구현
  * @author 공통서비스 장동한
@@ -21,6 +25,8 @@ import jakarta.validation.constraints.Size;
  *
  * </pre>
  */
+@Getter
+@Setter
 public class QustnrQestnManageVO implements Serializable {
 
 	private static final long serialVersionUID = -1010670861596333788L;
@@ -70,214 +76,5 @@ public class QustnrQestnManageVO implements Serializable {
 
 	/** 검색모드설정  */
 	private String searchMode = "";
-
-	/**
-	 * qestnrSj attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getQestnrSj() {
-		return qestnrSj;
-	}
-
-	/**
-	 * qestnrSj attribute 값을 설정한다.
-	 * @return qestnrSj String
-	 */
-	public void setQestnrSj(String qestnrSj) {
-		this.qestnrSj = qestnrSj;
-	}
-
-	/**
-	 * qestnrQesitmId attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getQestnrQesitmId() {
-		return qestnrQesitmId;
-	}
-
-	/**
-	 * qestnrQesitmId attribute 값을 설정한다.
-	 * @return qestnrQesitmId String
-	 */
-	public void setQestnrQesitmId(String qestnrQesitmId) {
-		this.qestnrQesitmId = qestnrQesitmId;
-	}
-
-	/**
-	 * qestnrId attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getQestnrId() {
-		return qestnrId;
-	}
-
-	/**
-	 * qestnrId attribute 값을 설정한다.
-	 * @return qestnrId String
-	 */
-	public void setQestnrId(String qestnrId) {
-		this.qestnrId = qestnrId;
-	}
-
-	/**
-	 * qestnSn attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getQestnSn() {
-		return qestnSn;
-	}
-
-	/**
-	 * qestnSn attribute 값을 설정한다.
-	 * @return qestnSn String
-	 */
-	public void setQestnSn(String qestnSn) {
-		this.qestnSn = qestnSn;
-	}
-
-	/**
-	 * qestnTyCode attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getQestnTyCode() {
-		return qestnTyCode;
-	}
-
-	/**
-	 * qestnTyCode attribute 값을 설정한다.
-	 * @return qestnTyCode String
-	 */
-	public void setQestnTyCode(String qestnTyCode) {
-		this.qestnTyCode = qestnTyCode;
-	}
-
-	/**
-	 * qestnCn attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getQestnCn() {
-		return qestnCn;
-	}
-
-	/**
-	 * qestnCn attribute 값을 설정한다.
-	 * @return qestnCn String
-	 */
-	public void setQestnCn(String qestnCn) {
-		this.qestnCn = qestnCn;
-	}
-
-	/**
-	 * mxmmChoiseCo attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getMxmmChoiseCo() {
-		return mxmmChoiseCo;
-	}
-
-	/**
-	 * mxmmChoiseCo attribute 값을 설정한다.
-	 * @return mxmmChoiseCo String
-	 */
-	public void setMxmmChoiseCo(String mxmmChoiseCo) {
-		this.mxmmChoiseCo = mxmmChoiseCo;
-	}
-
-	/**
-	 * qestnrTmplatId attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getQestnrTmplatId() {
-		return qestnrTmplatId;
-	}
-
-	/**
-	 * qestnrTmplatId attribute 값을 설정한다.
-	 * @return qestnrTmplatId String
-	 */
-	public void setQestnrTmplatId(String qestnrTmplatId) {
-		this.qestnrTmplatId = qestnrTmplatId;
-	}
-
-	/**
-	 * frstRegisterPnttm attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getFrstRegisterPnttm() {
-		return frstRegisterPnttm;
-	}
-
-	/**
-	 * frstRegisterPnttm attribute 값을 설정한다.
-	 * @return frstRegisterPnttm String
-	 */
-	public void setFrstRegisterPnttm(String frstRegisterPnttm) {
-		this.frstRegisterPnttm = frstRegisterPnttm;
-	}
-
-	/**
-	 * frstRegisterId attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getFrstRegisterId() {
-		return frstRegisterId;
-	}
-
-	/**
-	 * frstRegisterId attribute 값을 설정한다.
-	 * @return frstRegisterId String
-	 */
-	public void setFrstRegisterId(String frstRegisterId) {
-		this.frstRegisterId = frstRegisterId;
-	}
-
-	/**
-	 * lastUpdusrPnttm attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getLastUpdusrPnttm() {
-		return lastUpdusrPnttm;
-	}
-
-	/**
-	 * lastUpdusrPnttm attribute 값을 설정한다.
-	 * @return lastUpdusrPnttm String
-	 */
-	public void setLastUpdusrPnttm(String lastUpdusrPnttm) {
-		this.lastUpdusrPnttm = lastUpdusrPnttm;
-	}
-
-	/**
-	 * lastUpdusrId attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getLastUpdusrId() {
-		return lastUpdusrId;
-	}
-
-	/**
-	 * lastUpdusrId attribute 값을 설정한다.
-	 * @return lastUpdusrId String
-	 */
-	public void setLastUpdusrId(String lastUpdusrId) {
-		this.lastUpdusrId = lastUpdusrId;
-	}
-
-	/**
-	 * searchMode attribute 를 리턴한다.
-	 * @return the String
-	 */
-	public String getSearchMode() {
-		return searchMode;
-	}
-
-	/**
-	 * searchMode attribute 값을 설정한다.
-	 * @return searchMode String
-	 */
-	public void setSearchMode(String searchMode) {
-		this.searchMode = searchMode;
-	}
-
 
 }
