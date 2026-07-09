e099b133c6348a018ff8691242e9f595f51cfee7
Eric <leesk55@gmail.com>
2026-07-09 15:45:08 +0900

    refactor: List.size()==0 대신 isEmpty() 사용 (PMD UseCollectionIsEmpty)

--- src/main/java/egovframework/com/utl/sys/fsm/service/FileSystemChecker.java
+++ src/main/java/egovframework/com/utl/sys/fsm/service/FileSystemChecker.java
@@ -271,7 +271,7 @@ public class FileSystemChecker {
 				// os command problem, throw exception
 				throw new IOException("Command line returned OS error code '" + p.exitValue() + "' for command " + Arrays.asList(cmdAttribs));
 			}
-			if (lines.size() == 0) {
+			if (lines.isEmpty()) {
 				// unknown problem, throw exception
 				throw new IOException("Command line did not return any info " + "for command " + Arrays.asList(cmdAttribs));
 			}
