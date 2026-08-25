af52c2345168d11ac29b5257373eda71861e8e7d
content_j <content_j@naver.com>
2026-08-25 09:25:23 +0900

    PR #1219 리뷰 피드백 반영 # EgovFileMngUtil.java 변경 제외, split()테스트 추가 - downFile() orgFileName/orginFile 속성키 변경은 반대 방향이였고 #1167에서 별도 처리 예정이라 이번 PR 번위에서 되돌림 - EgovStringUtil.split() 구분자 길이 반영 수정은 유지 - 두 글자 이상 구분자에 대한 회귀 테스트 6개 추가(단일/2글자/3글자 구분자, 미매치, 배열길이 지정 버전 포함)

--- src/main/java/egovframework/com/cmm/service/EgovFileMngUtil.java
+++ src/main/java/egovframework/com/cmm/service/EgovFileMngUtil.java
@@ -36,12 +36,12 @@ import egovframework.com.cmm.EgovWebUtil;
 
 /**
  * 파일 관리 유틸리티
- *
+ * 
  * @author 공통 서비스 개발팀 이삼섭
  * @since 2009. 02. 13
  * @version 1.0
  * @see
- *
+ * 
  *      <pre>
  *  == 개정이력(Modification Information) ==
  *
@@ -54,8 +54,7 @@ import egovframework.com.cmm.EgovWebUtil;
  *   2022.11.11  김혜준          시큐어코딩 처리
  *   2024.12.04  신용호          downFile() KISA 시큐어코딩 처리
  *   2025.05.26  이백행          PMD로 소프트웨어 보안약점 진단하고 제거하기-FormalParameterNamingConventions(공식 매개변수 명명 규칙), CloseResource(리소스 닫기), LocalVariableNamingConventions(지역 변수 명명 규칙), AssignmentInOperand(피연산자의 할당)
- *   2026.08.14  content_j      downFile() orgFileName 조회 속성키 오타 수정(orginFile → orgFileName, NullPointerException 가능성 제거)
- *
+ * 
  *      </pre>
  */
 @Component("EgovFileMngUtil")
@@ -255,11 +254,10 @@ public class EgovFileMngUtil {
 			downFileName = (String) request.getAttribute("downFile");
 		}
 
-		// 2026-08-14 content_j downFile() orgFileName 조회 속성키 오타 수정(orginFile→orgFileName, NullPointerException 가능성 제거)
 		if ((String) request.getAttribute("orgFileName") == null) {
 			orgFileName = "";
 		} else {
-			orgFileName = (String) request.getAttribute("orgFileName");
+			orgFileName = (String) request.getAttribute("orginFile");
 		}
 
 		orgFileName = orgFileName.replaceAll("\r", "").replaceAll("\n", "");
@@ -391,19 +389,19 @@ public class EgovFileMngUtil {
 
 		/*
 		 * String uploadPath = propertiesService.getString("fileDir");
-		 *
+		 * 
 		 * File uFile = new File(uploadPath, requestedFile); int fSize = (int)
 		 * uFile.length();
-		 *
+		 * 
 		 * if (fSize > 0) { BufferedInputStream in = new BufferedInputStream(new
 		 * FileInputStream(uFile));
-		 *
+		 * 
 		 * String mimetype = "text/html";
-		 *
+		 * 
 		 * //response.setBufferSize(fSize); response.setContentType(mimetype);
 		 * response.setHeader("Content-Disposition", "attachment; filename=\"" +
 		 * requestedFile + "\""); response.setContentLength(fSize);
-		 *
+		 * 
 		 * FileCopyUtils.copy(in, response.getOutputStream()); in.close();
 		 * response.getOutputStream().flush(); response.getOutputStream().close(); }
 		 * else { response.setContentType("text/html"); PrintWriter printwriter =
@@ -422,15 +420,15 @@ public class EgovFileMngUtil {
 		 * String(orgFileName.getBytes(),"UTF-8" ));
 		 * response.setHeader("Content-Transfer-Encoding","binary");
 		 * response.setHeader("Pragma","no-cache"); response.setHeader("Expires","0");
-		 *
+		 * 
 		 * BufferedInputStream fin = new BufferedInputStream(new FileInputStream(file));
 		 * BufferedOutputStream outs = new
 		 * BufferedOutputStream(response.getOutputStream()); int read = 0;
-		 *
+		 * 
 		 * while ((read = fin.read(b)) != -1) { outs.write(b,0,read); }
 		 * log.debug(this.getClass().getName()
 		 * +" BufferedOutputStream Write Complete!!! ");
-		 *
+		 * 
 		 * outs.close(); fin.close(); //
 		 */
 	}
