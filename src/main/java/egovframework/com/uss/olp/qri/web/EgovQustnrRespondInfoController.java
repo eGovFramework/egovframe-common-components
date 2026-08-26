60d3f0be3c7f76b82969a8ca8f3b0b732c3bbaae
mochoping <hodolskm@gmail.com>
2026-08-26 11:18:49 +0900

    fix: restore PostMapping import removed by PR #1102

--- src/main/java/egovframework/com/uss/olp/qri/web/EgovQustnrRespondInfoController.java
+++ src/main/java/egovframework/com/uss/olp/qri/web/EgovQustnrRespondInfoController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
