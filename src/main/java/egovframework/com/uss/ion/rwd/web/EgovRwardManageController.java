b923a8498f540fbe15d59b582f1e82389413773a
이백행 <dlqorgod@naver.com>
2026-09-08 19:51:49 +0900

    chore(rward): 로그인 사용자 유형 디버그 로그 추가
    포상 수정 및 삭제 요청에서 인증된 사용자의 사용자 유형을
    debug 레벨로 기록하도록 Slf4j 로깅을 적용한다.
    
    로그 사용으로 불필요해진 unused 경고 억제 선언을 제거한다.

--- src/main/java/egovframework/com/uss/ion/rwd/web/EgovRwardManageController.java
+++ src/main/java/egovframework/com/uss/ion/rwd/web/EgovRwardManageController.java
@@ -31,6 +31,7 @@ import egovframework.com.uss.ion.rwd.service.RwardManageVO;
 import egovframework.com.utl.fcc.service.EgovStringUtil;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * <pre>
@@ -60,6 +61,7 @@ import jakarta.validation.Valid;
  *      </pre>
  */
 @Controller
+@Slf4j
 public class EgovRwardManageController extends EgovComAbstractController {
 
 	@Resource(name = "egovMessageSource")
@@ -243,7 +245,6 @@ public class EgovRwardManageController extends EgovComAbstractController {
 	 * @param rwardManage - 포상관리 model
 	 * @return String - 리턴 Url
 	 */
-	@SuppressWarnings("unused")
 	@PostMapping("/uss/ion/rwd/updtRwardManage.do")
 	public String updtRwardManage(@RequestParam("atchFileAt") String atchFileAt,
 			final MultipartHttpServletRequest multiRequest, @Valid @ModelAttribute("rwardManage") RwardManage rwardManage, BindingResult bindingResult,
@@ -292,6 +293,7 @@ public class EgovRwardManageController extends EgovComAbstractController {
 			}
 			// 첨부파일 관련 ID 생성 end...
 			LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+			log.debug("getUserSe={}", user.getUserSe());
 			rwardManage.setRwardDe(EgovStringUtil.removeMinusChar(rwardManage.getRwardDe()));
 			egovRwardManageService.updtRwardManage(rwardManage);
 			return "forward:/uss/ion/rwd/selectRwardManageList.do";
@@ -309,6 +311,7 @@ public class EgovRwardManageController extends EgovComAbstractController {
 			ModelMap model) throws Exception {
 		// 2026.07.13 KISA 보안취약점 조치
 		LoginVO _loginVO = egovAssertLoginUser();
+		log.debug("getUserSe={}", _loginVO.getUserSe());
 
 		// 신청자 본인 또는 관리자만 삭제 가능하도록 소유권 검증
 		RwardManageVO storedForDelete = new RwardManageVO();
